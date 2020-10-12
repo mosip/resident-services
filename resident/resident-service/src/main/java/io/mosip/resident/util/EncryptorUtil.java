62c7a5730c86816d12aa7249aa7224ec14591ead
Monobikash Das <M1045447@mindtree.com>
2020-10-12 17:12:37 +0530

    MOSIP-8064 : integrating with external container encryption functionality

--- resident/resident-service/src/main/java/io/mosip/resident/util/EncryptorUtil.java
+++ resident/resident-service/src/main/java/io/mosip/resident/util/EncryptorUtil.java
@@ -1,62 +1,29 @@
 package io.mosip.resident.util;
 
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.mosip.commons.khazana.util.EncryptionUtil;
+import io.mosip.commons.packet.constants.CryptomanagerConstant;
 import io.mosip.commons.packet.dto.packet.CryptomanagerRequestDto;
-import io.mosip.commons.packet.dto.packet.CryptomanagerResponseDto;
 import io.mosip.commons.packet.dto.packet.DecryptResponseDto;
 import io.mosip.commons.packet.exception.ApiNotAccessibleException;
 import io.mosip.commons.packet.exception.PacketDecryptionFailureException;
-import io.mosip.kernel.core.crypto.spi.CryptoCoreSpec;
-import io.mosip.kernel.core.exception.BaseCheckedException;
-import io.mosip.kernel.core.exception.ExceptionUtils;
 import io.mosip.kernel.core.exception.ServiceError;
-import io.mosip.kernel.core.logger.spi.Logger;
-import io.mosip.kernel.core.security.exception.MosipInvalidDataException;
-import io.mosip.kernel.core.security.exception.MosipInvalidKeyException;
 import io.mosip.kernel.core.util.CryptoUtil;
 import io.mosip.kernel.core.util.DateUtils;
-import io.mosip.kernel.keygenerator.bouncycastle.KeyGenerator;
-import io.mosip.resident.config.LoggerConfiguration;
 import io.mosip.resident.constant.ApiName;
-import io.mosip.resident.constant.LoggerFileConstant;
-import io.mosip.resident.constant.ResidentErrorCode;
-import io.mosip.resident.dto.LogDescription;
-import io.mosip.resident.dto.PublicKeyResponseDto;
-import io.mosip.resident.dto.RequestWrapper;
 import io.mosip.resident.dto.ResponseWrapper;
-import io.mosip.resident.exception.ApisResourceAccessException;
-import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.HttpServerErrorException;
 
-import javax.crypto.SecretKey;
 import java.io.IOException;
-import java.io.InputStream;
-import java.security.KeyFactory;
-import java.security.NoSuchAlgorithmException;
-import java.security.PrivateKey;
-import java.security.PublicKey;
-import java.security.Security;
-import java.security.spec.InvalidKeySpecException;
-import java.security.spec.X509EncodedKeySpec;
-import java.text.ParseException;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.security.SecureRandom;
 import java.time.format.DateTimeParseException;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * The Class EncryptorUtil.
@@ -95,6 +62,13 @@ public class EncryptorUtil {
             cryptomanagerRequestDto.setApplicationId(APPLICATION_ID);
             cryptomanagerRequestDto.setData(packetString);
             cryptomanagerRequestDto.setReferenceId(refId);
+            SecureRandom sRandom = new SecureRandom();
+            byte[] nonce = new byte[CryptomanagerConstant.GCM_NONCE_LENGTH];
+            byte[] aad = new byte[CryptomanagerConstant.GCM_AAD_LENGTH];
+            sRandom.nextBytes(nonce);
+            sRandom.nextBytes(aad);
+            cryptomanagerRequestDto.setAad(CryptoUtil.encodeBase64String(aad));
+            cryptomanagerRequestDto.setSalt(CryptoUtil.encodeBase64String(nonce));
             cryptomanagerRequestDto.setTimeStamp(DateUtils.getUTCCurrentDateTime());
 
             request.setId(DECRYPT_SERVICE_ID);
@@ -112,7 +86,7 @@ public class EncryptorUtil {
             }
 
             DecryptResponseDto responseObject = mapper.readValue(mapper.writeValueAsString(responseDto.getResponse()), DecryptResponseDto.class);
-            return responseObject.getData();
+            return CryptoUtil.encodeBase64(EncryptionUtil.mergeEncryptedData(CryptoUtil.decodeBase64(responseObject.getData()), nonce, aad));
 
         } catch (IOException e) {
             throw new PacketDecryptionFailureException(IO_EXCEPTION, e);
