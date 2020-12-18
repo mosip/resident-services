d56eee43209bb94e242771385d765fd30b51881c
M1022006 <M1022006@A2ML18485.mindtree.com>
2020-12-18 12:17:00 +0530

    deployment issue fixed

--- resident/resident-service/src/main/java/io/mosip/resident/ResidentBootApplication.java
+++ resident/resident-service/src/main/java/io/mosip/resident/ResidentBootApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = { "io.mosip.resident.*", "io.mosip.kernel.core.*",
-		"io.mosip.kernel.crypto.jce.*", "io.mosip.commons.packet.*", "io.mosip.kernel.keygenerator.bouncycastle.*"})
+		"io.mosip.kernel.crypto.jce.*", "io.mosip.commons.packet.*", "io.mosip.kernel.keygenerator.bouncycastle.*",
+		"${mosip.auth.adapter.impl.basepackage}" })
 public class ResidentBootApplication {
 
 	public static void main(String[] args) {
