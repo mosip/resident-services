996a4e9a87fef80d511629e6ac0e2b60f2f08da8
kameshsr <kameshsr1338@gmail.com>
2021-06-23 13:47:17 +0530

    openapi.json file generated

--- resident/resident-service/src/main/java/io/mosip/resident/ResidentBootApplication.java
+++ resident/resident-service/src/main/java/io/mosip/resident/ResidentBootApplication.java
@@ -2,10 +2,14 @@ package io.mosip.resident;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.mosip.kernel.dataaccess.hibernate.config.HibernateDaoConfig;
 
 @SpringBootApplication(scanBasePackages = { "io.mosip.resident.*", "io.mosip.kernel.core.*",
 		"io.mosip.kernel.crypto.jce.*", "io.mosip.commons.packet.*", "io.mosip.kernel.keygenerator.bouncycastle.*",
-		"${mosip.auth.adapter.impl.basepackage}" })
+		"${mosip.auth.adapter.impl.basepackage}" }
+		, exclude = HibernateDaoConfig.class
+)
+
 public class ResidentBootApplication {
 
 	public static void main(String[] args) {
