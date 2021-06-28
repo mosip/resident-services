82f88a47631f9eca61d299362fc28f277cc123f5
kameshsr <kameshsr1338@gmail.com>
2021-06-28 18:20:16 +0530

    MOSIP-15287 : Deleted Extra line in ResidentBootApplication.java

--- resident/resident-service/src/main/java/io/mosip/resident/ResidentBootApplication.java
+++ resident/resident-service/src/main/java/io/mosip/resident/ResidentBootApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication(scanBasePackages = { "io.mosip.resident.*", "io.mosip.kernel.core.*",
 		"io.mosip.kernel.crypto.jce.*", "io.mosip.commons.packet.*", "io.mosip.kernel.keygenerator.bouncycastle.*",
 		"${mosip.auth.adapter.impl.basepackage}" })
-
 public class ResidentBootApplication {
 
 	public static void main(String[] args) {
