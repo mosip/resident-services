eae8aaf69f02076bab29a3e686c097a844567607
Abubacker Siddik A <abuabdul86@hotmail.com>
2021-09-08 20:59:54 +0530

    MOSIP:17387 - Fix the mock bindings and update tests

--- resident/resident-service/src/test/java/io/mosip/resident/test/util/ResidentServiceRestClientTest.java
+++ resident/resident-service/src/test/java/io/mosip/resident/test/util/ResidentServiceRestClientTest.java
@@ -1,20 +1,8 @@
 package io.mosip.resident.test.util;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
-import java.net.URI;
-import java.security.KeyManagementException;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
-import java.util.List;
-
+import io.mosip.resident.constant.ApiName;
+import io.mosip.resident.dto.AutnTxnResponseDto;
+import io.mosip.resident.exception.ApisResourceAccessException;
 import io.mosip.resident.util.ResidentServiceRestClient;
 import org.junit.Before;
 import org.junit.Test;
@@ -35,178 +23,188 @@ import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import io.mosip.resident.constant.ApiName;
-import io.mosip.resident.dto.AutnTxnResponseDto;
-import io.mosip.resident.exception.ApisResourceAccessException;
+import java.net.URI;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ResidentServiceRestClientTest {
-	@InjectMocks
-    ResidentServiceRestClient residentServiceRestClient=new ResidentServiceRestClient();
-	
-	@Mock
-	RestTemplateBuilder builder;
 
-	@Mock
-	Environment environment;
-	
-	RestTemplate restTemplate;
-	
-	
-	
-	@Before 
-	public void setup() {
-		 restTemplate = mock( RestTemplate.class);
-		
-		 
-		
-	}
-	
-	@Test
-	public void testgetApi() throws KeyManagementException, NoSuchAlgorithmException, KeyStoreException, ApisResourceAccessException {
-		AutnTxnResponseDto autnTxnResponseDto=new AutnTxnResponseDto();
-		autnTxnResponseDto.setId("ancd");
-		ResponseEntity<AutnTxnResponseDto> obj=new ResponseEntity<AutnTxnResponseDto>(autnTxnResponseDto, HttpStatus.OK);
-		URI uri=UriComponentsBuilder.fromUriString("https://int.mosip.io/individualIdType/UIN/individualId/1234").build(false).encode().toUri();
-		ResidentServiceRestClient client=Mockito.spy(residentServiceRestClient);
-		doReturn(restTemplate).when(client).getResidentRestTemplate();
-		when(restTemplate.exchange(any(URI.class), any(HttpMethod.class),any(),Matchers.<Class<AutnTxnResponseDto>>any())).
-		thenReturn(obj);
-		
-		assertTrue(client.getApi(uri, AutnTxnResponseDto.class, "abcde").toString().contains("ancd"));
-	}
-	@Test(expected=ApisResourceAccessException.class)
-	public void testgetApiException() throws KeyManagementException, NoSuchAlgorithmException, KeyStoreException, ApisResourceAccessException {
-		AutnTxnResponseDto autnTxnResponseDto=new AutnTxnResponseDto();
-		autnTxnResponseDto.setId("ancd");
-		
-		URI uri=UriComponentsBuilder.fromUriString("https://int.mosip.io/individualIdType/UIN/individualId/1234").build(false).encode().toUri();
-		ResidentServiceRestClient client=Mockito.spy(residentServiceRestClient);
-		doReturn(restTemplate).when(client).getResidentRestTemplate();
-		when(restTemplate.exchange(any(URI.class), any(HttpMethod.class),any(),Matchers.<Class<AutnTxnResponseDto>>any())).
-		thenThrow(new RestClientException(""));
-		
-		client.getApi(uri, AutnTxnResponseDto.class, "abcde");
-	}
-	
-	@Test
-	public void testgetApiObject() throws KeyManagementException, NoSuchAlgorithmException, KeyStoreException, ApisResourceAccessException {
-		AutnTxnResponseDto autnTxnResponseDto=new AutnTxnResponseDto();
-		autnTxnResponseDto.setId("ancd");
-		
-		when(environment.getProperty(any(String.class))).thenReturn("https://int.mosip.io/");
-		ResidentServiceRestClient client=Mockito.spy(residentServiceRestClient);
-		doReturn(autnTxnResponseDto).when(client).getApi(any(),any(),any());
-		List<String> list=new ArrayList<>();
-		list.add("individualIdType");
-		list.add("UIN");
-		list.add("individualId");
-		list.add("1234");
-		
-		
-		assertTrue(client.getApi(ApiName.INTERNALAUTHTRANSACTIONS,list,"",null, AutnTxnResponseDto.class, "abcde").toString().contains("ancd"));
-	}
-	@Test(expected=ApisResourceAccessException.class)
-	public void testgetApiObjectException() throws KeyManagementException, NoSuchAlgorithmException, KeyStoreException, ApisResourceAccessException {
-		AutnTxnResponseDto autnTxnResponseDto=new AutnTxnResponseDto();
-		autnTxnResponseDto.setId("ancd");
-		
-		when(environment.getProperty(any(String.class))).thenReturn("https://int.mosip.io/");
-		ResidentServiceRestClient client=Mockito.spy(residentServiceRestClient);
-		doThrow(new ApisResourceAccessException()).when(client).getApi(any(),any(),any());
-		List<String> list=new ArrayList<>();
-		list.add("individualIdType");
-		list.add("UIN");
-		list.add("individualId");
-		list.add("1234");
-		
-		
-		client.getApi(ApiName.INTERNALAUTHTRANSACTIONS,list,"pageFetch,pageStart","50,1", AutnTxnResponseDto.class, "abcde");
-	}
-	
-	@Test
-	public void testpostApi() throws KeyManagementException, NoSuchAlgorithmException, KeyStoreException, ApisResourceAccessException {
-		AutnTxnResponseDto autnTxnResponseDto=new AutnTxnResponseDto();
-		autnTxnResponseDto.setId("ancd");
-		
-		
-		ResidentServiceRestClient client=Mockito.spy(residentServiceRestClient);
-		doReturn(restTemplate).when(client).getResidentRestTemplate();
-		when(restTemplate.postForObject(any(String.class),any(),Matchers.<Class<AutnTxnResponseDto>>any())).
-		thenReturn(autnTxnResponseDto);
-		
-		assertTrue(client.postApi("https://int.mosip.io/individualIdType/UIN/individualId/1234",MediaType.APPLICATION_JSON,autnTxnResponseDto, AutnTxnResponseDto.class, "abcde").toString().contains("ancd"));
-	}
-	@Test(expected=ApisResourceAccessException.class)
-	public void testpostApiException() throws KeyManagementException, NoSuchAlgorithmException, KeyStoreException, ApisResourceAccessException {
-		AutnTxnResponseDto autnTxnResponseDto=new AutnTxnResponseDto();
-		autnTxnResponseDto.setId("ancd");
-		
-		
-		ResidentServiceRestClient client=Mockito.spy(residentServiceRestClient);
-		doReturn(restTemplate).when(client).getResidentRestTemplate();
-		when(restTemplate.postForObject(any(String.class),any(),Matchers.<Class<AutnTxnResponseDto>>any())).
-		thenThrow(new RestClientException(""));
-		
-		assertTrue(client.postApi("https://int.mosip.io/individualIdType/UIN/individualId/1234",MediaType.APPLICATION_JSON,autnTxnResponseDto, AutnTxnResponseDto.class, "abcde").toString().contains("ancd"));
-	}
-	
-	@Test
-	public void testpatchApi() throws Exception {
-		AutnTxnResponseDto autnTxnResponseDto=new AutnTxnResponseDto();
-		autnTxnResponseDto.setId("ancd");
-		
-		
-		ResidentServiceRestClient client=Mockito.spy(residentServiceRestClient);
-		doReturn(restTemplate).when(client).getResidentRestTemplate();
-		when(restTemplate.patchForObject(any(String.class),any(),Matchers.<Class<AutnTxnResponseDto>>any())).
-		thenReturn(autnTxnResponseDto);
-		
-		assertTrue(client.patchApi("https://int.mosip.io/individualIdType/UIN/individualId/1234",autnTxnResponseDto, AutnTxnResponseDto.class, "abcde").toString().contains("ancd"));
-	}
-	@Test(expected=ApisResourceAccessException.class)
-	public void testpatchApiException() throws Exception {
-		AutnTxnResponseDto autnTxnResponseDto=new AutnTxnResponseDto();
-		autnTxnResponseDto.setId("ancd");
-		
-		
-		ResidentServiceRestClient client=Mockito.spy(residentServiceRestClient);
-		doReturn(restTemplate).when(client).getResidentRestTemplate();
-		when(restTemplate.patchForObject(any(String.class),any(),Matchers.<Class<AutnTxnResponseDto>>any())).
-		thenThrow(new RestClientException(""));
-		
-		assertTrue(client.patchApi("https://int.mosip.io/individualIdType/UIN/individualId/1234",autnTxnResponseDto, AutnTxnResponseDto.class, "abcde").toString().contains("ancd"));
-	}
-	
-	@Test
-	public void testputApi() throws KeyManagementException, NoSuchAlgorithmException, KeyStoreException, ApisResourceAccessException {
-		AutnTxnResponseDto autnTxnResponseDto=new AutnTxnResponseDto();
-		autnTxnResponseDto.setId("ancd");
-		ResponseEntity<AutnTxnResponseDto> obj=new ResponseEntity<AutnTxnResponseDto>(autnTxnResponseDto, HttpStatus.OK);
-		
-		ResidentServiceRestClient client=Mockito.spy(residentServiceRestClient);
-		doReturn(restTemplate).when(client).getResidentRestTemplate();
-		when(restTemplate.exchange(any(String.class), any(HttpMethod.class),any(),Matchers.<Class<AutnTxnResponseDto>>any())).
-		thenReturn(obj);
-		
-		assertTrue(client.putApi("https://int.mosip.io/individualIdType/UIN/individualId/1234",autnTxnResponseDto, AutnTxnResponseDto.class,MediaType.APPLICATION_JSON, "abcde").toString().contains("ancd"));
-	}
-	@Test(expected=ApisResourceAccessException.class)
-	public void testputApiException() throws KeyManagementException, NoSuchAlgorithmException, KeyStoreException, ApisResourceAccessException {
-		AutnTxnResponseDto autnTxnResponseDto=new AutnTxnResponseDto();
-		autnTxnResponseDto.setId("ancd");
-		
-		
-		ResidentServiceRestClient client=Mockito.spy(residentServiceRestClient);
-		doReturn(restTemplate).when(client).getResidentRestTemplate();
-		when(restTemplate.exchange(any(String.class), any(HttpMethod.class),any(),Matchers.<Class<AutnTxnResponseDto>>any())).
-		thenThrow(new RestClientException(""));
-		
-		client.putApi("https://int.mosip.io/individualIdType/UIN/individualId/1234",autnTxnResponseDto, AutnTxnResponseDto.class,MediaType.APPLICATION_JSON, "abcde");
-	}
-	@Test
-	public void testgetRestTemplate() throws KeyManagementException, NoSuchAlgorithmException, KeyStoreException {
-		when(environment.getActiveProfiles()).thenReturn(new String[10]);
-		assertEquals(residentServiceRestClient.getResidentRestTemplate().getRequestFactory().getClass(),HttpComponentsClientHttpRequestFactory.class);
-	} 
+    @InjectMocks
+    ResidentServiceRestClient residentServiceRestClient;
+
+    @Mock
+    RestTemplateBuilder builder;
+
+    @Mock
+    Environment environment;
+
+    @Mock
+    RestTemplate restTemplate;
+
+
+    @Before
+    public void setup() {
+    }
+
+    @Test
+    public void testgetApi() throws KeyManagementException, NoSuchAlgorithmException, KeyStoreException, ApisResourceAccessException {
+        AutnTxnResponseDto autnTxnResponseDto = new AutnTxnResponseDto();
+        autnTxnResponseDto.setId("ancd");
+        ResponseEntity<AutnTxnResponseDto> obj = new ResponseEntity<>(autnTxnResponseDto, HttpStatus.OK);
+        URI uri = UriComponentsBuilder.fromUriString("https://int.mosip.io/individualIdType/UIN/individualId/1234").build(false).encode().toUri();
+        ResidentServiceRestClient client = Mockito.spy(residentServiceRestClient);
+        doReturn(restTemplate).when(client).getResidentRestTemplate();
+        when(restTemplate.exchange(any(URI.class), any(HttpMethod.class), any(), Matchers.<Class<AutnTxnResponseDto>>any())).thenReturn(obj);
+
+        assertTrue(client.getApi(uri, AutnTxnResponseDto.class, "abcde").toString().contains("ancd"));
+    }
+
+    @Test(expected = ApisResourceAccessException.class)
+    public void testgetApiException() throws KeyManagementException, NoSuchAlgorithmException, KeyStoreException, ApisResourceAccessException {
+        AutnTxnResponseDto autnTxnResponseDto = new AutnTxnResponseDto();
+        autnTxnResponseDto.setId("ancd");
+
+        URI uri = UriComponentsBuilder.fromUriString("https://int.mosip.io/individualIdType/UIN/individualId/1234").build(false).encode().toUri();
+        ResidentServiceRestClient client = Mockito.spy(residentServiceRestClient);
+        doReturn(restTemplate).when(client).getResidentRestTemplate();
+        when(restTemplate.exchange(any(URI.class), any(HttpMethod.class), any(), Matchers.<Class<AutnTxnResponseDto>>any())).
+                thenThrow(new RestClientException(""));
+
+        client.getApi(uri, AutnTxnResponseDto.class, "abcde");
+    }
+
+    @Test
+    public void testgetApiObject() throws ApisResourceAccessException {
+        AutnTxnResponseDto autnTxnResponseDto = new AutnTxnResponseDto();
+        autnTxnResponseDto.setId("ancd");
+
+        when(environment.getProperty(any(String.class))).thenReturn("https://int.mosip.io/");
+        ResidentServiceRestClient client = Mockito.spy(residentServiceRestClient);
+        doReturn(autnTxnResponseDto).when(client).getApi(any(), any(), any());
+        List<String> list = new ArrayList<>();
+        list.add("individualIdType");
+        list.add("UIN");
+        list.add("individualId");
+        list.add("1234");
+
+
+        assertTrue(client.getApi(ApiName.INTERNALAUTHTRANSACTIONS, list, "", null, AutnTxnResponseDto.class, "abcde").toString().contains("ancd"));
+    }
+
+    @Test(expected = ApisResourceAccessException.class)
+    public void testgetApiObjectException() throws KeyManagementException, NoSuchAlgorithmException, KeyStoreException, ApisResourceAccessException {
+        AutnTxnResponseDto autnTxnResponseDto = new AutnTxnResponseDto();
+        autnTxnResponseDto.setId("ancd");
+
+        when(environment.getProperty(any(String.class))).thenReturn("https://int.mosip.io/");
+        ResidentServiceRestClient client = Mockito.spy(residentServiceRestClient);
+        doThrow(new ApisResourceAccessException()).when(client).getApi(any(), any(), any());
+        List<String> list = new ArrayList<>();
+        list.add("individualIdType");
+        list.add("UIN");
+        list.add("individualId");
+        list.add("1234");
+
+
+        client.getApi(ApiName.INTERNALAUTHTRANSACTIONS, list, "pageFetch,pageStart", "50,1", AutnTxnResponseDto.class, "abcde");
+    }
+
+    @Test
+    public void testpostApi() throws KeyManagementException, NoSuchAlgorithmException, KeyStoreException, ApisResourceAccessException {
+        AutnTxnResponseDto autnTxnResponseDto = new AutnTxnResponseDto();
+        autnTxnResponseDto.setId("ancd");
+
+
+        ResidentServiceRestClient client = Mockito.spy(residentServiceRestClient);
+        doReturn(restTemplate).when(client).getResidentRestTemplate();
+        when(restTemplate.postForObject(any(String.class), any(), Matchers.<Class<AutnTxnResponseDto>>any())).
+                thenReturn(autnTxnResponseDto);
+
+        assertTrue(client.postApi("https://int.mosip.io/individualIdType/UIN/individualId/1234", MediaType.APPLICATION_JSON, autnTxnResponseDto, AutnTxnResponseDto.class, "abcde").toString().contains("ancd"));
+    }
+
+    @Test(expected = ApisResourceAccessException.class)
+    public void testpostApiException() throws KeyManagementException, NoSuchAlgorithmException, KeyStoreException, ApisResourceAccessException {
+        AutnTxnResponseDto autnTxnResponseDto = new AutnTxnResponseDto();
+        autnTxnResponseDto.setId("ancd");
+
+
+        ResidentServiceRestClient client = Mockito.spy(residentServiceRestClient);
+        doReturn(restTemplate).when(client).getResidentRestTemplate();
+        when(restTemplate.postForObject(any(String.class), any(), Matchers.<Class<AutnTxnResponseDto>>any())).
+                thenThrow(new RestClientException(""));
+
+        assertTrue(client.postApi("https://int.mosip.io/individualIdType/UIN/individualId/1234", MediaType.APPLICATION_JSON, autnTxnResponseDto, AutnTxnResponseDto.class, "abcde").toString().contains("ancd"));
+    }
+
+    @Test
+    public void testpatchApi() throws Exception {
+        AutnTxnResponseDto autnTxnResponseDto = new AutnTxnResponseDto();
+        autnTxnResponseDto.setId("ancd");
+
+
+        ResidentServiceRestClient client = Mockito.spy(residentServiceRestClient);
+        doReturn(restTemplate).when(client).getResidentRestTemplate();
+        when(restTemplate.patchForObject(any(String.class), any(), Matchers.<Class<AutnTxnResponseDto>>any())).
+                thenReturn(autnTxnResponseDto);
+
+        assertTrue(client.patchApi("https://int.mosip.io/individualIdType/UIN/individualId/1234", autnTxnResponseDto, AutnTxnResponseDto.class, "abcde").toString().contains("ancd"));
+    }
+
+    @Test(expected = ApisResourceAccessException.class)
+    public void testpatchApiException() throws Exception {
+        AutnTxnResponseDto autnTxnResponseDto = new AutnTxnResponseDto();
+        autnTxnResponseDto.setId("ancd");
+
+
+        ResidentServiceRestClient client = Mockito.spy(residentServiceRestClient);
+        doReturn(restTemplate).when(client).getResidentRestTemplate();
+        when(restTemplate.patchForObject(any(String.class), any(), Matchers.<Class<AutnTxnResponseDto>>any())).
+                thenThrow(new RestClientException(""));
+
+        assertTrue(client.patchApi("https://int.mosip.io/individualIdType/UIN/individualId/1234", autnTxnResponseDto, AutnTxnResponseDto.class, "abcde").toString().contains("ancd"));
+    }
+
+    @Test
+    public void testputApi() throws KeyManagementException, NoSuchAlgorithmException, KeyStoreException, ApisResourceAccessException {
+        AutnTxnResponseDto autnTxnResponseDto = new AutnTxnResponseDto();
+        autnTxnResponseDto.setId("ancd");
+        ResponseEntity<AutnTxnResponseDto> obj = new ResponseEntity<AutnTxnResponseDto>(autnTxnResponseDto, HttpStatus.OK);
+
+        ResidentServiceRestClient client = Mockito.spy(residentServiceRestClient);
+        doReturn(restTemplate).when(client).getResidentRestTemplate();
+        when(restTemplate.exchange(any(String.class), any(HttpMethod.class), any(), Matchers.<Class<AutnTxnResponseDto>>any())).
+                thenReturn(obj);
+
+        assertTrue(client.putApi("https://int.mosip.io/individualIdType/UIN/individualId/1234", autnTxnResponseDto, AutnTxnResponseDto.class, MediaType.APPLICATION_JSON, "abcde").toString().contains("ancd"));
+    }
+
+    @Test(expected = ApisResourceAccessException.class)
+    public void testputApiException() throws KeyManagementException, NoSuchAlgorithmException, KeyStoreException, ApisResourceAccessException {
+        AutnTxnResponseDto autnTxnResponseDto = new AutnTxnResponseDto();
+        autnTxnResponseDto.setId("ancd");
+
+
+        ResidentServiceRestClient client = Mockito.spy(residentServiceRestClient);
+        doReturn(restTemplate).when(client).getResidentRestTemplate();
+        when(restTemplate.exchange(any(String.class), any(HttpMethod.class), any(), Matchers.<Class<AutnTxnResponseDto>>any())).
+                thenThrow(new RestClientException(""));
+
+        client.putApi("https://int.mosip.io/individualIdType/UIN/individualId/1234", autnTxnResponseDto, AutnTxnResponseDto.class, MediaType.APPLICATION_JSON, "abcde");
+    }
+
+    @Test
+    public void testgetRestTemplate() throws KeyManagementException, NoSuchAlgorithmException, KeyStoreException {
+        when(environment.getActiveProfiles()).thenReturn(new String[10]);
+        assertEquals(residentServiceRestClient.getResidentRestTemplate().getRequestFactory().getClass(), HttpComponentsClientHttpRequestFactory.class);
+    }
 }
