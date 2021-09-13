96732380ff81f9bb4d75325f5beb1f3e18b47635
Abubacker Siddik <abuabdul86@hotmail.com>
2021-09-13 20:47:07 +0530

    Update rest calls to fetch regprocessor json on postconstruct - [MOSIP-17379] (#128)
    * MOSIP:17295: Update pom to 1.1.5.2
    
    * MOSIP:17295 - Add enhancement on the rest template instance creation
    
    * MOSIP:17387 - Fix the mock bindings and update tests
    
    * MOSIP:17379 - update rest calls in postconstruct for regprocessor json
    
    * MOSIP:17379 - remove deprecated getTemplate method

--- resident/resident-service/src/main/java/io/mosip/resident/util/Utilitiy.java
+++ resident/resident-service/src/main/java/io/mosip/resident/util/Utilitiy.java
@@ -1,21 +1,5 @@
 package io.mosip.resident.util;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.HttpServerErrorException;
-import org.springframework.web.client.RestTemplate;
-
 import io.mosip.kernel.core.exception.ServiceError;
 import io.mosip.kernel.core.http.ResponseWrapper;
 import io.mosip.kernel.core.logger.spi.Logger;
@@ -29,9 +13,21 @@ import io.mosip.resident.exception.ApisResourceAccessException;
 import io.mosip.resident.exception.IdRepoAppException;
 import io.mosip.resident.exception.ResidentServiceCheckedException;
 import io.mosip.resident.exception.ResidentServiceException;
+import org.apache.commons.lang3.StringUtils;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.util.*;
 
 /**
- * 
  * @author Girish Yarru
  * @version 1.0
  */
@@ -39,153 +35,164 @@ import io.mosip.resident.exception.ResidentServiceException;
 @Component
 public class Utilitiy {
 
-	private static final Logger logger = LoggerConfiguration.logConfig(Utilitiy.class);
+    private static final Logger logger = LoggerConfiguration.logConfig(Utilitiy.class);
 
-	@Autowired
-	private ResidentServiceRestClient residentServiceRestClient;
+    @Autowired
+    private ResidentServiceRestClient residentServiceRestClient;
 
-	@Autowired
-	private TokenGenerator tokenGenerator;
+    @Autowired
+    private TokenGenerator tokenGenerator;
 
-	@Value("${config.server.file.storage.uri}")
-	private String configServerFileStorageURL;
+    @Value("${config.server.file.storage.uri}")
+    private String configServerFileStorageURL;
 
-	@Value("${resident.identityjson}")
-	private String getRegProcessorIdentityJson;
+    @Value("${resident.identityjson}")
+    private String getRegProcessorIdentityJson;
 
-	@Value("${mosip.primary-language}")
-	private String primaryLang;
+    @Value("${mosip.primary-language}")
+    private String primaryLang;
 
-	@Value("${mosip.secondary-language}")
-	private String secondaryLang;
+    @Value("${mosip.secondary-language}")
+    private String secondaryLang;
 
-	@Value("${mosip.notification.language-type}")
-	private String languageType;
+    @Value("${mosip.notification.language-type}")
+    private String languageType;
 
-	@Autowired
-	private RestTemplate residentRestTemplate;
+    @Autowired
+    private RestTemplate residentRestTemplate;
 
-	private static final String IDENTITY = "identity";
-	private static final String VALUE = "value";
+    private static final String IDENTITY = "identity";
+    private static final String VALUE = "value";
+    private static String regProcessorIdentityJson = StringUtils.EMPTY;
 
-	@SuppressWarnings("unchecked")
-	public JSONObject retrieveIdrepoJson(String id) throws ResidentServiceCheckedException {
-		logger.debug(LoggerFileConstant.APPLICATIONID.toString(), LoggerFileConstant.UIN.name(), id,
-				"Utilitiy::retrieveIdrepoJson()::entry");
-		List<String> pathsegments = new ArrayList<>();
-		pathsegments.add(id);
-		ResponseWrapper<IdRepoResponseDto> response = null;
-		try {
-				response = (ResponseWrapper<IdRepoResponseDto>) residentServiceRestClient.getApi(
-						ApiName.IDREPOGETIDBYUIN, pathsegments, "", null, ResponseWrapper.class,
-						tokenGenerator.getToken());
+    @PostConstruct
+    private void loadRegProcessorIdentityJson() {
+        regProcessorIdentityJson = residentRestTemplate.getForObject(configServerFileStorageURL + getRegProcessorIdentityJson, String.class);
+        logger.info(LoggerFileConstant.SESSIONID.toString(), LoggerFileConstant.APPLICATIONID.toString(),
+                LoggerFileConstant.APPLICATIONID.toString(), "loadRegProcessorIdentityJson completed successfully");
+    }
 
-		} catch (IOException e) {
-			throw new ResidentServiceCheckedException(ResidentErrorCode.TOKEN_GENERATION_FAILED.getErrorCode(),
-					ResidentErrorCode.TOKEN_GENERATION_FAILED.getErrorMessage(), e);
-		} catch (ApisResourceAccessException e) {
-			if (e.getCause() instanceof HttpClientErrorException) {
-				HttpClientErrorException httpClientException = (HttpClientErrorException) e.getCause();
-				throw new ResidentServiceCheckedException(
-						ResidentErrorCode.API_RESOURCE_ACCESS_EXCEPTION.getErrorCode(),
-						httpClientException.getResponseBodyAsString());
+    @SuppressWarnings("unchecked")
+    public JSONObject retrieveIdrepoJson(String id) throws ResidentServiceCheckedException {
+        logger.debug(LoggerFileConstant.APPLICATIONID.toString(), LoggerFileConstant.UIN.name(), id,
+                "Utilitiy::retrieveIdrepoJson()::entry");
+        List<String> pathsegments = new ArrayList<>();
+        pathsegments.add(id);
+        ResponseWrapper<IdRepoResponseDto> response = null;
+        try {
+            response = (ResponseWrapper<IdRepoResponseDto>) residentServiceRestClient.getApi(
+                    ApiName.IDREPOGETIDBYUIN, pathsegments, "", null, ResponseWrapper.class,
+                    tokenGenerator.getToken());
 
-			} else if (e.getCause() instanceof HttpServerErrorException) {
-				HttpServerErrorException httpServerException = (HttpServerErrorException) e.getCause();
-				throw new ResidentServiceCheckedException(
-						ResidentErrorCode.API_RESOURCE_ACCESS_EXCEPTION.getErrorCode(),
-						httpServerException.getResponseBodyAsString());
-			} else {
-				throw new ResidentServiceCheckedException(
-						ResidentErrorCode.API_RESOURCE_ACCESS_EXCEPTION.getErrorCode(),
-						ResidentErrorCode.API_RESOURCE_ACCESS_EXCEPTION.getErrorMessage() + e.getMessage(), e);
-			}
-		}
-		
-		return retrieveErrorCode(response, id);
-	}
-	
-	public JSONObject retrieveErrorCode(ResponseWrapper<IdRepoResponseDto> response, String id)
-			throws ResidentServiceCheckedException {
-		ResidentErrorCode errorCode;
-		errorCode = ResidentErrorCode.INVALID_ID;
-		try {
-			logger.info(LoggerFileConstant.APPLICATIONID.toString(), LoggerFileConstant.UIN.name(), id,
-					"Utilitiy::retrieveIdrepoJson()::id repo response for given id::"
-							+ JsonUtil.writeValueAsString(response));
-			if (response == null)
-				throw new IdRepoAppException(errorCode.getErrorCode(), errorCode.getErrorMessage(),
-						"In valid response while requesting ID Repositary");
-			if (!response.getErrors().isEmpty()) {
-				List<ServiceError> error = response.getErrors();
-				throw new IdRepoAppException(errorCode.getErrorCode(), errorCode.getErrorMessage(),
-						error.get(0).getMessage());
-			}
-			String jsonResponse;
+        } catch (IOException e) {
+            throw new ResidentServiceCheckedException(ResidentErrorCode.TOKEN_GENERATION_FAILED.getErrorCode(),
+                    ResidentErrorCode.TOKEN_GENERATION_FAILED.getErrorMessage(), e);
+        } catch (ApisResourceAccessException e) {
+            if (e.getCause() instanceof HttpClientErrorException) {
+                HttpClientErrorException httpClientException = (HttpClientErrorException) e.getCause();
+                throw new ResidentServiceCheckedException(
+                        ResidentErrorCode.API_RESOURCE_ACCESS_EXCEPTION.getErrorCode(),
+                        httpClientException.getResponseBodyAsString());
 
-			jsonResponse = JsonUtil.writeValueAsString(response.getResponse());
-			JSONObject json = JsonUtil.readValue(jsonResponse, JSONObject.class);
-			logger.debug(LoggerFileConstant.APPLICATIONID.toString(), LoggerFileConstant.UIN.name(), id,
-					"Utilitiy::retrieveIdrepoJson()::exit");
-			return JsonUtil.getJSONObject(json, "identity");
-		} catch (IOException e) {
-			throw new ResidentServiceCheckedException(ResidentErrorCode.RESIDENT_SYS_EXCEPTION.getErrorCode(),
-					ResidentErrorCode.RESIDENT_SYS_EXCEPTION.getErrorMessage(), e);
-		}
-	}
+            } else if (e.getCause() instanceof HttpServerErrorException) {
+                HttpServerErrorException httpServerException = (HttpServerErrorException) e.getCause();
+                throw new ResidentServiceCheckedException(
+                        ResidentErrorCode.API_RESOURCE_ACCESS_EXCEPTION.getErrorCode(),
+                        httpServerException.getResponseBodyAsString());
+            } else {
+                throw new ResidentServiceCheckedException(
+                        ResidentErrorCode.API_RESOURCE_ACCESS_EXCEPTION.getErrorCode(),
+                        ResidentErrorCode.API_RESOURCE_ACCESS_EXCEPTION.getErrorMessage() + e.getMessage(), e);
+            }
+        }
 
-	@SuppressWarnings("unchecked")
-	public Map<String, Object> getMailingAttributes(String id) throws ResidentServiceCheckedException {
-		logger.debug(LoggerFileConstant.APPLICATIONID.toString(), LoggerFileConstant.UIN.name(), id,
-				"Utilitiy::getMailingAttributes()::entry");
-		Map<String, Object> attributes = new HashMap<>();
-		String mappingJsonString = getMappingJson();
-		if(mappingJsonString==null || mappingJsonString.trim().isEmpty()) {
-			throw new ResidentServiceException(ResidentErrorCode.JSON_PROCESSING_EXCEPTION.getErrorCode(),
-					ResidentErrorCode.JSON_PROCESSING_EXCEPTION.getErrorMessage() );
-		}
-		JSONObject mappingJsonObject;
-		try {
-			JSONObject demographicIdentity = retrieveIdrepoJson(id);
-			mappingJsonObject = JsonUtil.readValue(mappingJsonString, JSONObject.class);
-			JSONObject mapperIdentity = JsonUtil.getJSONObject(mappingJsonObject, IDENTITY);
-			List<String> mapperJsonKeys = new ArrayList<>(mapperIdentity.keySet());
-			for (String key : mapperJsonKeys) {
-				LinkedHashMap<String, String> jsonObject = JsonUtil.getJSONValue(mapperIdentity, key);
-				String values = jsonObject.get(VALUE);
-				for (String value : values.split(",")) {
-					Object object = demographicIdentity.get(value);
-					if (object instanceof ArrayList) {
-						JSONArray node = JsonUtil.getJSONArray(demographicIdentity, value);
-						JsonValue[] jsonValues = JsonUtil.mapJsonNodeToJavaObject(JsonValue.class, node);
-						for (JsonValue jsonValue : jsonValues) {
-							if (jsonValue.getLanguage().equals(primaryLang))
-								attributes.put(value + "_" + primaryLang, jsonValue.getValue());
-							if (jsonValue.getLanguage().equals(secondaryLang))
-								attributes.put(value + "_" + secondaryLang, jsonValue.getValue());
-						}
-					} else if (object instanceof LinkedHashMap) {
-						JSONObject json = JsonUtil.getJSONObject(demographicIdentity, value);
-						attributes.put(value, (String) json.get(VALUE));
-					} else {
-						attributes.put(value, String.valueOf(object));
-					}
-				}
-			}
+        return retrieveErrorCode(response, id);
+    }
 
-			logger.info(LoggerFileConstant.APPLICATIONID.toString(), LoggerFileConstant.UIN.name(), id,
-					"Utilitiy::getMailingAttributes()::mailingAttributes::" + attributes);
-		} catch (IOException | ReflectiveOperationException e) {
-			throw new ResidentServiceCheckedException(ResidentErrorCode.RESIDENT_SYS_EXCEPTION.getErrorCode(),
-					ResidentErrorCode.RESIDENT_SYS_EXCEPTION.getErrorMessage(), e);
-		}
-		logger.debug(LoggerFileConstant.APPLICATIONID.toString(), LoggerFileConstant.UIN.name(), id,
-				"Utilitiy::getMailingAttributes()::exit");
-		return attributes;
-	}
+    public JSONObject retrieveErrorCode(ResponseWrapper<IdRepoResponseDto> response, String id)
+            throws ResidentServiceCheckedException {
+        ResidentErrorCode errorCode;
+        errorCode = ResidentErrorCode.INVALID_ID;
+        try {
+            logger.info(LoggerFileConstant.APPLICATIONID.toString(), LoggerFileConstant.UIN.name(), id,
+                    "Utilitiy::retrieveIdrepoJson()::id repo response for given id::"
+                            + JsonUtil.writeValueAsString(response));
+            if (response == null)
+                throw new IdRepoAppException(errorCode.getErrorCode(), errorCode.getErrorMessage(),
+                        "In valid response while requesting ID Repositary");
+            if (!response.getErrors().isEmpty()) {
+                List<ServiceError> error = response.getErrors();
+                throw new IdRepoAppException(errorCode.getErrorCode(), errorCode.getErrorMessage(),
+                        error.get(0).getMessage());
+            }
+            String jsonResponse;
 
-	public String getMappingJson() {
-		return residentRestTemplate.getForObject(configServerFileStorageURL + getRegProcessorIdentityJson, String.class);
-	}
+            jsonResponse = JsonUtil.writeValueAsString(response.getResponse());
+            JSONObject json = JsonUtil.readValue(jsonResponse, JSONObject.class);
+            logger.debug(LoggerFileConstant.APPLICATIONID.toString(), LoggerFileConstant.UIN.name(), id,
+                    "Utilitiy::retrieveIdrepoJson()::exit");
+            return JsonUtil.getJSONObject(json, "identity");
+        } catch (IOException e) {
+            throw new ResidentServiceCheckedException(ResidentErrorCode.RESIDENT_SYS_EXCEPTION.getErrorCode(),
+                    ResidentErrorCode.RESIDENT_SYS_EXCEPTION.getErrorMessage(), e);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public Map<String, Object> getMailingAttributes(String id) throws ResidentServiceCheckedException {
+        logger.debug(LoggerFileConstant.APPLICATIONID.toString(), LoggerFileConstant.UIN.name(), id,
+                "Utilitiy::getMailingAttributes()::entry");
+        Map<String, Object> attributes = new HashMap<>();
+        String mappingJsonString = getMappingJson();
+        if (mappingJsonString == null || mappingJsonString.trim().isEmpty()) {
+            throw new ResidentServiceException(ResidentErrorCode.JSON_PROCESSING_EXCEPTION.getErrorCode(),
+                    ResidentErrorCode.JSON_PROCESSING_EXCEPTION.getErrorMessage());
+        }
+        JSONObject mappingJsonObject;
+        try {
+            JSONObject demographicIdentity = retrieveIdrepoJson(id);
+            mappingJsonObject = JsonUtil.readValue(mappingJsonString, JSONObject.class);
+            JSONObject mapperIdentity = JsonUtil.getJSONObject(mappingJsonObject, IDENTITY);
+            List<String> mapperJsonKeys = new ArrayList<>(mapperIdentity.keySet());
+            for (String key : mapperJsonKeys) {
+                LinkedHashMap<String, String> jsonObject = JsonUtil.getJSONValue(mapperIdentity, key);
+                String values = jsonObject.get(VALUE);
+                for (String value : values.split(",")) {
+                    Object object = demographicIdentity.get(value);
+                    if (object instanceof ArrayList) {
+                        JSONArray node = JsonUtil.getJSONArray(demographicIdentity, value);
+                        JsonValue[] jsonValues = JsonUtil.mapJsonNodeToJavaObject(JsonValue.class, node);
+                        for (JsonValue jsonValue : jsonValues) {
+                            if (jsonValue.getLanguage().equals(primaryLang))
+                                attributes.put(value + "_" + primaryLang, jsonValue.getValue());
+                            if (jsonValue.getLanguage().equals(secondaryLang))
+                                attributes.put(value + "_" + secondaryLang, jsonValue.getValue());
+                        }
+                    } else if (object instanceof LinkedHashMap) {
+                        JSONObject json = JsonUtil.getJSONObject(demographicIdentity, value);
+                        attributes.put(value, (String) json.get(VALUE));
+                    } else {
+                        attributes.put(value, String.valueOf(object));
+                    }
+                }
+            }
+
+            logger.info(LoggerFileConstant.APPLICATIONID.toString(), LoggerFileConstant.UIN.name(), id,
+                    "Utilitiy::getMailingAttributes()::mailingAttributes::" + attributes);
+        } catch (IOException | ReflectiveOperationException e) {
+            throw new ResidentServiceCheckedException(ResidentErrorCode.RESIDENT_SYS_EXCEPTION.getErrorCode(),
+                    ResidentErrorCode.RESIDENT_SYS_EXCEPTION.getErrorMessage(), e);
+        }
+        logger.debug(LoggerFileConstant.APPLICATIONID.toString(), LoggerFileConstant.UIN.name(), id,
+                "Utilitiy::getMailingAttributes()::exit");
+        return attributes;
+    }
+
+    public String getMappingJson() {
+        if (StringUtils.isEmpty(regProcessorIdentityJson)) {
+            return residentRestTemplate.getForObject(configServerFileStorageURL + getRegProcessorIdentityJson, String.class);
+        }
+        return regProcessorIdentityJson;
+    }
 
 }
