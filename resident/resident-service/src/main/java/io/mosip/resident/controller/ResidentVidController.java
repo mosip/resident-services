9411a1e9785037771c50e40e8be153873a071c1a
kameshsr <47484458+kameshsr@users.noreply.github.com>
2021-11-02 14:43:02 +0530

    MOSIP:15287 upgrade swagger2 to openapi3 (#160)
    * Mosip:15287 Upgraded swagger2 to openApi3
    
    Merge conflict resolved
    
    * Mosip:15287 Updated openapi properties

--- resident/resident-service/src/main/java/io/mosip/resident/controller/ResidentVidController.java
+++ resident/resident-service/src/main/java/io/mosip/resident/controller/ResidentVidController.java
@@ -14,10 +14,13 @@ import io.mosip.resident.service.ResidentVidService;
 import io.mosip.resident.util.AuditUtil;
 import io.mosip.resident.util.EventEnum;
 import io.mosip.resident.validator.RequestValidator;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.MediaType;
@@ -34,7 +37,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RefreshScope
 @RestController
-@Api(tags = "Resident Service")
+@Tag(name = "Resident Service", description = "Resident Vid Controller")
 public class ResidentVidController {
 
     Logger logger = LoggerConfiguration.logConfig(ResidentVidController.class);
@@ -44,30 +47,41 @@ public class ResidentVidController {
 
     @Autowired
     private RequestValidator validator;
-    
+
     @Autowired
     private AuditUtil auditUtil;
 
     @PostMapping(path = "/vid", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    @ApiOperation(value = "Generate new VID", response = ResidentVidRequestDto.class)
-    @ApiResponses(value = { @ApiResponse(code = 200, message = "VID successfully generated"),
-            @ApiResponse(code = 400, message = "Unable to generate VID") })
+    @Operation(summary = "generateVid", description = "generateVid", tags = { "Resident Service" })
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "VID successfully generated",
+                    content = @Content(array = @ArraySchema(schema = @Schema(implementation = ResidentVidRequestDto.class)))),
+            @ApiResponse(responseCode = "201", description = "Created" ,content = @Content(schema = @Schema(hidden = true))),
+            @ApiResponse(responseCode = "400", description = "Unable to generate VID" ,content = @Content(schema = @Schema(hidden = true))),
+            @ApiResponse(responseCode = "401", description = "Unauthorized" ,content = @Content(schema = @Schema(hidden = true))),
+            @ApiResponse(responseCode = "403", description = "Forbidden" ,content = @Content(schema = @Schema(hidden = true))),
+            @ApiResponse(responseCode = "404", description = "Not Found" ,content = @Content(schema = @Schema(hidden = true)))})
     public ResponseEntity<Object> generateVid(@RequestBody(required = true) ResidentVidRequestDto requestDto) throws OtpValidationFailedException, ResidentServiceCheckedException {
-    	auditUtil.setAuditRequestDto(EventEnum.getEventEnumWithValue(EventEnum.VALIDATE_REQUEST,"Request to generate VID"));
-    	validator.validateVidCreateRequest(requestDto);
+        auditUtil.setAuditRequestDto(EventEnum.getEventEnumWithValue(EventEnum.VALIDATE_REQUEST,"Request to generate VID"));
+        validator.validateVidCreateRequest(requestDto);
         auditUtil.setAuditRequestDto(EventEnum.getEventEnumWithValue(EventEnum.GENERATE_VID,requestDto.getRequest().getIndividualId()));
         ResponseWrapper<VidResponseDto> vidResponseDto = residentVidService.generateVid(requestDto.getRequest());
         auditUtil.setAuditRequestDto(EventEnum.getEventEnumWithValue(EventEnum.GENERATE_VID_SUCCESS,requestDto.getRequest().getIndividualId()));
         return ResponseEntity.ok().body(vidResponseDto);
     }
-    
+
     @PatchMapping(path = "/vid/{vid}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    @ApiOperation(value = "Revoke VID", response = ResponseWrapper.class)
-    @ApiResponses(value = { @ApiResponse(code = 200, message = "VID successfully revoked"),
-            @ApiResponse(code = 400, message = "Unable to revoke VID") })
+    @Operation(summary = "Revoke VID", description = "Revoke VID", tags = { "Resident Service" })
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "VID successfully revoked",
+                    content = @Content(array = @ArraySchema(schema = @Schema(implementation = ResponseWrapper.class)))),
+            @ApiResponse(responseCode = "204", description = "No Content" ,content = @Content(schema = @Schema(hidden = true))),
+            @ApiResponse(responseCode = "400", description = "Unable to revoke VID" ,content = @Content(schema = @Schema(hidden = true))),
+            @ApiResponse(responseCode = "401", description = "Unauthorized" ,content = @Content(schema = @Schema(hidden = true))),
+            @ApiResponse(responseCode = "403", description = "Forbidden" ,content = @Content(schema = @Schema(hidden = true)))})
     public ResponseEntity<Object> revokeVid(@RequestBody(required = true) RequestWrapper<VidRevokeRequestDTO> requestDto, @PathVariable String vid) throws OtpValidationFailedException, ResidentServiceCheckedException {
-    	auditUtil.setAuditRequestDto(EventEnum.getEventEnumWithValue(EventEnum.VALIDATE_REQUEST,"Request to revoke VID"));
-    	validator.validateVidRevokeRequest(requestDto);
+        auditUtil.setAuditRequestDto(EventEnum.getEventEnumWithValue(EventEnum.VALIDATE_REQUEST,"Request to revoke VID"));
+        validator.validateVidRevokeRequest(requestDto);
         auditUtil.setAuditRequestDto(EventEnum.getEventEnumWithValue(EventEnum.REVOKE_VID,requestDto.getRequest().getIndividualId()));
         ResponseWrapper<VidRevokeResponseDTO> vidResponseDto = residentVidService.revokeVid(requestDto.getRequest(),vid);
         auditUtil.setAuditRequestDto(EventEnum.getEventEnumWithValue(EventEnum.REVOKE_VID_SUCCESS,requestDto.getRequest().getIndividualId()));
