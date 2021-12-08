a5ce649b4c9768263761a0142c926082bcefdce9
Abubacker Siddik A <abuabdul86@hotmail.com>
2021-12-08 21:35:01 +0530

    MOSIP-18902: Update identity json property

--- resident/resident-service/src/main/java/io/mosip/resident/util/Utilities.java
+++ resident/resident-service/src/main/java/io/mosip/resident/util/Utilities.java
@@ -89,7 +89,7 @@ public class Utilities {
 	private String configServerFileStorageURL;
 
 	/** The get reg processor identity json. */
-	@Value("${registration-processor-identity.json}")
+	@Value("${registration.processor.identityjson}")
 	private String residentIdentityJson;
 
 	/** The id repo update. */
