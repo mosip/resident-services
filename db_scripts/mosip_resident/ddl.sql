b77a319ed3839bd29dbe1b061edd4dadf8eb2495
Manoj SP <43261486+manojsp12@users.noreply.github.com>
2022-04-11 13:03:17 +0530

    [MOSIP-21171] Added db scripts for resident services (#294)

---
+++ db_scripts/mosip_resident/ddl.sql
@@ -0,0 +1,17 @@
+-- -------------------------------------------------------------------------------------------------
+-- Database Name: mosip_resident
+-- Release Version 	: 1.2.1
+-- Purpose    		: Database scripts for Resident Service DB.       
+-- Create By   		: Manoj SP
+-- Created Date		: April-2022
+-- 
+-- Modified Date        Modified By         Comments / Remarks
+-- --------------------------------------------------------------------------------------------------
+-- April-2022			Manoj SP	    Creation of otp_transaction table.
+-----------------------------------------------------------------------------------------------------
+
+\c mosip_resident
+
+\ir ddl/otp_transaction.sql
+--\ir ddl/resident_transaction.sql
+-----------------------------------------------------------------------------------------------------
