c856e8cc4721e79d882ec4555cda6f38c68429f5
RitikJain4108 <99730411+RitikJain4108@users.noreply.github.com>
2022-04-21 20:17:31 +0530

    Mosip 21173 create api for the ida internal otp auth (#304)
    * created api for ida internal otp auth
    
    * added junit test cases for IdAuthControllerTest, ProxyMasterdataServiceTest, ResidentServiceRestClientTest
    
    * added junit test cases for masterdata apis controller and service class
    
    Co-authored-by: Ritik Jain <M1074108@mindtree.com>

--- resident/resident-service/src/test/java/io/mosip/resident/test/service/ProxyMasterdataServiceTest.java
+++ resident/resident-service/src/test/java/io/mosip/resident/test/service/ProxyMasterdataServiceTest.java
@@ -1,11 +1,12 @@
 package io.mosip.resident.test.service;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -19,7 +20,6 @@ import org.springframework.test.context.ContextConfiguration;
 
 import io.mosip.kernel.core.exception.ServiceError;
 import io.mosip.kernel.core.http.ResponseWrapper;
-import io.mosip.resident.constant.ApiName;
 import io.mosip.resident.exception.ApisResourceAccessException;
 import io.mosip.resident.exception.ResidentServiceCheckedException;
 import io.mosip.resident.service.ProxyMasterdataService;
@@ -27,6 +27,11 @@ import io.mosip.resident.service.impl.ProxyMasterdataServiceImpl;
 import io.mosip.resident.util.AuditUtil;
 import io.mosip.resident.util.ResidentServiceRestClient;
 
+/**
+ * Resident proxy masterdata service test class.
+ * 
+ * @author Ritik Jain
+ */
 @RunWith(MockitoJUnitRunner.class)
 @RefreshScope
 @ContextConfiguration
@@ -145,6 +150,167 @@ public class ProxyMasterdataServiceTest {
 			throws ApisResourceAccessException, ResidentServiceCheckedException {
 		when(residentServiceRestClient.getApi(any(), any(), any())).thenThrow(new ApisResourceAccessException());
 		proxyMasterdataService.getImmediateChildrenByLocCodeAndLangCode("MOR", "eng");
+	}
+
+	@Test
+	public void testGetLocationDetailsByLocCodeAndLangCode()
+			throws ApisResourceAccessException, ResidentServiceCheckedException {
+		when(residentServiceRestClient.getApi(any(), any(), any())).thenReturn(responseWrapper);
+		ResponseWrapper<?> result = proxyMasterdataService.getLocationDetailsByLocCodeAndLangCode("MOR", "eng");
+		assertNotNull(result);
+	}
+
+	@Test(expected = ResidentServiceCheckedException.class)
+	public void testGetLocationDetailsByLocCodeAndLangCodeIf()
+			throws ApisResourceAccessException, ResidentServiceCheckedException {
+		when(residentServiceRestClient.getApi(any(), any(), any())).thenReturn(responseWrapper);
+		ServiceError error = new ServiceError();
+		error.setErrorCode("101");
+		error.setMessage("errors");
+
+		List<ServiceError> errorList = new ArrayList<ServiceError>();
+		errorList.add(error);
+
+		responseWrapper.setErrors(errorList);
+		proxyMasterdataService.getLocationDetailsByLocCodeAndLangCode("MOR", "eng");
+	}
+
+	@Test(expected = ResidentServiceCheckedException.class)
+	public void testGetLocationDetailsByLocCodeAndLangCodeWithApisResourceAccessException()
+			throws ApisResourceAccessException, ResidentServiceCheckedException {
+		when(residentServiceRestClient.getApi(any(), any(), any())).thenThrow(new ApisResourceAccessException());
+		proxyMasterdataService.getLocationDetailsByLocCodeAndLangCode("MOR", "eng");
+	}
+
+	@Test
+	public void testGetCoordinateSpecificRegistrationCenters()
+			throws ApisResourceAccessException, ResidentServiceCheckedException {
+		when(residentServiceRestClient.getApi(any(), any(), any())).thenReturn(responseWrapper);
+		ResponseWrapper<?> result = proxyMasterdataService.getCoordinateSpecificRegistrationCenters("eng", "-6.516428",
+				"34.287879", "2000");
+		assertNotNull(result);
+	}
+
+	@Test(expected = ResidentServiceCheckedException.class)
+	public void testGetCoordinateSpecificRegistrationCentersIf()
+			throws ApisResourceAccessException, ResidentServiceCheckedException {
+		when(residentServiceRestClient.getApi(any(), any(), any())).thenReturn(responseWrapper);
+		ServiceError error = new ServiceError();
+		error.setErrorCode("101");
+		error.setMessage("errors");
+
+		List<ServiceError> errorList = new ArrayList<ServiceError>();
+		errorList.add(error);
+
+		responseWrapper.setErrors(errorList);
+		proxyMasterdataService.getCoordinateSpecificRegistrationCenters("eng", "-6.516428", "34.287879", "2000");
+	}
+
+	@Test(expected = ResidentServiceCheckedException.class)
+	public void testGetCoordinateSpecificRegistrationCentersWithApisResourceAccessException()
+			throws ApisResourceAccessException, ResidentServiceCheckedException {
+		when(residentServiceRestClient.getApi(any(), any(), any())).thenThrow(new ApisResourceAccessException());
+		proxyMasterdataService.getCoordinateSpecificRegistrationCenters("eng", "-6.516428", "34.287879", "2000");
+	}
+
+	@Test
+	public void testGetApplicantValidDocument() throws ApisResourceAccessException, ResidentServiceCheckedException {
+		when(residentServiceRestClient.getApi(any(), (Map<String, String>) any(), (List<String>) any(), any(), any()))
+				.thenReturn(responseWrapper);
+		ResponseWrapper<?> result = proxyMasterdataService.getApplicantValidDocument("002", "eng");
+		assertNotNull(result);
+	}
+
+	@Test(expected = ResidentServiceCheckedException.class)
+	public void testGetApplicantValidDocumentIf() throws ApisResourceAccessException, ResidentServiceCheckedException {
+		when(residentServiceRestClient.getApi(any(), (Map<String, String>) any(), (List<String>) any(), any(), any()))
+				.thenReturn(responseWrapper);
+		ServiceError error = new ServiceError();
+		error.setErrorCode("101");
+		error.setMessage("errors");
+
+		List<ServiceError> errorList = new ArrayList<ServiceError>();
+		errorList.add(error);
+
+		responseWrapper.setErrors(errorList);
+		proxyMasterdataService.getApplicantValidDocument("002", "eng");
+	}
+
+	@Test(expected = ResidentServiceCheckedException.class)
+	public void testGetApplicantValidDocumentWithApisResourceAccessException()
+			throws ApisResourceAccessException, ResidentServiceCheckedException {
+		when(residentServiceRestClient.getApi(any(), (Map<String, String>) any(), (List<String>) any(), any(), any()))
+				.thenThrow(new ApisResourceAccessException());
+		proxyMasterdataService.getApplicantValidDocument("002", "eng");
+	}
+
+	@Test
+	public void testGetRegistrationCentersByHierarchyLevel()
+			throws ApisResourceAccessException, ResidentServiceCheckedException {
+		when(residentServiceRestClient.getApi(any(), (Map<String, String>) any(), (List<String>) any(), any(), any()))
+				.thenReturn(responseWrapper);
+		ResponseWrapper<?> result = proxyMasterdataService.getRegistrationCentersByHierarchyLevel("ang", "5", "14110");
+		assertNotNull(result);
+	}
+
+	@Test(expected = ResidentServiceCheckedException.class)
+	public void testGetRegistrationCentersByHierarchyLevelIf()
+			throws ApisResourceAccessException, ResidentServiceCheckedException {
+		when(residentServiceRestClient.getApi(any(), (Map<String, String>) any(), (List<String>) any(), any(), any()))
+				.thenReturn(responseWrapper);
+		ServiceError error = new ServiceError();
+		error.setErrorCode("101");
+		error.setMessage("errors");
+
+		List<ServiceError> errorList = new ArrayList<ServiceError>();
+		errorList.add(error);
+
+		responseWrapper.setErrors(errorList);
+		proxyMasterdataService.getRegistrationCentersByHierarchyLevel("ang", "5", "14110");
+	}
+
+	@Test(expected = ResidentServiceCheckedException.class)
+	public void testGetRegistrationCentersByHierarchyLevelWithApisResourceAccessException()
+			throws ApisResourceAccessException, ResidentServiceCheckedException {
+		when(residentServiceRestClient.getApi(any(), (Map<String, String>) any(), (List<String>) any(), any(), any()))
+				.thenThrow(new ApisResourceAccessException());
+		proxyMasterdataService.getRegistrationCentersByHierarchyLevel("ang", "5", "14110");
+	}
+
+	@Test
+	public void testGetRegistrationCenterByHierarchyLevelAndTextPaginated()
+			throws ApisResourceAccessException, ResidentServiceCheckedException {
+		when(residentServiceRestClient.getApi(any(), (Map<String, String>) any(), (List<String>) any(), any(), any()))
+				.thenReturn(responseWrapper);
+		ResponseWrapper<?> result = proxyMasterdataService.getRegistrationCenterByHierarchyLevelAndTextPaginated("eng",
+				"5", "14110", "0", "10", "desc", "createdDateTime");
+		assertNotNull(result);
+	}
+
+	@Test(expected = ResidentServiceCheckedException.class)
+	public void testGetRegistrationCenterByHierarchyLevelAndTextPaginatedIf()
+			throws ApisResourceAccessException, ResidentServiceCheckedException {
+		when(residentServiceRestClient.getApi(any(), (Map<String, String>) any(), (List<String>) any(), any(), any()))
+				.thenReturn(responseWrapper);
+		ServiceError error = new ServiceError();
+		error.setErrorCode("101");
+		error.setMessage("errors");
+
+		List<ServiceError> errorList = new ArrayList<ServiceError>();
+		errorList.add(error);
+
+		responseWrapper.setErrors(errorList);
+		proxyMasterdataService.getRegistrationCenterByHierarchyLevelAndTextPaginated("eng", "5", "14110", "0", "10",
+				"desc", "createdDateTime");
+	}
+
+	@Test(expected = ResidentServiceCheckedException.class)
+	public void testGetRegistrationCenterByHierarchyLevelAndTextPaginatedWithApisResourceAccessException()
+			throws ApisResourceAccessException, ResidentServiceCheckedException {
+		when(residentServiceRestClient.getApi(any(), (Map<String, String>) any(), (List<String>) any(), any(), any()))
+				.thenThrow(new ApisResourceAccessException());
+		proxyMasterdataService.getRegistrationCenterByHierarchyLevelAndTextPaginated("eng", "5", "14110", "0", "10",
+				"desc", "createdDateTime");
 	}
 
 }
