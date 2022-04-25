97f698522e1b9351d605077233e10e6eab65c676
kameshsr <kameshsr1338@gmail.com>
2022-04-25 11:51:28 +0530

    MOSIP-21171: Added resident_transaction.sql.

--- db_release_scripts/mosip_resident/sql/1.2.0_resident-scripts_revoke.sql
+++ db_release_scripts/mosip_resident/sql/1.2.0_resident-scripts_revoke.sql
@@ -5,12 +5,13 @@
 -- Create By   		: Manoj SP
 -- Created Date		: April-2022
 -- 
--- Modified Date        Modified By         Comments / Remarks
+-- Modified Date        Modified By             Comments / Remarks
 -- --------------------------------------------------------------------------------------------------
--- April-2022			Manoj SP	    Added otp_transaction table creation scripts with comments.
+-- April-2022			Manoj SP	            Added otp_transaction table creation scripts with comments.
+-- April-2022           Kamesh Shekhar Prasad   Added resident_transaction table creation scripts with comments.
 -----------------------------------------------------------------------------------------------------
 \c mosip_resident sysadmin
 
-DROP TABLE IF EXISTS idrepo.otp_transaction;
--- DROP TABLE IF EXISTS idrepo.resident_transaction;
+DROP TABLE IF EXISTS resident.otp_transaction;
+DROP TABLE IF EXISTS resident.resident_transaction;
 -----------------------------------------------------------------------------------------------------
