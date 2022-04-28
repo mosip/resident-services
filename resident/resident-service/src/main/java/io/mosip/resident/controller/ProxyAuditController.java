729cc6efe3b2fcf62692aaf1e358825fd4ec97db
Loganathan Sekar <42532387+LoganathanSekar7627@users.noreply.github.com>
2022-04-28 15:30:53 +0530

    MOSIP-21378 proxy api for audit log (#310)
    * Added api get resident ui properties
    
    * Added audit log proxy
    
    * Added audit log proxy

--- resident/resident-service/src/main/java/io/mosip/resident/controller/ProxyAuditController.java
+++ resident/resident-service/src/main/java/io/mosip/resident/controller/ProxyAuditController.java
@@ -1,18 +1,19 @@
 package io.mosip.resident.controller;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.mosip.kernel.core.http.ResponseFilter;
 import io.mosip.kernel.core.http.ResponseWrapper;
-import io.mosip.kernel.core.logger.spi.Logger;
-import io.mosip.resident.config.LoggerConfiguration;
+import io.mosip.resident.dto.AuditRequestDTO;
 import io.mosip.resident.exception.ResidentServiceCheckedException;
-import io.mosip.resident.handler.service.ResidentConfigService;
 import io.mosip.resident.util.AuditUtil;
-import io.mosip.resident.util.EventEnum;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -20,49 +21,28 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
-
-/**
- * The Class ProxyConfigController.
- * 
- * @author Loganathan. S
- */
 @RestController
-@RequestMapping("/proxy/config")
-@Tag(name = "proxy-config-controller", description = "Proxy Config Controller")
-public class ProxyConfigController {
+@RequestMapping("/proxy/audit")
+@Tag(name = "proxy-audit-controller", description = "Proxy Audit Controller")
+public class ProxyAuditController {
 	
-	/** The Constant logger. */
-	private static final Logger logger = LoggerConfiguration.logConfig(ProxyConfigController.class);
-
-	/** The audit util. */
 	@Autowired
 	private AuditUtil auditUtil;
 	
-	@Autowired
-	private ResidentConfigService residentConfigService;
-
-	/**
-	 * Gets the resident properties.
-	 *
-	 * @return the resident properties
-	 * @throws ResidentServiceCheckedException the resident service checked exception
-	 */
 	@ResponseFilter
-	@GetMapping("/ui-properties")
-	@Operation(summary = "getResidentUIProperties", description = "Get the Resident-UI properties", tags = {
-			"proxy-config-controller" })
+	@GetMapping("/log")
+	@Operation(summary = "getImmediateChildrenByLocCodeAndLangCode", description = "getImmediateChildrenByLocCodeAndLangCode", tags = {
+			"proxy-masterdata-controller" })
 	@ApiResponses(value = { @ApiResponse(responseCode = "200", description = "OK"),
 			@ApiResponse(responseCode = "201", description = "Created", content = @Content(schema = @Schema(hidden = true))),
 			@ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content(schema = @Schema(hidden = true))),
 			@ApiResponse(responseCode = "403", description = "Forbidden", content = @Content(schema = @Schema(hidden = true))),
 			@ApiResponse(responseCode = "404", description = "Not Found", content = @Content(schema = @Schema(hidden = true))) })
-	public ResponseWrapper<?> getResidentProperties()
+	public ResponseEntity<?> getImmediateChildrenByLocCodeAndLangCode(
+			@RequestBody AuditRequestDTO auditRequestDto)
 			throws ResidentServiceCheckedException {
-		logger.debug("ProxyConfigController::getResidentProperties()::entry");
-		auditUtil.setAuditRequestDto(EventEnum.GET_CONFIGURATION_PROPERTIES);
-		ResponseWrapper<?> propertiesResponse = residentConfigService.getUIProperties();
-		auditUtil.setAuditRequestDto(EventEnum.GET_CONFIGURATION_PROPERTIES_SUCCESS);
-		logger.debug("ProxyConfigController::getResidentProperties()::exit");
-		return propertiesResponse;
+		auditUtil.callAuditManager(auditRequestDto);
+		return ResponseEntity.ok().build();
 	}
+
 }
