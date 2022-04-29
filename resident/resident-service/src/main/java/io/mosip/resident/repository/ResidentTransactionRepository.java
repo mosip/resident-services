0ae0de8d63b0d9efe2bb9ae3a39745618b5b072f
kameshsr <47484458+kameshsr@users.noreply.github.com>
2022-04-29 15:48:04 +0530

    MOSIP-21490: added entry to resident_transaction table. (#308)
    * MOSIP-21171: Added resident_transaction.sql.
    
    * MOSIP-21171: Added resident_transaction.sql.
    
    * MOSIP-21202: Added unique constraint for request_trn_id.
    
    * MOSIP-21490: added entry to resident_transaction table.
    
    * MOSIP-21490: Fixed test case failure.
    
    * MOSIP-21490: Removed IdRepoServiceImpl.java.
    
    * MOSIP-21490: Added insertData method.
    
    * MOSIP-21490: Added error logs.
    
    * MOSIP-21490: Fixed test case failure.
    
    * MOSIP-21490: Fixed test case failure.

--- resident/resident-service/src/main/java/io/mosip/resident/repository/ResidentTransactionRepository.java
+++ resident/resident-service/src/main/java/io/mosip/resident/repository/ResidentTransactionRepository.java
@@ -2,12 +2,14 @@ package io.mosip.resident.repository;
 
 import io.mosip.resident.entity.ResidentTransactionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * The Interface ResidentTransactionRepository.
  * @author Kamesh Shekhar Prasad.
  * @since 1.2.0.1
  */
+@Repository
 public interface ResidentTransactionRepository extends JpaRepository<ResidentTransactionEntity, String> {
     public ResidentTransactionEntity findByRequestTrnId(String requestTrnId);
 }
