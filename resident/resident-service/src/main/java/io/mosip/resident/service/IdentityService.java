4327a01b1716565913e730a7dfee49c064e9dbfe
kameshsr <kameshsr1338@gmail.com>
2022-04-29 16:22:35 +0530

    MOSIP-21544: moved getIdaToken method to identityservice.

--- resident/resident-service/src/main/java/io/mosip/resident/service/IdentityService.java
+++ resident/resident-service/src/main/java/io/mosip/resident/service/IdentityService.java
@@ -21,5 +21,5 @@ public interface IdentityService {
 	 * @throws ResidentServiceCheckedException
 	 */
 	public IdentityDTO getIdentity(String id) throws ResidentServiceCheckedException;
-
+	public String getIdaToken(String uin);
 }
