541fcb31e9fc19270d27549da2499ea394c1ff31
Loganathan Sekar <loganathan.sekar@mindtree.com>
2022-05-04 19:20:56 +0530

    Added support for jdbc with kernel-dao config

--- resident/resident-service/src/main/java/io/mosip/resident/ResidentBootApplication.java
+++ resident/resident-service/src/main/java/io/mosip/resident/ResidentBootApplication.java
@@ -3,7 +3,7 @@ package io.mosip.resident;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = { "io.mosip.resident.*", "io.mosip.kernel.core.*",
+@SpringBootApplication(scanBasePackages = { "io.mosip.resident.*", "io.mosip.kernel.core.*", "io.mosip.kernel.dataaccess.hibernate.*",
 		"io.mosip.kernel.crypto.jce.*", "io.mosip.commons.packet.*", "io.mosip.kernel.keygenerator.bouncycastle.*",
 		"${mosip.auth.adapter.impl.basepackage}" })
 public class ResidentBootApplication {
