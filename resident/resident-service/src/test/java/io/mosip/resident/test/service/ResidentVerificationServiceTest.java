cd7b90146ac063114dd2d258e18a899cbe96076e
kameshsr <kameshsr1338@gmail.com>
2022-05-04 10:26:25 +0530

    MOSIP-21544: Added ResidentVerificationServiceTest.java

---
+++ resident/resident-service/src/test/java/io/mosip/resident/test/service/ResidentVerificationServiceTest.java
@@ -0,0 +1,70 @@
+package io.mosip.resident.test.service;
+
+import io.mosip.resident.dto.VerificationResponseDTO;
+import io.mosip.resident.dto.VerificationStatusDTO;
+import io.mosip.resident.exception.ApisResourceAccessException;
+import io.mosip.resident.exception.ResidentServiceCheckedException;
+import io.mosip.resident.repository.ResidentTransactionRepository;
+import io.mosip.resident.service.VerificationService;
+import io.mosip.resident.service.impl.IdentityServiceImpl;
+import io.mosip.resident.service.impl.VerificationServiceImpl;
+import io.mosip.resident.util.AuditUtil;
+import io.mosip.resident.util.ResidentServiceRestClient;
+import org.joda.time.DateTime;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.ContextConfiguration;
+
+import java.security.NoSuchAlgorithmException;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(MockitoJUnitRunner.class)
+@RefreshScope
+@ContextConfiguration
+public class ResidentVerificationServiceTest {
+
+    @Mock
+    private ResidentServiceRestClient residentServiceRestClient;
+
+    @Mock
+    Environment env;
+
+    @Mock
+    private AuditUtil audit;
+
+    @InjectMocks
+    private VerificationService verificationService = new VerificationServiceImpl();
+
+    @Mock
+    private IdentityServiceImpl identityServiceImpl;
+
+    @Mock
+    private ResidentTransactionRepository residentTransactionRepository;
+
+    @Before
+    public void setup() {
+    }
+
+    @Test
+    public void testCheckChannelVerificationStatus() throws ResidentServiceCheckedException, NoSuchAlgorithmException, ApisResourceAccessException {
+
+        boolean verificationStatus = false;
+        VerificationStatusDTO verificationStatusDTO = new VerificationStatusDTO();
+        VerificationResponseDTO verificationResponseDTO = new VerificationResponseDTO();
+        verificationStatusDTO.setVerificationStatus(verificationStatus);
+        verificationResponseDTO.setResponse(verificationStatusDTO);
+        verificationResponseDTO.setId("mosip.resident.channel.verification.status");
+        verificationResponseDTO.setVersion("v1");
+        verificationResponseDTO.setResponseTime(DateTime.now().toString());
+
+        VerificationResponseDTO verificationResponseDTO1 =verificationService.checkChannelVerificationStatus("email", "8251649601");
+        assertEquals(verificationResponseDTO.getResponse().isVerificationStatus(), verificationResponseDTO1.getResponse().isVerificationStatus());
+    }
+}
