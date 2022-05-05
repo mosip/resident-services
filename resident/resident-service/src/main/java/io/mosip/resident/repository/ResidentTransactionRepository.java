6242044f1ccf768d70f9f18090c07df3f1a9ee14
Loganathan Sekar <42532387+LoganathanSekar7627@users.noreply.github.com>
2022-05-05 21:10:22 +0530

    Fix verification update logic (#321)

--- resident/resident-service/src/main/java/io/mosip/resident/repository/ResidentTransactionRepository.java
+++ resident/resident-service/src/main/java/io/mosip/resident/repository/ResidentTransactionRepository.java
@@ -1,10 +1,11 @@
 package io.mosip.resident.repository;
 
-import io.mosip.resident.entity.ResidentTransactionEntity;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import io.mosip.resident.entity.ResidentTransactionEntity;
 
 /**
  * The Interface ResidentTransactionRepository.
@@ -13,6 +14,6 @@ import java.util.List;
  */
 @Repository
 public interface ResidentTransactionRepository extends JpaRepository<ResidentTransactionEntity, String> {
-    public List<ResidentTransactionEntity> findByRequestTrnIdAndRefIdOrderByCrDtimes(String requestTrnId, String refId);
+    public List<ResidentTransactionEntity> findByRequestTrnIdAndRefIdOrderByCrDtimesDesc(String requestTrnId, String refId);
     public ResidentTransactionEntity findByAid(String aid);
 }
