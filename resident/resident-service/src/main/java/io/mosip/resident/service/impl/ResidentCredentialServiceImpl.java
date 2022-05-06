1398572b9a6589be3d521fdbc774f3b86d548975
Loganathan Sekar <loganathan.sekar@mindtree.com>
2022-05-06 19:20:17 +0530

    Fix import

--- resident/resident-service/src/main/java/io/mosip/resident/service/impl/ResidentCredentialServiceImpl.java
+++ resident/resident-service/src/main/java/io/mosip/resident/service/impl/ResidentCredentialServiceImpl.java
@@ -20,6 +20,7 @@ import org.springframework.stereotype.Service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import io.mosip.kernel.core.exception.ExceptionUtils;
 import io.mosip.kernel.core.logger.spi.Logger;
 import io.mosip.kernel.core.util.CryptoUtil;
 import io.mosip.kernel.core.util.DateUtils;
