114bff4ef571159110b2dccfd7905f63eaec8c63
Manoj SP <Manoj.SP@mindtree.com>
2022-05-09 23:15:16 +0530

    [MOSIP-20577] Added get policy api and added api without otp

--- resident/resident-service/src/main/java/io/mosip/resident/controller/ResidentVidController.java
+++ resident/resident-service/src/main/java/io/mosip/resident/controller/ResidentVidController.java
@@ -39,6 +39,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 
 /**
  * Resident VID controller class.
+ * 
  * @Author : Monobikash Das
  */
 @RefreshScope
@@ -46,25 +47,24 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 @Tag(name = "Resident Service", description = "Resident Vid Controller")
 public class ResidentVidController {
 
-    Logger logger = LoggerConfiguration.logConfig(ResidentVidController.class);
+	Logger logger = LoggerConfiguration.logConfig(ResidentVidController.class);
 
-    @Autowired
-    private ResidentVidService residentVidService;
+	@Autowired
+	private ResidentVidService residentVidService;
 
-    @Autowired
-    private RequestValidator validator;
+	@Autowired
+	private RequestValidator validator;
 
-    @Autowired
-    private AuditUtil auditUtil;
-    
+	@Autowired
+	private AuditUtil auditUtil;
+
 	@GetMapping(path = "/vid/policy")
-    @Operation(summary = "Retrieve VID policy", description = "Retrieve VID policy", tags = { "Resident Service" })
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "VID Policy retrieved successfully",
-                    content = @Content(array = @ArraySchema(schema = @Schema(implementation = ResidentVidRequestDto.class)))),
-            @ApiResponse(responseCode = "401", description = "Unauthorized" ,content = @Content(schema = @Schema(hidden = true))),
-            @ApiResponse(responseCode = "403", description = "Forbidden" ,content = @Content(schema = @Schema(hidden = true))),
-            @ApiResponse(responseCode = "404", description = "Not Found" ,content = @Content(schema = @Schema(hidden = true)))})
+	@Operation(summary = "Retrieve VID policy", description = "Retrieve VID policy", tags = { "Resident Service" })
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "VID Policy retrieved successfully", content = @Content(array = @ArraySchema(schema = @Schema(implementation = ResidentVidRequestDto.class)))),
+			@ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content(schema = @Schema(hidden = true))),
+			@ApiResponse(responseCode = "403", description = "Forbidden", content = @Content(schema = @Schema(hidden = true))),
+			@ApiResponse(responseCode = "404", description = "Not Found", content = @Content(schema = @Schema(hidden = true))) })
 	public ResponseEntity<ResponseWrapper<String>> getVidPolicy() {
 		ResponseWrapper<String> response = new ResponseWrapper<>();
 		try {
@@ -76,79 +76,88 @@ public class ResidentVidController {
 		return ResponseEntity.ok().body(response);
 	}
 
-    @PostMapping(path = "/vid", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    @Operation(summary = "generateVid", description = "generateVid", tags = { "Resident Service" })
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "VID successfully generated",
-                    content = @Content(array = @ArraySchema(schema = @Schema(implementation = ResidentVidRequestDto.class)))),
-            @ApiResponse(responseCode = "201", description = "Created" ,content = @Content(schema = @Schema(hidden = true))),
-            @ApiResponse(responseCode = "400", description = "Unable to generate VID" ,content = @Content(schema = @Schema(hidden = true))),
-            @ApiResponse(responseCode = "401", description = "Unauthorized" ,content = @Content(schema = @Schema(hidden = true))),
-            @ApiResponse(responseCode = "403", description = "Forbidden" ,content = @Content(schema = @Schema(hidden = true))),
-            @ApiResponse(responseCode = "404", description = "Not Found" ,content = @Content(schema = @Schema(hidden = true)))})
-    public ResponseEntity<Object> generateVid(@RequestBody(required = true) ResidentVidRequestDto requestDto) throws OtpValidationFailedException, ResidentServiceCheckedException {
-        return generateVid(requestDto, true);
-    }
-    
-    @PostMapping(path = "/generate-vid", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    @Operation(summary = "generateVid", description = "generateVid", tags = { "Resident Service" })
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "VID successfully generated",
-                    content = @Content(array = @ArraySchema(schema = @Schema(implementation = ResidentVidRequestDto.class)))),
-            @ApiResponse(responseCode = "201", description = "Created" ,content = @Content(schema = @Schema(hidden = true))),
-            @ApiResponse(responseCode = "400", description = "Unable to generate VID" ,content = @Content(schema = @Schema(hidden = true))),
-            @ApiResponse(responseCode = "401", description = "Unauthorized" ,content = @Content(schema = @Schema(hidden = true))),
-            @ApiResponse(responseCode = "403", description = "Forbidden" ,content = @Content(schema = @Schema(hidden = true))),
-            @ApiResponse(responseCode = "404", description = "Not Found" ,content = @Content(schema = @Schema(hidden = true)))})
-    public ResponseEntity<Object> generateVidWithoutOTP(@RequestBody(required = true) ResidentVidRequestDto requestDto) throws ResidentServiceCheckedException, OtpValidationFailedException {
-    	requestDto.getRequest().setOtp(null);
-    	return generateVid(requestDto, false);
-    }
+	@PostMapping(path = "/vid", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	@Operation(summary = "generateVid", description = "generateVid", tags = { "Resident Service" })
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "VID successfully generated", content = @Content(array = @ArraySchema(schema = @Schema(implementation = ResidentVidRequestDto.class)))),
+			@ApiResponse(responseCode = "201", description = "Created", content = @Content(schema = @Schema(hidden = true))),
+			@ApiResponse(responseCode = "400", description = "Unable to generate VID", content = @Content(schema = @Schema(hidden = true))),
+			@ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content(schema = @Schema(hidden = true))),
+			@ApiResponse(responseCode = "403", description = "Forbidden", content = @Content(schema = @Schema(hidden = true))),
+			@ApiResponse(responseCode = "404", description = "Not Found", content = @Content(schema = @Schema(hidden = true))) })
+	public ResponseEntity<Object> generateVid(@RequestBody(required = true) ResidentVidRequestDto requestDto)
+			throws OtpValidationFailedException, ResidentServiceCheckedException {
+		return generateVid(requestDto, true);
+	}
+
+	@PostMapping(path = "/generate-vid", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	@Operation(summary = "generateVid", description = "generateVid", tags = { "Resident Service" })
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "VID successfully generated", content = @Content(array = @ArraySchema(schema = @Schema(implementation = ResidentVidRequestDto.class)))),
+			@ApiResponse(responseCode = "201", description = "Created", content = @Content(schema = @Schema(hidden = true))),
+			@ApiResponse(responseCode = "400", description = "Unable to generate VID", content = @Content(schema = @Schema(hidden = true))),
+			@ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content(schema = @Schema(hidden = true))),
+			@ApiResponse(responseCode = "403", description = "Forbidden", content = @Content(schema = @Schema(hidden = true))),
+			@ApiResponse(responseCode = "404", description = "Not Found", content = @Content(schema = @Schema(hidden = true))) })
+	public ResponseEntity<Object> generateVidWithoutOTP(@RequestBody(required = true) ResidentVidRequestDto requestDto)
+			throws ResidentServiceCheckedException, OtpValidationFailedException {
+		requestDto.getRequest().setOtp(null);
+		return generateVid(requestDto, false);
+	}
 
 	private ResponseEntity<Object> generateVid(ResidentVidRequestDto requestDto, boolean isOtpValidationRequired)
 			throws OtpValidationFailedException, ResidentServiceCheckedException {
-		auditUtil.setAuditRequestDto(EventEnum.getEventEnumWithValue(EventEnum.VALIDATE_REQUEST,"Request to generate VID"));
-        validator.validateVidCreateRequest(requestDto, isOtpValidationRequired);
-        auditUtil.setAuditRequestDto(EventEnum.getEventEnumWithValue(EventEnum.GENERATE_VID,requestDto.getRequest().getIndividualId()));
-        ResponseWrapper<VidResponseDto> vidResponseDto = residentVidService.generateVid(requestDto.getRequest());
-        auditUtil.setAuditRequestDto(EventEnum.getEventEnumWithValue(EventEnum.GENERATE_VID_SUCCESS,requestDto.getRequest().getIndividualId()));
-        return ResponseEntity.ok().body(vidResponseDto);
+		auditUtil.setAuditRequestDto(
+				EventEnum.getEventEnumWithValue(EventEnum.VALIDATE_REQUEST, "Request to generate VID"));
+		validator.validateVidCreateRequest(requestDto, isOtpValidationRequired);
+		auditUtil.setAuditRequestDto(
+				EventEnum.getEventEnumWithValue(EventEnum.GENERATE_VID, requestDto.getRequest().getIndividualId()));
+		ResponseWrapper<VidResponseDto> vidResponseDto = residentVidService.generateVid(requestDto.getRequest());
+		auditUtil.setAuditRequestDto(EventEnum.getEventEnumWithValue(EventEnum.GENERATE_VID_SUCCESS,
+				requestDto.getRequest().getIndividualId()));
+		return ResponseEntity.ok().body(vidResponseDto);
 	}
 
-    @PatchMapping(path = "/vid/{vid}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    @Operation(summary = "Revoke VID", description = "Revoke VID", tags = { "Resident Service" })
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "VID successfully revoked",
-                    content = @Content(array = @ArraySchema(schema = @Schema(implementation = ResponseWrapper.class)))),
-            @ApiResponse(responseCode = "204", description = "No Content" ,content = @Content(schema = @Schema(hidden = true))),
-            @ApiResponse(responseCode = "400", description = "Unable to revoke VID" ,content = @Content(schema = @Schema(hidden = true))),
-            @ApiResponse(responseCode = "401", description = "Unauthorized" ,content = @Content(schema = @Schema(hidden = true))),
-            @ApiResponse(responseCode = "403", description = "Forbidden" ,content = @Content(schema = @Schema(hidden = true)))})
-    public ResponseEntity<Object> revokeVid(@RequestBody(required = true) RequestWrapper<VidRevokeRequestDTO> requestDto, @PathVariable String vid) throws OtpValidationFailedException, ResidentServiceCheckedException {
-        return revokeVid(requestDto, vid, true);
-    }
-    
-    @PatchMapping(path = "/revoke-vid/{vid}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    @Operation(summary = "Revoke VID", description = "Revoke VID", tags = { "Resident Service" })
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "VID successfully revoked",
-                    content = @Content(array = @ArraySchema(schema = @Schema(implementation = ResponseWrapper.class)))),
-            @ApiResponse(responseCode = "204", description = "No Content" ,content = @Content(schema = @Schema(hidden = true))),
-            @ApiResponse(responseCode = "400", description = "Unable to revoke VID" ,content = @Content(schema = @Schema(hidden = true))),
-            @ApiResponse(responseCode = "401", description = "Unauthorized" ,content = @Content(schema = @Schema(hidden = true))),
-            @ApiResponse(responseCode = "403", description = "Forbidden" ,content = @Content(schema = @Schema(hidden = true)))})
-    public ResponseEntity<Object> revokeVidWithoutOTP(@RequestBody(required = true) RequestWrapper<VidRevokeRequestDTO> requestDto, @PathVariable String vid) throws OtpValidationFailedException, ResidentServiceCheckedException {
-    	requestDto.getRequest().setOtp(null); 
-    	return revokeVid(requestDto, vid, false);
-    }
-
-	private ResponseEntity<Object> revokeVid(RequestWrapper<VidRevokeRequestDTO> requestDto, String vid, boolean isOtpValidationRequired)
+	@PatchMapping(path = "/vid/{vid}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	@Operation(summary = "Revoke VID", description = "Revoke VID", tags = { "Resident Service" })
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "VID successfully revoked", content = @Content(array = @ArraySchema(schema = @Schema(implementation = ResponseWrapper.class)))),
+			@ApiResponse(responseCode = "204", description = "No Content", content = @Content(schema = @Schema(hidden = true))),
+			@ApiResponse(responseCode = "400", description = "Unable to revoke VID", content = @Content(schema = @Schema(hidden = true))),
+			@ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content(schema = @Schema(hidden = true))),
+			@ApiResponse(responseCode = "403", description = "Forbidden", content = @Content(schema = @Schema(hidden = true))) })
+	public ResponseEntity<Object> revokeVid(
+			@RequestBody(required = true) RequestWrapper<VidRevokeRequestDTO> requestDto, @PathVariable String vid)
 			throws OtpValidationFailedException, ResidentServiceCheckedException {
-		auditUtil.setAuditRequestDto(EventEnum.getEventEnumWithValue(EventEnum.VALIDATE_REQUEST,"Request to revoke VID"));
-        validator.validateVidRevokeRequest(requestDto, isOtpValidationRequired);
-        auditUtil.setAuditRequestDto(EventEnum.getEventEnumWithValue(EventEnum.REVOKE_VID,requestDto.getRequest().getIndividualId()));
-        ResponseWrapper<VidRevokeResponseDTO> vidResponseDto = residentVidService.revokeVid(requestDto.getRequest(),vid);
-        auditUtil.setAuditRequestDto(EventEnum.getEventEnumWithValue(EventEnum.REVOKE_VID_SUCCESS,requestDto.getRequest().getIndividualId()));
-        return ResponseEntity.ok().body(vidResponseDto);
+		return revokeVid(requestDto, vid, true);
+	}
+
+	@PatchMapping(path = "/revoke-vid/{vid}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	@Operation(summary = "Revoke VID", description = "Revoke VID", tags = { "Resident Service" })
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "VID successfully revoked", content = @Content(array = @ArraySchema(schema = @Schema(implementation = ResponseWrapper.class)))),
+			@ApiResponse(responseCode = "204", description = "No Content", content = @Content(schema = @Schema(hidden = true))),
+			@ApiResponse(responseCode = "400", description = "Unable to revoke VID", content = @Content(schema = @Schema(hidden = true))),
+			@ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content(schema = @Schema(hidden = true))),
+			@ApiResponse(responseCode = "403", description = "Forbidden", content = @Content(schema = @Schema(hidden = true))) })
+	public ResponseEntity<Object> revokeVidWithoutOTP(
+			@RequestBody(required = true) RequestWrapper<VidRevokeRequestDTO> requestDto, @PathVariable String vid)
+			throws OtpValidationFailedException, ResidentServiceCheckedException {
+		requestDto.getRequest().setOtp(null);
+		return revokeVid(requestDto, vid, false);
+	}
+
+	private ResponseEntity<Object> revokeVid(RequestWrapper<VidRevokeRequestDTO> requestDto, String vid,
+			boolean isOtpValidationRequired) throws OtpValidationFailedException, ResidentServiceCheckedException {
+		auditUtil.setAuditRequestDto(
+				EventEnum.getEventEnumWithValue(EventEnum.VALIDATE_REQUEST, "Request to revoke VID"));
+		validator.validateVidRevokeRequest(requestDto, isOtpValidationRequired);
+		auditUtil.setAuditRequestDto(
+				EventEnum.getEventEnumWithValue(EventEnum.REVOKE_VID, requestDto.getRequest().getIndividualId()));
+		ResponseWrapper<VidRevokeResponseDTO> vidResponseDto = residentVidService.revokeVid(requestDto.getRequest(),
+				vid);
+		auditUtil.setAuditRequestDto(EventEnum.getEventEnumWithValue(EventEnum.REVOKE_VID_SUCCESS,
+				requestDto.getRequest().getIndividualId()));
+		return ResponseEntity.ok().body(vidResponseDto);
 	}
 }
