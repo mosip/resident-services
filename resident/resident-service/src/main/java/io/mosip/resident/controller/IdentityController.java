cbebb33577d52a83f83ed3a890e715105c6503d1
Loganathan Sekar <42532387+LoganathanSekar7627@users.noreply.github.com>
2022-05-12 14:34:40 +0530

    Mosip 21144 secure api (#338)
    * Adding get identity attributes API- WIP
    
    * Added Security for input-identity-attributes API
    
    * Fix compilation issue
    
    * Test fix
    
    * Test fix
    
    * Added missing files

---
+++ resident/resident-service/src/main/java/io/mosip/resident/controller/IdentityController.java
@@ -0,0 +1,69 @@
+package io.mosip.resident.controller;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.mosip.kernel.core.http.ResponseFilter;
+import io.mosip.kernel.core.http.ResponseWrapper;
+import io.mosip.kernel.core.logger.spi.Logger;
+import io.mosip.resident.config.LoggerConfiguration;
+import io.mosip.resident.exception.ApisResourceAccessException;
+import io.mosip.resident.exception.ResidentServiceCheckedException;
+import io.mosip.resident.service.impl.IdentityServiceImpl;
+import io.mosip.resident.util.AuditUtil;
+import io.mosip.resident.util.EventEnum;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@RestController
+@RequestMapping("/identity")
+@Tag(name = "identity-controller", description = "IdentityController Controller")
+public class IdentityController {
+	
+	/** The Constant logger. */
+	private static final Logger logger = LoggerConfiguration.logConfig(IdentityController.class);
+
+	/** The audit util. */
+	@Autowired
+	private AuditUtil auditUtil;
+	
+	@Autowired
+	private IdentityServiceImpl idServiceImpl;
+	
+	@ResponseFilter
+	@PreAuthorize("hasAnyRole(@authorizedRoles.getGetinputattributevalues())")
+	@GetMapping("/input-attributes/values")
+	@Operation(summary = "getInputAttributeValues", description = "Get the Resident-UI Schema", tags = {
+			"proxy-config-controller" })
+	@ApiResponses(value = { @ApiResponse(responseCode = "200", description = "OK"),
+			@ApiResponse(responseCode = "201", description = "Created", content = @Content(schema = @Schema(hidden = true))),
+			@ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content(schema = @Schema(hidden = true))),
+			@ApiResponse(responseCode = "403", description = "Forbidden", content = @Content(schema = @Schema(hidden = true))),
+			@ApiResponse(responseCode = "404", description = "Not Found", content = @Content(schema = @Schema(hidden = true))) })
+	public ResponseWrapper<Object> getInputAttributeValues()
+			throws ResidentServiceCheckedException, ApisResourceAccessException {
+		logger.debug("IdentityController::getResidentUISchema()::entry");
+		auditUtil.setAuditRequestDto(EventEnum.GET_CONFIGURATION_PROPERTIES);
+		String id = getIdFromUser();
+		Map<String, ?> propertiesResponse = idServiceImpl.getIdentityAttributes(id);
+		auditUtil.setAuditRequestDto(EventEnum.GET_CONFIGURATION_PROPERTIES_SUCCESS);
+		logger.debug("IdentityController::getResidentUISchema()::exit");
+		ResponseWrapper<Object> responseWrapper = new ResponseWrapper<>();
+		responseWrapper.setResponse(propertiesResponse);
+		return responseWrapper;
+	}
+
+	private String getIdFromUser() throws ApisResourceAccessException {
+		return idServiceImpl.getResidentIndvidualId();
+	}
+
+}
