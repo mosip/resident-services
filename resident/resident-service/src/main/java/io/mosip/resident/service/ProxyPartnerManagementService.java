be8bc8418d1e0b4f2262f768bb98fe169b603af2
Ritik Jain <M1074108@mindtree.com>
2022-05-23 17:06:51 +0530

    created proxy api to get partners list from pms by partner type

---
+++ resident/resident-service/src/main/java/io/mosip/resident/service/ProxyPartnerManagementService.java
@@ -0,0 +1,28 @@
+package io.mosip.resident.service;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import io.mosip.kernel.core.http.ResponseWrapper;
+import io.mosip.resident.exception.ResidentServiceCheckedException;
+
+/**
+ * Resident proxy partner management service class.
+ * 
+ * @author Ritik Jain
+ */
+@Service
+public interface ProxyPartnerManagementService {
+
+	/**
+	 * Get partners by partner type.
+	 * 
+	 * @param partnerType
+	 * @return ResponseWrapper object
+	 * @throws ResidentServiceCheckedException
+	 */
+	public ResponseWrapper<?> getPartnersByPartnerType(Optional<String> partnerType)
+			throws ResidentServiceCheckedException;
+
+}
