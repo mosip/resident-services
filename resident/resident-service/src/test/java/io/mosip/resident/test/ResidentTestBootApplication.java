a22bb87e0106a8dd15869b4168cf94a5401df242
Loganathan Sekar <loganathan.sekar@mindtree.com>
2022-05-31 01:02:48 +0530

    Test fix

--- resident/resident-service/src/test/java/io/mosip/resident/test/ResidentTestBootApplication.java
+++ resident/resident-service/src/test/java/io/mosip/resident/test/ResidentTestBootApplication.java
@@ -5,10 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 import io.mosip.idrepository.core.util.TokenIDGenerator;
+import io.mosip.kernel.authcodeflowproxy.api.service.validator.ScopeValidator;
 import io.mosip.kernel.authcodeflowproxy.api.service.validator.ValidateTokenHelper;
 
 @SpringBootApplication(scanBasePackages = {"io.mosip.resident.*"})
-@Import({TokenIDGenerator.class, ValidateTokenHelper.class})
+@Import({TokenIDGenerator.class, ValidateTokenHelper.class, ScopeValidator.class})
 public class ResidentTestBootApplication {
 
 	public static void main(String[] args) {
