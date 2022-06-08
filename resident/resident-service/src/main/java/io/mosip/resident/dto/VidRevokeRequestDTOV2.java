3942b2b53ae59034ea5a51e01a3b6e3787bbda75
Loganathan Sekar <loganathan.sekar@mindtree.com>
2022-06-08 20:55:56 +0530

    Added missing dto file

---
+++ resident/resident-service/src/main/java/io/mosip/resident/dto/VidRevokeRequestDTOV2.java
@@ -0,0 +1,12 @@
+package io.mosip.resident.dto;
+
+public class VidRevokeRequestDTOV2 extends BaseVidRevokeRequestDTO {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5855973435741502178L;
+
+	
+	
+}
