f3b8c78a5fe157e2054a69414a02ecedf6c63e2b
kameshsr <kameshsr1338@gmail.com>
2022-06-20 17:25:01 +0530

    MOSIP-22380  Improved Junit of AuthTransactionCallBackServiceImpl.java

--- resident/resident-service/src/test/java/io/mosip/resident/test/service/AuthTransactionCallbackServiceTest.java
+++ resident/resident-service/src/test/java/io/mosip/resident/test/service/AuthTransactionCallbackServiceTest.java
@@ -7,6 +7,9 @@ import io.mosip.kernel.core.websub.spi.SubscriptionClient;
 import io.mosip.kernel.websub.api.model.SubscriptionChangeRequest;
 import io.mosip.kernel.websub.api.model.SubscriptionChangeResponse;
 import io.mosip.kernel.websub.api.model.UnsubscriptionRequest;
+import io.mosip.resident.dto.ResidentTransactionType;
+import io.mosip.resident.entity.AutnTxn;
+import io.mosip.resident.exception.ApisResourceAccessException;
 import io.mosip.resident.exception.ResidentServiceCheckedException;
 import io.mosip.resident.repository.AutnTxnRepository;
 import io.mosip.resident.service.impl.AuthTransactionCallBackServiceImpl;
@@ -18,6 +21,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,8 +32,11 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.security.NoSuchAlgorithmException;
 import java.time.LocalDateTime;
 import java.util.Map;
+
+import static org.powermock.api.mockito.PowerMockito.mock;
 
 @RunWith(MockitoJUnitRunner.class)
 @RefreshScope
@@ -63,38 +70,13 @@ public class AuthTransactionCallbackServiceTest {
     @Mock
     SubscriptionClient<SubscriptionChangeRequest, UnsubscriptionRequest, SubscriptionChangeResponse> subscribe;
 
+    EventModel eventModel;
 
     @Before
     public void setup() {
         MockitoAnnotations.initMocks(this);
         this.mockMvc = MockMvcBuilders.standaloneSetup(authTransactionCallBackService).build();
-    }
-
-    @Test
-    public void testWebSubUpdateAuthTypeService() throws ResidentServiceCheckedException {
-
-        EventModel eventModel = new EventModel();
-        EventModel e1=new EventModel();
-        Event event=new Event();
-        event.setTransactionId("1234");
-        event.setId("8251649601");
-        Map<String, Object> partnerIdMap = new java.util.HashMap<>();
-        partnerIdMap.put("olv_partner_id", "mpartner-default-auth");
-        event.setData(partnerIdMap);
-
-        e1.setEvent(event);
-        e1.setTopic("AUTH_TYPE_STATUS_UPDATE_ACK");
-        e1.setPublishedOn(String.valueOf(LocalDateTime.now()));
-        e1.setPublisher("AUTH_TYPE_STATUS_UPDATE_ACK");
-
-        authTransactionCallBackService.updateAuthTransactionCallBackService(e1);
-
-    }
-
-    @Test
-    public void testWebSubInsertDataAuthTypeService() throws ResidentServiceCheckedException {
-        EventModel eventModel = new EventModel();
-        EventModel e1=new EventModel();
+        eventModel=new EventModel();
         Event event=new Event();
         event.setTransactionId("1234");
         event.setId("io.mosip.idauthentication");
@@ -102,11 +84,42 @@ public class AuthTransactionCallbackServiceTest {
         partnerIdMap.put("olv_partner_id", "mpartner-default-auth");
         event.setData(partnerIdMap);
 
-        e1.setEvent(event);
-        e1.setTopic("AUTH_TYPE_STATUS_UPDATE_ACK");
-        e1.setPublishedOn(String.valueOf(LocalDateTime.now()));
-        e1.setPublisher("AUTH_TYPE_STATUS_UPDATE_ACK");
-
-        authTransactionCallBackService.updateAuthTransactionCallBackService(e1);
+        eventModel.setEvent(event);
+        eventModel.setTopic("AUTH_TYPE_STATUS_UPDATE_ACK");
+        eventModel.setPublishedOn(String.valueOf(LocalDateTime.now()));
+        eventModel.setPublisher("AUTH_TYPE_STATUS_UPDATE_ACK");
     }
+
+    @Test
+    public void testWebSubUpdateAuthTypeService() throws ResidentServiceCheckedException, ApisResourceAccessException, NoSuchAlgorithmException {
+        authTransactionCallBackService.updateAuthTransactionCallBackService(eventModel);
+        authTransactionCallBackService = mock(AuthTransactionCallBackServiceImpl.class);
+        Mockito.lenient().doNothing().when(authTransactionCallBackService).updateAuthTransactionCallBackService(Mockito.any());
+    }
+
+    @Test
+    public void testWebSubUpdateAuthTypeServiceUpdate() throws ResidentServiceCheckedException, ApisResourceAccessException, NoSuchAlgorithmException {
+        AutnTxn autnTxn = new AutnTxn();
+        autnTxn.setResponseDTimes(LocalDateTime.now());
+        autnTxn.setStatusCode("NEW");
+        autnTxn.setStatusComment("NEW");
+        autnTxn.setUpdBy("RESIDENT");
+        autnTxn.setUpdDTimes(LocalDateTime.now());
+        autnTxn.setOlvPartnerId("mpartner-default-auth");
+        autnTxn.setToken("1234");
+        autnTxn.setAuthTypeCode(ResidentTransactionType.SERVICE_REQUEST.toString());
+
+        Mockito.when(autnTxnRepository.findById(Mockito.anyString())).thenReturn(autnTxn);
+        authTransactionCallBackService.updateAuthTransactionCallBackService(eventModel);
+        authTransactionCallBackService = mock(AuthTransactionCallBackServiceImpl.class);
+        Mockito.lenient().doNothing().when(authTransactionCallBackService).updateAuthTransactionCallBackService(Mockito.any());
+    }
+
+    @Test
+    public void testWebSubUpdateAuthTypeServiceException() throws ResidentServiceCheckedException, ApisResourceAccessException, NoSuchAlgorithmException {
+        authTransactionCallBackService.updateAuthTransactionCallBackService(eventModel);
+        authTransactionCallBackService = mock(AuthTransactionCallBackServiceImpl.class);
+        Mockito.lenient().doThrow(ResidentServiceCheckedException.class).when(authTransactionCallBackService).updateAuthTransactionCallBackService(Mockito.any());
+    }
+
 }
