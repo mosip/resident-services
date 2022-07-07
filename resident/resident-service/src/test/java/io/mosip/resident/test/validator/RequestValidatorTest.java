572a36c9169f52e387864665637c42ddd84041c3
kameshsr <kameshsr1338@gmail.com>
2022-07-07 12:59:35 +0530

    MOSIP-22443 Added testValidateAuthLockOrUnlockRequestV2EmptyAuthType.

--- resident/resident-service/src/test/java/io/mosip/resident/test/validator/RequestValidatorTest.java
+++ resident/resident-service/src/test/java/io/mosip/resident/test/validator/RequestValidatorTest.java
@@ -984,6 +984,20 @@ public class RequestValidatorTest {
 	}
 
 	@Test(expected = InvalidInputException.class)
+	public void testValidateAuthLockOrUnlockRequestV2EmptyAuthType() throws Exception{
+		AuthLockOrUnLockRequestDtoV2 authLockOrUnLockRequestDtoV2 = new AuthLockOrUnLockRequestDtoV2();
+		authLockOrUnLockRequestDtoV2.setIndividualId("12344567");
+		List<AuthTypeStatusDto> authTypes = new ArrayList<>();
+		authLockOrUnLockRequestDtoV2.setAuthType(authTypes);
+		RequestWrapper<AuthLockOrUnLockRequestDtoV2> requestWrapper = new RequestWrapper<>();
+		requestWrapper.setRequesttime(DateUtils.getUTCCurrentDateTimeString(pattern));
+		requestWrapper.setId("mosip.resident.authlock");
+		requestWrapper.setVersion("v1");
+		requestWrapper.setRequest(authLockOrUnLockRequestDtoV2);
+		requestValidator.validateAuthLockOrUnlockRequestV2(requestWrapper);
+	}
+
+	@Test(expected = InvalidInputException.class)
 	public void testValidateAuthLockOrUnlockRequestV2NullUnlockForSeconds() throws Exception{
 		AuthLockOrUnLockRequestDtoV2 authLockOrUnLockRequestDtoV2 = new AuthLockOrUnLockRequestDtoV2();
 		authLockOrUnLockRequestDtoV2.setIndividualId("12344567");
