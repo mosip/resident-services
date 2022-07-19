d66161577a9431fdeafcfb74812713abba431f62
kameshsr <47484458+kameshsr@users.noreply.github.com>
2022-07-19 18:40:54 +0530

    Update resident_transaction.sql

--- db_release_scripts/mosip_resident/ddl/resident_transaction.sql
+++ db_release_scripts/mosip_resident/ddl/resident_transaction.sql
@@ -12,6 +12,7 @@
 
 -- This Table is used to save the  transaction related to residents.
 CREATE TABLE resident.resident_transaction(
+	event_id character varying(64) NOT NULL,
 	aid character varying(64) NOT NULL,
 	request_dtimes timestamp NOT NULL,
 	response_dtime timestamp NOT NULL,
@@ -43,10 +44,11 @@ CREATE TABLE resident.resident_transaction(
     pinned_status boolean NOT NULL DEFAULT false,
     purpose character varying(64),
     credential_request_id character varying(256),
-    CONSTRAINT pk_restrn_aid PRIMARY KEY (aid)
+    CONSTRAINT pk_restrn_event_id PRIMARY KEY (event_id)
 );
 
 COMMENT ON TABLE resident_transaction IS 'This Table is used to save the  transaction related to residents.';
+COMMENT ON COLUMN resident_transaction.event_id IS 'Unique Id of the transaction.';
 COMMENT ON COLUMN resident.resident_transaction.aid IS 'A unique identifier for each transaction';
 COMMENT ON COLUMN resident.resident_transaction.request_dtimes IS 'The time when the request is received by the service';
 COMMENT ON COLUMN resident.resident_transaction.response_dtime IS 'The time when the response is received by the service';
