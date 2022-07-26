8fc96b80e6334d2e5991df58104e537fad7d6400
Ritik Jain <M1074108@mindtree.com>
2022-07-26 17:14:24 +0530

    changed event id, name, type to auditEvent id,name,type

--- resident/resident-service/src/main/java/io/mosip/resident/controller/ProxyAuditController.java
+++ resident/resident-service/src/main/java/io/mosip/resident/controller/ProxyAuditController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import io.mosip.kernel.core.http.ResponseFilter;
 import io.mosip.resident.dto.AuditRequestDTO;
+import io.mosip.resident.dto.AuditRequestDtoV2;
 import io.mosip.resident.exception.ResidentServiceCheckedException;
 import io.mosip.resident.util.AuditUtil;
 import io.swagger.v3.oas.annotations.Operation;
@@ -50,8 +51,25 @@ public class ProxyAuditController {
 			@ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content(schema = @Schema(hidden = true))),
 			@ApiResponse(responseCode = "403", description = "Forbidden", content = @Content(schema = @Schema(hidden = true))),
 			@ApiResponse(responseCode = "404", description = "Not Found", content = @Content(schema = @Schema(hidden = true))) })
-	public ResponseEntity<?> auditLog(@RequestBody AuditRequestDTO auditRequestDto)
+	public ResponseEntity<?> auditLog(@RequestBody AuditRequestDtoV2 auditRequestDtoV2)
 			throws ResidentServiceCheckedException {
+		AuditRequestDTO auditRequestDto=new AuditRequestDTO();
+		auditRequestDto.setEventId(auditRequestDtoV2.getAuditEventId());
+		auditRequestDto.setEventName(auditRequestDtoV2.getAuditEventName());
+		auditRequestDto.setEventType(auditRequestDtoV2.getAuditEventType());
+		auditRequestDto.setActionTimeStamp(auditRequestDtoV2.getActionTimeStamp());
+		auditRequestDto.setHostName(auditRequestDtoV2.getHostName());
+		auditRequestDto.setHostIp(auditRequestDtoV2.getHostIp());
+		auditRequestDto.setApplicationId(auditRequestDtoV2.getApplicationId());
+		auditRequestDto.setApplicationName(auditRequestDtoV2.getApplicationName());
+		auditRequestDto.setSessionUserId(auditRequestDtoV2.getSessionUserId());
+		auditRequestDto.setSessionUserName(auditRequestDtoV2.getSessionUserName());
+		auditRequestDto.setId(auditRequestDtoV2.getId());
+		auditRequestDto.setIdType(auditRequestDtoV2.getIdType());
+		auditRequestDto.setCreatedBy(auditRequestDtoV2.getCreatedBy());
+		auditRequestDto.setModuleName(auditRequestDtoV2.getModuleName());
+		auditRequestDto.setModuleId(auditRequestDtoV2.getModuleId());
+		auditRequestDto.setDescription(auditRequestDtoV2.getDescription());
 		auditUtil.callAuditManager(auditRequestDto);
 		return ResponseEntity.ok().build();
 	}
