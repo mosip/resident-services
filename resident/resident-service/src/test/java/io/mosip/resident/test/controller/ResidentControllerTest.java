9ccbbfbaa6d173c8404c7e4b97ef1ccdbc346f9c
kameshsr <kameshsr1338@gmail.com>
2022-08-02 15:06:29 +0530

    MOSIP-21376 Fixed test case failure.

--- resident/resident-service/src/test/java/io/mosip/resident/test/controller/ResidentControllerTest.java
+++ resident/resident-service/src/test/java/io/mosip/resident/test/controller/ResidentControllerTest.java
@@ -495,6 +495,6 @@ public class ResidentControllerTest {
 	public void testGetCredentialRequestStatusSuccess() throws Exception {
 		residentController.checkAidStatus("17", "eng");
 		when(residentService.checkAidStatus("17")).thenReturn("PROCESSED");
-		this.mockMvc.perform(get("/checkAidStatus/events/86c2ad43-e2a4-4952-bafc-d97ad1e5e453")).andExpect(status().isOk());
+		this.mockMvc.perform(get("/events/86c2ad43-e2a4-4952-bafc-d97ad1e5e453/?langCode=eng")).andExpect(status().isOk());
 	}
 }
