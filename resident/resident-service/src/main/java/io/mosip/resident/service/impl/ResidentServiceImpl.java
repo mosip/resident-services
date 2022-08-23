a0139ec12e455e89d290284466732ad689708e32
kameshsr <kameshsr1338@gmail.com>
2022-08-23 18:42:17 +0530

    fix auth status

--- resident/resident-service/src/main/java/io/mosip/resident/service/impl/ResidentServiceImpl.java
+++ resident/resident-service/src/main/java/io/mosip/resident/service/impl/ResidentServiceImpl.java
@@ -6,15 +6,7 @@ import static io.mosip.resident.constant.ResidentErrorCode.PACKET_SIGNKEY_EXCEPT
 import java.io.IOException;
 import java.math.BigInteger;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -254,14 +246,27 @@ public class ResidentServiceImpl implements ResidentService {
 
 	@Value("${resident.service.unreadnotificationlist.id}")
 	private String unreadnotificationlist;
-	
+
+	private static String authTypes;
+
+	@Value("${auth.types.allowed}")
+	public void setAuthTypes(String authType) {
+		authTypes = authType;
+	}
+
 	public static Function<? super AuthTypeStatusDtoV2, ? extends String> AUTH_TYPE_FUNCTION = t -> {
-		if(t.getAuthSubType()!=null) {
-			return t.getAuthType()+AUTH_TYPE_SEPERATOR+t.getAuthSubType();
+		String[] authTypesArray = authTypes.split(",");
+		List<String> authTypesAllowed = new ArrayList<>(Arrays.asList(authTypesArray));
+		for(String authType : authTypesAllowed) {
+			if(t.getAuthSubType()!=null) {
+				if(authType.toLowerCase().contains(t.getAuthSubType().toLowerCase())){
+					return authType;
+				}
+			} else {
+				return t.getAuthType();
+			}
 		}
-		else {
-			return t.getAuthType();
-		}
+		return null;
 	};
 
 	@Override
