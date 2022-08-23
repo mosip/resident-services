993c9d139576606585de0576714c57c00c39fa31
kameshsr <kameshsr1338@gmail.com>
2022-08-23 10:50:56 +0530

    MOSIP-22868 Return pdf

--- resident/resident-service/src/main/java/io/mosip/resident/service/impl/AcknowledgementServiceImpl.java
+++ resident/resident-service/src/main/java/io/mosip/resident/service/impl/AcknowledgementServiceImpl.java
@@ -2,6 +2,9 @@ package io.mosip.resident.service.impl;
 
 import io.mosip.kernel.core.http.ResponseWrapper;
 import io.mosip.kernel.core.logger.spi.Logger;
+import io.mosip.kernel.core.pdfgenerator.spi.PDFGenerator;
+import io.mosip.kernel.core.templatemanager.spi.TemplateManager;
+import io.mosip.kernel.core.templatemanager.spi.TemplateManagerBuilder;
 import io.mosip.resident.config.LoggerConfiguration;
 import io.mosip.resident.constant.RequestType;
 import io.mosip.resident.repository.ResidentTransactionRepository;
@@ -11,7 +14,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -32,6 +42,23 @@ public class AcknowledgementServiceImpl implements AcknowledgementService {
     @Value("${resident.template.ack.share-cred-with-partner}")
     private String shareCredWithPartnerTemplate;
 
+    private static final String CLASSPATH = "classpath";
+    private static final String ENCODE_TYPE = "UTF-8";
+
+    private TemplateManager templateManager;
+
+    @Autowired
+    private TemplateManagerBuilder templateManagerBuilder;
+
+    @PostConstruct
+    public void idTemplateManagerPostConstruct() {
+        templateManager = templateManagerBuilder.encodingType(ENCODE_TYPE).enableCache(false).resourceLoader(CLASSPATH)
+                .build();
+    }
+
+    @Autowired
+    private PDFGenerator pdfGenerator;
+
     @Override
     public byte[] getAcknowledgementPDF(String eventId, String languageCode) {
         logger.debug("AcknowledgementServiceImpl::getAcknowledgementPDF()::entry");
@@ -47,15 +74,28 @@ public class AcknowledgementServiceImpl implements AcknowledgementService {
             System.out.println(responseWrapper.getResponse());
             Map<String, Object> templateResponse = new LinkedHashMap<>((Map<String, Object>) responseWrapper.getResponse());
             String fileText = (String) templateResponse.get("fileText");
-            System.out.println(fileText);
 
-            Map<String, String> templateVariables = RequestType.ORDER_PHYSICAL_CARD.getAckTemplateVariables(templateUtil, eventId);
+            Map<String, String> templateVariables = RequestType.valueOf(requestTypeCode).getAckTemplateVariables(templateUtil, eventId);
+            InputStream stream = new ByteArrayInputStream(fileText.getBytes(StandardCharsets.UTF_8));
+            InputStream templateValue = templateManager.merge(stream, convertMapValueFromStringToObject(templateVariables));
+            System.out.println(templateValue);
+
+            ByteArrayOutputStream pdfValue= (ByteArrayOutputStream)pdfGenerator.generate(templateValue);
             logger.debug("AcknowledgementServiceImpl::getAcknowledgementPDF()::exit");
+            return pdfValue.toByteArray();
         }catch (Exception e){
             logger.error("AcknowledgementServiceImpl::getAcknowledgementPDF()::error::"+e.getMessage());
         }
 
         return new byte[0];
+    }
+
+    private Map<String, Object> convertMapValueFromStringToObject(Map<String, String> templateVariables) {
+        Map<String, Object> templateMapObject = new HashMap<>();
+        for(Map.Entry<String, String> entry: templateVariables.entrySet()){
+            templateMapObject.put(entry.getKey(), entry.getValue());
+        }
+        return templateMapObject;
     }
 
 }
