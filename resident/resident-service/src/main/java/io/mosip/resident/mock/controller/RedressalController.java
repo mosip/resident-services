9f1e4b19ea44e5760d0530e600bf708d15ab94a7
Neha Farheen <m1092365@mindtree.com>
2022-09-05 18:43:29 +0530

    Bug fix

--- resident/resident-service/src/main/java/io/mosip/resident/mock/controller/RedressalController.java
+++ resident/resident-service/src/main/java/io/mosip/resident/mock/controller/RedressalController.java
@@ -2,7 +2,6 @@ package io.mosip.resident.mock.controller;
 
 import java.net.MalformedURLException;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,14 +11,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class RedressalController {
-	@Value("${mosip.resident.griveance}")
-	private String griveance;
-
 	@RequestMapping("/external/grievence/redressel")
 	public String grievence(Model model, @RequestParam("name") String name, @RequestParam("emailId") String emailId,
 			@RequestParam("phoneNo") String phoneNo, @RequestParam("eventId") String eventId)
 			throws MalformedURLException {
-		String griveancevalue = griveance;
+
 		model.addAttribute("name", name);
 		model.addAttribute("email", emailId);
 		model.addAttribute("phoneno", phoneNo);
