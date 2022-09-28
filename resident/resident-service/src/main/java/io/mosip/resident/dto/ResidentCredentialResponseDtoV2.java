0093e99f3947de3ab2e944def83f2386ea348e2a
Ritik Jain <M1074108@mindtree.com>
2022-09-28 14:50:09 +0530

    added javadoc

--- resident/resident-service/src/main/java/io/mosip/resident/dto/ResidentCredentialResponseDtoV2.java
+++ resident/resident-service/src/main/java/io/mosip/resident/dto/ResidentCredentialResponseDtoV2.java
@@ -2,6 +2,11 @@ package io.mosip.resident.dto;
 
 import lombok.Data;
 
+/**
+ * The credential response dto v2.
+ * 
+ * @author Ritik Jain
+ */
 @Data
 public class ResidentCredentialResponseDtoV2 {
 	
