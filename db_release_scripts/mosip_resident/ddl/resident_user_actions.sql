af3e1aebf3af33dd680cb5417dea5c1f30839acf
Ritik Jain <M1074108@mindtree.com>
2022-10-03 19:15:04 +0530

    updated the ddl scripts

--- db_release_scripts/mosip_resident/ddl/resident_user_actions.sql
+++ db_release_scripts/mosip_resident/ddl/resident_user_actions.sql
@@ -16,6 +16,9 @@ CREATE TABLE resident.resident_user_actions(
     ida_token character varying(128) NOT NULL,
     last_bell_notif_click_dtimes timestamp NOT NULL,
     last_login_dtimes timestamp NOT NULL,
+	ip_address character varying(128),
+	host character varying(128),
+	machine_type character varying(30),
     CONSTRAINT pk_ida_token PRIMARY KEY (ida_token)
 );
 
@@ -23,3 +26,6 @@ COMMENT ON TABLE resident_user_actions IS 'This Table is used to save the  user 
 COMMENT ON COLUMN resident_user_actions.ida_token IS 'The unique identifier for each user';
 COMMENT ON COLUMN resident_user_actions.last_bell_notif_click_dtimes IS 'The time when the user last clicked on the bell notification';
 COMMENT ON COLUMN resident_user_actions.last_login_dtimes IS 'The time when the user last logged in';
+COMMENT ON COLUMN resident_user_actions.ip_address IS 'The ip_address of device from which the user logged in';
+COMMENT ON COLUMN resident_user_actions.host IS 'The host of the site';
+COMMENT ON COLUMN resident_user_actions.machine_type IS 'The OS of device used for accessing the portal/app';
