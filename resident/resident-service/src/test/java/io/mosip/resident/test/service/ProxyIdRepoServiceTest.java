f6b819a41a864ff24d696708d020633d5dc79580
RitikJain4108 <99730411+RitikJain4108@users.noreply.github.com>
2022-10-04 22:39:48 +0530

    Mosip 23215 corrections required in api end points or response request body 2 (#510)
    * corrected end point and response structure
    
    * removed duplicate code
    
    * changed response body
    
    * fixed rollback jpa transaction error
    
    * fixed the response structure
    
    * added javadoc and author
    
    * added javadoc
    
    * quick fix
    
    * changed purpose to description in response body
    
    * fixed the update count proxy api
    
    * updated the response
    
    * fixed the response body
    
    Co-authored-by: Ritik Jain <M1074108@mindtree.com>

--- resident/resident-service/src/test/java/io/mosip/resident/test/service/ProxyIdRepoServiceTest.java
+++ resident/resident-service/src/test/java/io/mosip/resident/test/service/ProxyIdRepoServiceTest.java
@@ -1,11 +1,10 @@
 package io.mosip.resident.test.service;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 import java.util.List;
-import java.util.Map;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -19,7 +18,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.context.WebApplicationContext;
 
 import io.mosip.kernel.core.http.ResponseWrapper;
-import io.mosip.resident.dto.UpdateCountDto;
 import io.mosip.resident.exception.ApisResourceAccessException;
 import io.mosip.resident.exception.ResidentServiceCheckedException;
 import io.mosip.resident.service.impl.IdentityServiceImpl;
@@ -45,14 +43,14 @@ public class ProxyIdRepoServiceTest {
 	@Test
 	public void testGetRemainingUpdateCountByIndividualId()
 			throws ResidentServiceCheckedException, ApisResourceAccessException {
-		ResponseWrapper<Map<String, Integer>> responseWrapper = new ResponseWrapper<>();
-		responseWrapper.setResponse(Map.of("fullName", 1));
+		ResponseWrapper<?> responseWrapper = new ResponseWrapper<>();
+		responseWrapper.setVersion("v1");
+		responseWrapper.setId("1");
 		when(identityServiceImpl.getResidentIndvidualId()).thenReturn("8251649601");
 		when(residentServiceRestClient.getApi(any(), any(List.class), any(String.class), any(String.class), any()))
 				.thenReturn(responseWrapper);
-		List<UpdateCountDto> response = service.getRemainingUpdateCountByIndividualId("",
-				List.of());
-		assertEquals(new UpdateCountDto("fullName", 1), response.get(0));
+		ResponseWrapper<?> response = service.getRemainingUpdateCountByIndividualId("", List.of());
+		assertNotNull(response);
 	}
 
 	@SuppressWarnings("unchecked")
@@ -62,7 +60,6 @@ public class ProxyIdRepoServiceTest {
 		when(identityServiceImpl.getResidentIndvidualId()).thenReturn("8251649601");
 		when(residentServiceRestClient.getApi(any(), any(List.class), any(String.class), any(String.class), any()))
 				.thenThrow(new ApisResourceAccessException());
-		service.getRemainingUpdateCountByIndividualId("",
-				List.of());
+		service.getRemainingUpdateCountByIndividualId("", List.of());
 	}
 }
