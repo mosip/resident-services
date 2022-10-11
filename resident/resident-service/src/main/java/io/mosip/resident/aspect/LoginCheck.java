c304bd054ce64e4a6b976c6748c261e14efddc5a
Ritik Jain <M1074108@mindtree.com>
2022-10-11 14:06:27 +0530

    updated constants and created mock method

--- resident/resident-service/src/main/java/io/mosip/resident/aspect/LoginCheck.java
+++ resident/resident-service/src/main/java/io/mosip/resident/aspect/LoginCheck.java
@@ -1,8 +1,8 @@
 package io.mosip.resident.aspect;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
+import java.net.HttpCookie;
 import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 import javax.servlet.http.HttpServletRequest;
@@ -13,7 +13,6 @@ import org.aspectj.lang.annotation.Aspect;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 
 import io.mosip.kernel.core.util.DateUtils;
 import io.mosip.resident.entity.ResidentUserEntity;
@@ -23,126 +22,111 @@ import io.mosip.resident.repository.ResidentUserRepository;
 @Aspect
 @EnableAspectJAutoProxy
 public class LoginCheck {
-	
+
+	private static final String ID_TOKEN = "id_token";
+	private static final String SET_COOKIE = "Set-Cookie";
+	private static final String USER_AGENT = "User-Agent";
+	private static final String WINDOWS = "Windows";
+	private static final String MAC = "Mac";
+	private static final String UNIX = "Unix";
+	private static final String X11 = "x11";
+	private static final String ANDROID = "Android";
+	private static final String IPHONE = "IPhone";
+	private static final String X_FORWARDED_FOR = "X-Forwarded-For";
+	private static final String PROXY_CLIENT_IP = "Proxy-Client-IP";
+	private static final String WL_PROXY_CLIENT_IP = "WL-Proxy-Client-IP";
+	private static final String HTTP_X_FORWARDED_FOR = "HTTP_X_FORWARDED_FOR";
+	private static final String HTTP_X_FORWARDED = "HTTP_X_FORWARDED";
+	private static final String HTTP_X_CLUSTER_CLIENT_IP = "HTTP_X_CLUSTER_CLIENT_IP";
+	private static final String HTTP_CLIENT_IP = "HTTP_CLIENT_IP";
+	private static final String HTTP_FORWARDED_FOR = "HTTP_FORWARDED_FOR";
+	private static final String HTTP_FORWARDED = "HTTP_FORWARDED";
+	private static final String HTTP_VIA = "HTTP_VIA";
+	private static final String REMOTE_ADDR = "REMOTE_ADDR";
+
 	@Autowired
 	private ResidentUserRepository residentUserRepository;
-	
-//	private final String LOCALHOST_IPV4 = "127.0.0.1";
-//	private final String LOCALHOST_IPV6 = "0:0:0:0:0:0:0:1";
-	
+
 	@After("execution(* io.mosip.kernel.authcodeflowproxy.api.controller.LoginController.loginRedirect(..)) && args(redirectURI,state,sessionState,code,stateCookie,req,res)")
-	public void getUserDetails(String redirectURI, String state, String sessionState, String code, String stateCookie, HttpServletRequest req, HttpServletResponse res) {
-		System.out.println("Running After Advice.============================"+redirectURI+"=============="+state+"----"+sessionState+"======--"+code);
-		
-		Collection<String> cookies=res.getHeaders("Set-Cookie");
-		cookies.forEach(cookie -> {
-			if(cookie.contains("id_token"))
-				System.out.println("================================"+cookie);
-		});
-		
-		System.out.println("ip address: "+getClientIp(req));
-		System.out.println("OS type: "+getMachineType(req));
-		System.out.println("Hostname: "+req.getRemoteHost());
-		System.out.println("Date/Time: "+DateUtils.getUTCCurrentDateTimeString());
-		
-//		Optional<ResidentUserEntity> userData = residentUserRepository.findById(idaToken);
-		Optional<ResidentUserEntity> userData = residentUserRepository.findById("283806899483626793628536705744577345");
-		if(userData.isPresent()) {
-//			residentUserRepository.updateUserData("idaToken", "lastLoginTime", "clientIP", "host", "machineType");
-			residentUserRepository.updateUserData("283806899483626793628536705744577345",
-					DateUtils.getUTCCurrentDateTime(), getClientIp(req), req.getRemoteHost(),
-					getMachineType(req));
+	public void getUserDetails(String redirectURI, String state, String sessionState, String code, String stateCookie,
+			HttpServletRequest req, HttpServletResponse res) {
+		String idaToken = "";
+		Collection<String> cookies = res.getHeaders(SET_COOKIE);
+		for (String cookie : cookies) {
+			if (cookie.contains(ID_TOKEN)) {
+				Optional<String> cookieIdToken = getCookieValueFromHeader(cookie);
+				if (cookieIdToken.isPresent()) {
+					idaToken = getIdaTokenFromIdToken(cookieIdToken.get()); // call a method to get ida token from
+																			// id_token
+				}
+				System.out.println("================================" + cookie); // remove this
+			}
+		}
+
+		System.out.println("ip address: " + getClientIp(req)); // remove this
+		System.out.println("OS type: " + getMachineType(req)); // remove this
+		System.out.println("Hostname: " + req.getRemoteHost()); // remove this
+		System.out.println("Date/Time: " + DateUtils.getUTCCurrentDateTimeString()); // remove this
+
+		Optional<ResidentUserEntity> userData = residentUserRepository.findById(idaToken);
+		if (userData.isPresent()) {
+			residentUserRepository.updateUserData(idaToken, DateUtils.getUTCCurrentDateTime(), getClientIp(req),
+					req.getRemoteHost(), getMachineType(req));
 		} else {
-//			residentUserRepository.save(new ResidentUserEntity("idaToken", null, "lastLoginTime", "clientIP", "host", "machineType"));
-			residentUserRepository.save(new ResidentUserEntity("283806899483626793628536705744577345", null,
-					DateUtils.getUTCCurrentDateTime(), getClientIp(req), req.getRemoteHost(),
-					getMachineType(req)));
+			residentUserRepository.save(new ResidentUserEntity(idaToken, null, DateUtils.getUTCCurrentDateTime(),
+					getClientIp(req), req.getRemoteHost(), getMachineType(req)));
 		}
 	}
-	
+
+	private String getIdaTokenFromIdToken(String cookieIdToken) {
+		// TODO Auto-generated method stub
+		return "268279069952256300388531046337488801";
+	}
+
+	private Optional<String> getCookieValueFromHeader(String cookie) {
+		List<HttpCookie> httpCookieList = HttpCookie.parse(cookie);
+		if (!httpCookieList.isEmpty()) {
+			HttpCookie httpCookie = httpCookieList.get(0);
+			String value = httpCookie.getValue();
+			return Optional.of(value);
+		}
+		return Optional.empty();
+	}
+
 	private String getMachineType(HttpServletRequest req) {
-		String  browserDetails  =   req.getHeader("User-Agent");
-        String  userAgent       =   browserDetails;
-    
-        String os = "";
-        //=================OS=======================
-         if (userAgent.toLowerCase().indexOf("windows") >= 0 )
-         {
-             os = "Windows";
-         } else if(userAgent.toLowerCase().indexOf("mac") >= 0)
-         {
-             os = "Mac";
-         } else if(userAgent.toLowerCase().indexOf("x11") >= 0)
-         {
-             os = "Unix";
-         } else if(userAgent.toLowerCase().indexOf("android") >= 0)
-         {
-             os = "Android";
-         } else if(userAgent.toLowerCase().indexOf("iphone") >= 0)
-         {
-             os = "IPhone";
-         }else{
-             os = "UnKnown, More-Info: "+userAgent;
-         }
+		String browserDetails = req.getHeader(USER_AGENT);
+		String userAgent = browserDetails;
+
+		String os = "";
+		if (userAgent.toLowerCase().indexOf(WINDOWS.toLowerCase()) >= 0) {
+			os = WINDOWS;
+		} else if (userAgent.toLowerCase().indexOf(MAC.toLowerCase()) >= 0) {
+			os = MAC;
+		} else if (userAgent.toLowerCase().indexOf(X11) >= 0) {
+			os = UNIX;
+		} else if (userAgent.toLowerCase().indexOf(ANDROID.toLowerCase()) >= 0) {
+			os = ANDROID;
+		} else if (userAgent.toLowerCase().indexOf(IPHONE.toLowerCase()) >= 0) {
+			os = IPHONE;
+		} else {
+			os = "UnKnown, More-Info: " + userAgent;
+		}
 		return os;
 	}
 
-//	public String getClientIp(HttpServletRequest request) {
-//		String ipAddress = request.getHeader("X-Forwarded-For");
-//		if(StringUtils.isEmpty(ipAddress) || "unknown".equalsIgnoreCase(ipAddress)) {
-//			ipAddress = request.getHeader("Proxy-Client-IP");
-//		}
-//		
-//		if(StringUtils.isEmpty(ipAddress) || "unknown".equalsIgnoreCase(ipAddress)) {
-//			ipAddress = request.getHeader("WL-Proxy-Client-IP");
-//		}
-//		
-//		if(StringUtils.isEmpty(ipAddress) || "unknown".equalsIgnoreCase(ipAddress)) {
-//			ipAddress = request.getRemoteAddr();
-//			if(LOCALHOST_IPV4.equals(ipAddress) || LOCALHOST_IPV6.equals(ipAddress)) {
-//				try {
-//					InetAddress inetAddress = InetAddress.getLocalHost();
-//					ipAddress = inetAddress.getHostAddress();
-//				} catch (UnknownHostException e) {
-//					e.printStackTrace();
-//				}
-//			}
-//		}
-//		
-//		if(!StringUtils.isEmpty(ipAddress) 
-//				&& ipAddress.length() > 15
-//				&& ipAddress.indexOf(",") > 0) {
-//			ipAddress = ipAddress.substring(0, ipAddress.indexOf(","));
-//		}
-//		
-//		return ipAddress;
-//	}
-	
 	public String getClientIp(HttpServletRequest req) {
-		String[] IP_HEADERS = {
-		        "X-Forwarded-For",
-		        "Proxy-Client-IP",
-		        "WL-Proxy-Client-IP",
-		        "HTTP_X_FORWARDED_FOR",
-		        "HTTP_X_FORWARDED",
-		        "HTTP_X_CLUSTER_CLIENT_IP",
-		        "HTTP_CLIENT_IP",
-		        "HTTP_FORWARDED_FOR",
-		        "HTTP_FORWARDED",
-		        "HTTP_VIA",
-		        "REMOTE_ADDR"
-
-		        // you can add more matching headers here ...
-		    };
-		for (String header: IP_HEADERS) {
-            String value = req.getHeader(header);
-            if (value == null || value.isEmpty()) {
-                continue;
-            }
-            String[] parts = value.split("\\s*,\\s*");
-            return parts[0];
-        }
-        return req.getRemoteAddr();
+		String[] IP_HEADERS = { X_FORWARDED_FOR, PROXY_CLIENT_IP, WL_PROXY_CLIENT_IP, HTTP_X_FORWARDED_FOR,
+				HTTP_X_FORWARDED, HTTP_X_CLUSTER_CLIENT_IP, HTTP_CLIENT_IP, HTTP_FORWARDED_FOR, HTTP_FORWARDED,
+				HTTP_VIA, REMOTE_ADDR };
+		for (String header : IP_HEADERS) {
+			String value = req.getHeader(header);
+			if (value == null || value.isEmpty()) {
+				continue;
+			}
+			String[] parts = value.split("\\s*,\\s*");
+			return parts[0];
+		}
+		return req.getRemoteAddr();
 	}
 
 }
