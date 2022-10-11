75b607fded4d5dfec4f1e37c72b31ca477cc081b
RitikJain4108 <99730411+RitikJain4108@users.noreply.github.com>
2022-10-11 15:07:31 +0530

    Mosip 23756 tech story extend the log in end point behaviour to store the last login details to the resident user action table (#517)
    * added aspect class
    
    * updated the ddl scripts
    
    * stored details in db
    
    * updated constants and created mock method
    
    * updated entity class
    
    * updated the scripts
    
    * created contructor for entity
    
    Co-authored-by: Ritik Jain <M1074108@mindtree.com>

--- resident/resident-service/src/main/java/io/mosip/resident/entity/ResidentUserEntity.java
+++ resident/resident-service/src/main/java/io/mosip/resident/entity/ResidentUserEntity.java
@@ -6,7 +6,6 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
 
 import lombok.Builder;
 import lombok.Data;
@@ -31,13 +30,20 @@ public class ResidentUserEntity {
 	@Column(name = "ida_token")
 	private String idaToken;
 
-	@NotNull
 	@Column(name = "last_bell_notif_click_dtimes")
-	LocalDateTime lastbellnotifDtimes;
+	private LocalDateTime lastbellnotifDtimes;
 
-	@NotNull
 	@Column(name = "last_login_dtimes")
-	private String lastloginDtime;
+	private LocalDateTime lastloginDtime;
+	
+	@Column(name = "ip_address")
+	private String ipAddress;
+	
+	@Column(name = "host")
+	private String host;
+	
+	@Column(name = "machine_type")
+	private String machineType;
 
 	/**
 	 * The constructor used in retrieval of the specific fields.
@@ -47,10 +53,28 @@ public class ResidentUserEntity {
 	 * @param lastloginDtime
 	 * 
 	 */
-	public ResidentUserEntity(String idaToken, LocalDateTime lastbellnotifDtimes, String lastloginDtime) {
+	public ResidentUserEntity(String idaToken, LocalDateTime lastbellnotifDtimes, LocalDateTime lastloginDtime) {
 		this.idaToken = idaToken;
 		this.lastbellnotifDtimes = lastbellnotifDtimes;
 		this.lastloginDtime = lastloginDtime;
 
+	}
+
+	public ResidentUserEntity(String idaToken, LocalDateTime lastloginDtime, String ipAddress, String host, String machineType) {
+		this.idaToken = idaToken;
+		this.lastloginDtime = lastloginDtime;
+		this.ipAddress = ipAddress;
+		this.host = host;
+		this.machineType = machineType;
+	}
+
+	public ResidentUserEntity(String idaToken, LocalDateTime lastbellnotifDtimes, LocalDateTime lastloginDtime,
+			String ipAddress, String host, String machineType) {
+		this.idaToken = idaToken;
+		this.lastbellnotifDtimes = lastbellnotifDtimes;
+		this.lastloginDtime = lastloginDtime;
+		this.ipAddress = ipAddress;
+		this.host = host;
+		this.machineType = machineType;
 	}
 }
