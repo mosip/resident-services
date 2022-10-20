1555e47d604f2ece53c6910425ee7c6542bd2828
kameshsr <kameshsr1338@gmail.com>
2022-10-20 17:50:33 +0530

    MOSIP-22275 Added junit for api /download-card.

---
+++ resident/resident-service/src/test/java/io/mosip/resident/test/controller/DownloadCardControllerTest.java
@@ -0,0 +1,107 @@
+package io.mosip.resident.test.controller;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import io.mosip.kernel.core.crypto.spi.CryptoCoreSpec;
+import io.mosip.resident.controller.DownloadCardController;
+import io.mosip.resident.dto.DownloadCardRequestDTO;
+import io.mosip.resident.dto.MainRequestDTO;
+import io.mosip.resident.helper.ObjectStoreHelper;
+import io.mosip.resident.service.DownloadCardService;
+import io.mosip.resident.service.ResidentVidService;
+import io.mosip.resident.service.impl.IdentityServiceImpl;
+import io.mosip.resident.test.ResidentTestBootApplication;
+import io.mosip.resident.util.AuditUtil;
+import io.mosip.resident.validator.RequestValidator;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.client.RestTemplate;
+
+import javax.crypto.SecretKey;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ResidentTestBootApplication.class)
+@AutoConfigureMockMvc
+@TestPropertySource(locations = "classpath:application.properties")
+public class DownloadCardControllerTest {
+	
+    @MockBean
+    private RequestValidator validator;
+
+    @Mock
+    private AuditUtil audit;
+	
+	@MockBean
+	private ObjectStoreHelper objectStore;
+
+
+    @MockBean
+    @Qualifier("selfTokenRestTemplate")
+    private RestTemplate residentRestTemplate;
+
+    @InjectMocks
+    DownloadCardController downloadCardController;
+
+    @MockBean
+    DownloadCardService downloadCardService;
+
+    @MockBean
+    IdentityServiceImpl identityService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ResidentVidService vidService;
+
+    @MockBean
+    private CryptoCoreSpec<byte[], byte[], SecretKey, PublicKey, PrivateKey, String> encryptor;
+
+    Gson gson = new GsonBuilder().serializeNulls().create();
+
+    String reqJson;
+
+    byte[] pdfbytes;
+
+    @Before
+    public void setup() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        this.mockMvc = MockMvcBuilders.standaloneSetup(downloadCardController).build();
+        MainRequestDTO<DownloadCardRequestDTO> downloadCardRequestDTOMainRequestDTO = new MainRequestDTO<>();
+        DownloadCardRequestDTO downloadCardRequestDTO = new DownloadCardRequestDTO();
+        downloadCardRequestDTO.setIndividualId("7841261580");
+        downloadCardRequestDTO.setOtp("111111");
+        downloadCardRequestDTO.setTransactionId("1234567890");
+        downloadCardRequestDTOMainRequestDTO.setRequest(downloadCardRequestDTO);
+        downloadCardRequestDTOMainRequestDTO.setId("mosip.resident.download.uin.card");
+        reqJson = gson.toJson(downloadCardRequestDTOMainRequestDTO);
+        pdfbytes = "uin".getBytes();
+    }
+
+    @Test
+    public void testgGetCardSuccess() throws Exception {
+        Mockito.when(downloadCardService.getDownloadCardPDF(Mockito.any())).thenReturn(pdfbytes);
+        mockMvc.perform(MockMvcRequestBuilders.post("/download-card").contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(reqJson.getBytes())).andExpect(status().isOk());
+    }
+}
