02b0fc9fe5c2d69fbfc32b47321a10fc18911bb9
kameshsr <kameshsr1338@gmail.com>
2022-10-21 14:19:47 +0530

    MOSIP-22275 Removed un-necessary variables.

--- resident/resident-service/src/main/java/io/mosip/resident/service/impl/DownloadCardServiceImpl.java
+++ resident/resident-service/src/main/java/io/mosip/resident/service/impl/DownloadCardServiceImpl.java
@@ -1,15 +1,10 @@
 package io.mosip.resident.service.impl;
 
-import io.mosip.kernel.core.http.ResponseWrapper;
 import io.mosip.kernel.core.logger.spi.Logger;
-import io.mosip.kernel.core.util.HMACUtils2;
 import io.mosip.resident.config.LoggerConfiguration;
-import io.mosip.resident.constant.ApiName;
 import io.mosip.resident.constant.LoggerFileConstant;
-import io.mosip.resident.constant.ResidentConstants;
 import io.mosip.resident.constant.ResidentErrorCode;
 import io.mosip.resident.controller.ResidentController;
-import io.mosip.resident.dto.DigitalCardStatusResponseDto;
 import io.mosip.resident.dto.DownloadCardRequestDTO;
 import io.mosip.resident.dto.MainRequestDTO;
 import io.mosip.resident.exception.ApisResourceAccessException;
@@ -20,21 +15,15 @@ import io.mosip.resident.service.DownloadCardService;
 import io.mosip.resident.service.IdAuthService;
 import io.mosip.resident.util.AuditUtil;
 import io.mosip.resident.util.EventEnum;
-import io.mosip.resident.util.JsonUtil;
 import io.mosip.resident.util.ResidentServiceRestClient;
 import io.mosip.resident.util.Utilities;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
-import java.net.URI;
-
 
 @Service
 public class DownloadCardServiceImpl implements DownloadCardService {
-
-    private static final String AVAILABLE = "AVAILABLE";
 
     @Autowired
     private ResidentController residentController;
@@ -56,6 +45,9 @@ public class DownloadCardServiceImpl implements DownloadCardService {
 
     @Autowired
     private IdAuthService idAuthService;
+
+    @Autowired
+    private ResidentServiceImpl residentService;
 
     private static final Logger logger = LoggerConfiguration.logConfig(DownloadCardServiceImpl.class);
 
@@ -85,52 +77,7 @@ public class DownloadCardServiceImpl implements DownloadCardService {
             throw new ResidentServiceException(ResidentErrorCode.OTP_VALIDATION_FAILED.getErrorCode(), e.getErrorText(),
                     e);
         }
-        return getUINCard(rid);
-    }
-
-    private byte[] getUINCard(String rid) {
-        try {
-            DigitalCardStatusResponseDto digitalCardStatusResponseDto = getDigitalCardStatus(rid);
-            if(digitalCardStatusResponseDto!=null){
-                if(!digitalCardStatusResponseDto.getStatusCode().equals(AVAILABLE)) {
-                    audit.setAuditRequestDto(EventEnum.RID_DIGITAL_CARD_REQ_EXCEPTION);
-                    throw new ResidentServiceException(
-                            ResidentErrorCode.DIGITAL_CARD_RID_NOT_FOUND.getErrorCode(),
-                            ResidentErrorCode.DIGITAL_CARD_RID_NOT_FOUND.getErrorMessage());
-                }
-                URI dataShareUri = URI.create(digitalCardStatusResponseDto.getUrl());
-                String encryptedData = residentServiceRestClient.getApi(dataShareUri, String.class);
-                return decryptDataShareData(encryptedData);
-            }
-        } catch (ApisResourceAccessException e) {
-            audit.setAuditRequestDto(EventEnum.RID_DIGITAL_CARD_REQ_EXCEPTION);
-            throw new ResidentServiceException(
-                    ResidentErrorCode.API_RESOURCE_ACCESS_EXCEPTION.getErrorCode(),
-                    ResidentErrorCode.API_RESOURCE_ACCESS_EXCEPTION.getErrorMessage(), e);
-        } catch (IOException e) {
-            audit.setAuditRequestDto(EventEnum.RID_DIGITAL_CARD_REQ_EXCEPTION);
-            throw new ResidentServiceException(
-                    ResidentErrorCode.IO_EXCEPTION.getErrorCode(),
-                    ResidentErrorCode.IO_EXCEPTION.getErrorMessage(), e);
-        }
-        return new byte[0];
-    }
-
-    private byte[] decryptDataShareData(String encryptedData) {
-        String decryptedData = objectStoreHelper.decryptData(encryptedData, env.getProperty(ResidentConstants.DATA_SHARE_APPLICATION_ID),
-                env.getProperty(ResidentConstants.DATA_SHARE_REFERENCE_ID));
-        return HMACUtils2.decodeBase64(decryptedData);
-    }
-
-    private DigitalCardStatusResponseDto getDigitalCardStatus(String individualId)
-            throws ApisResourceAccessException, IOException {
-        String digitalCardStatusUrl = env.getProperty(ApiName.DIGITAL_CARD_STATUS_URL.name()) +
-                individualId;
-        URI digitalCardStatusUri = URI.create(digitalCardStatusUrl);
-        ResponseWrapper<DigitalCardStatusResponseDto> responseDto =
-                residentServiceRestClient.getApi(digitalCardStatusUri, ResponseWrapper.class);
-        return JsonUtil.readValue(
-                JsonUtil.writeValueAsString(responseDto.getResponse()), DigitalCardStatusResponseDto.class);
+        return residentService.getUINCard(rid);
     }
 
 }
