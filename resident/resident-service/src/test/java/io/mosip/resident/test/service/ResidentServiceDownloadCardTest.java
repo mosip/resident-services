f5fa53ff3a2c494b3816b4679786794775e01a5a
kameshsr <kameshsr1338@gmail.com>
2022-11-28 17:40:35 +0530

    MOSIP-24939 Improved code coverage of Notification of resident.

--- resident/resident-service/src/test/java/io/mosip/resident/test/service/ResidentServiceDownloadCardTest.java
+++ resident/resident-service/src/test/java/io/mosip/resident/test/service/ResidentServiceDownloadCardTest.java
@@ -1,9 +1,14 @@
 package io.mosip.resident.test.service;
 
 import io.mosip.kernel.core.http.ResponseWrapper;
+import io.mosip.kernel.core.templatemanager.spi.TemplateManager;
 import io.mosip.resident.constant.ApiName;
 import io.mosip.resident.constant.RequestType;
+import io.mosip.resident.dto.BellNotificationDto;
 import io.mosip.resident.dto.DigitalCardStatusResponseDto;
+import io.mosip.resident.dto.PageDto;
+import io.mosip.resident.dto.ServiceHistoryResponseDto;
+import io.mosip.resident.dto.UnreadServiceNotificationDto;
 import io.mosip.resident.dto.UserInfoDto;
 import io.mosip.resident.entity.ResidentTransactionEntity;
 import io.mosip.resident.entity.ResidentUserEntity;
@@ -14,12 +19,14 @@ import io.mosip.resident.exception.ResidentServiceException;
 import io.mosip.resident.helper.ObjectStoreHelper;
 import io.mosip.resident.repository.ResidentTransactionRepository;
 import io.mosip.resident.repository.ResidentUserRepository;
+import io.mosip.resident.service.ProxyMasterdataService;
 import io.mosip.resident.service.impl.IdentityServiceImpl;
 import io.mosip.resident.service.impl.ResidentCredentialServiceImpl;
 import io.mosip.resident.service.impl.ResidentServiceImpl;
 import io.mosip.resident.util.AuditUtil;
 import io.mosip.resident.util.ResidentServiceRestClient;
 import io.mosip.resident.util.TemplateUtil;
+import io.mosip.resident.util.Utilitiy;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -30,7 +37,14 @@ import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
@@ -74,6 +88,15 @@ public class ResidentServiceDownloadCardTest {
 
     @Mock
     private ResidentUserRepository residentUserRepository;
+
+    @Mock
+    private ProxyMasterdataService proxyMasterdataService;
+
+    @Mock
+    private TemplateManager templateManager;
+
+    @Mock
+    private Utilitiy utility;
 
     private byte[] result;
     private String eventId;
@@ -180,5 +203,83 @@ public class ResidentServiceDownloadCardTest {
         Mockito.when(residentUserRepository.findById(Mockito.anyString())).thenReturn(response);
         ResponseWrapper<UserInfoDto> responseWrapper = residentServiceImpl.getUserinfo("123");
         assertEquals(responseWrapper.getResponse().getFullName(), responseWrapper.getResponse().getFullName());
+    }
+
+    @Test
+    public void testDownloadServiceHistory() throws ResidentServiceCheckedException, IOException {
+        ResponseWrapper<PageDto<ServiceHistoryResponseDto>> responseWrapper = new ResponseWrapper<>();
+        ServiceHistoryResponseDto serviceHistoryResponseDto = new ServiceHistoryResponseDto();
+        serviceHistoryResponseDto.setEventId("123");
+        PageDto<ServiceHistoryResponseDto> responseDtoPageDto= new PageDto<>();
+        responseDtoPageDto.setData(List.of(serviceHistoryResponseDto));
+        responseWrapper.setResponse(responseDtoPageDto);
+        ResponseWrapper responseWrapper1 = new ResponseWrapper<>();
+        Map<String, Object> templateResponse = new LinkedHashMap<>();
+        templateResponse.put("fileText", "test");
+        responseWrapper1.setResponse(templateResponse);
+        Mockito.when(proxyMasterdataService.getAllTemplateBylangCodeAndTemplateTypeCode(Mockito.anyString(), Mockito.anyString()))
+                .thenReturn(responseWrapper1);
+        Mockito.when(templateManager.merge(any(), any())).thenReturn(new ByteArrayInputStream("pdf".getBytes()));
+        Mockito.when(utility.signPdf(any(), any())).thenReturn("pdf".getBytes(StandardCharsets.UTF_8));
+        byte[] pdfDocument = residentServiceImpl.downLoadServiceHistory(responseWrapper, "eng",
+                LocalDateTime.now(), LocalDateTime.now(), LocalDateTime.now(),
+                String.valueOf(RequestType.DOWNLOAD_PERSONALIZED_CARD), "SUCCESS");
+        assertNotNull(pdfDocument);
+    }
+
+    @Test
+    public void testGetUnreadNotifyList(){
+        ResponseWrapper<List<UnreadServiceNotificationDto>> responseWrapper = new ResponseWrapper<>();
+        UnreadServiceNotificationDto unreadServiceNotificationDto = new UnreadServiceNotificationDto();
+        unreadServiceNotificationDto.setEventId("123");
+        unreadServiceNotificationDto.setRequestType("SERVICE_HISTORY");
+        responseWrapper.setResponse(List.of(unreadServiceNotificationDto));
+        ResidentTransactionEntity residentTransactionEntity1 = new ResidentTransactionEntity();
+        residentTransactionEntity1.setStatusComment("notification send");
+        residentTransactionEntity1.setEventId("123");
+        residentTransactionEntity1.setRequestSummary("notification");
+        residentTransactionEntity1.setStatusCode("200");
+        residentTransactionEntity1.setRequestDtimes(LocalDateTime.now());
+        residentTransactionEntity1.setRequestTypeCode("SERVICE_HISTORY");
+        Mockito.when(residentTransactionRepository.findByIdandStatus(Mockito.anyString())).thenReturn(List.of(residentTransactionEntity1));
+        assertEquals("123", residentServiceImpl.getUnreadnotifylist("123").getResponse().get(0).getEventId());
+    }
+
+    @Test
+    public void testUpdatebellClickdttimes(){
+        ResidentUserEntity residentUserEntity = new ResidentUserEntity();
+        residentUserEntity.setHost("localhost");
+        residentUserEntity.setIdaToken("123");
+        residentUserEntity.setIpAddress("http");
+        Optional<ResidentUserEntity> response = Optional.of(residentUserEntity);
+        Mockito.when(residentUserRepository.findById(Mockito.anyString())).thenReturn(response);
+        Mockito.when(residentUserRepository.updateByIdandTime(any(), any())).thenReturn(2);
+        assertEquals(2, residentServiceImpl.updatebellClickdttimes("123"));
+    }
+
+    @Test
+    public void testUpdatebellClickdttimesNewRecord(){
+        Optional<ResidentUserEntity> response = Optional.empty();
+        Mockito.when(residentUserRepository.findById(Mockito.anyString())).thenReturn(response);
+        Mockito.when(residentUserRepository.insertRecordByIdAndNotificationClickTime(any(), any())).thenReturn(1);
+        assertEquals(1, residentServiceImpl.updatebellClickdttimes("123"));
+    }
+
+    @Test
+    public void testGetbellClickdttimes(){
+        ResidentUserEntity residentUserEntity = new ResidentUserEntity();
+        residentUserEntity.setHost("localhost");
+        residentUserEntity.setIdaToken("123");
+        residentUserEntity.setIpAddress("http");
+        residentUserEntity.setLastbellnotifDtimes(LocalDateTime.of(2015, 12, 3, 4, 4, 4));
+        Optional<ResidentUserEntity> response = Optional.of(residentUserEntity);
+        Mockito.when(residentUserRepository.findById(Mockito.anyString())).thenReturn(response);
+        Mockito.when(residentUserRepository.insertRecordByIdAndNotificationClickTime(any(), any())).thenReturn(1);
+        ResponseWrapper<BellNotificationDto> responseWrapper = new ResponseWrapper<>();
+        BellNotificationDto bellNotificationDto = new BellNotificationDto();
+        bellNotificationDto.setLastbellnotifclicktime(LocalDateTime.now());
+        responseWrapper.setResponse(bellNotificationDto);
+        assertEquals(LocalDateTime.of(2015, 12, 3, 4, 4, 4),
+                residentServiceImpl.getbellClickdttimes("123").getResponse().getLastbellnotifclicktime());
     }
 }
