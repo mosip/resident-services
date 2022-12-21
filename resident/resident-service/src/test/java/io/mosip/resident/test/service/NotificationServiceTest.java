1f2762f7a85133b5330479297d35881d43f1be8b
kameshsr <kameshsr1338@gmail.com>
2022-12-21 10:57:00 +0530

    MOSIP-21674 Added junit for NotificationServiceTest

--- resident/resident-service/src/test/java/io/mosip/resident/test/service/NotificationServiceTest.java
+++ resident/resident-service/src/test/java/io/mosip/resident/test/service/NotificationServiceTest.java
@@ -415,4 +415,13 @@ public class NotificationServiceTest {
 
 	}
 
+	@Test(expected = ResidentServiceException.class)
+	public void testGetTemplateFailed() throws ApisResourceAccessException {
+		Mockito.when(restClient.getApi(Mockito.any(), Mockito.any(), Mockito.anyString(), Mockito.any(),
+				Mockito.any(Class.class))).thenReturn(null);
+		ReflectionTestUtils.invokeMethod(notificationService, "getTemplate",
+						"eng", "ack-download-personalized-card");
+
+	}
+
 }
