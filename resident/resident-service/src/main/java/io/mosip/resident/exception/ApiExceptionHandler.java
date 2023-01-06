cc7b6105c0b5ce4b8581095f1f3748fc89b1c998
Ritik Jain <M1074108@mindtree.com>
2023-01-06 22:06:11 +0530

    seperately checking data

--- resident/resident-service/src/main/java/io/mosip/resident/exception/ApiExceptionHandler.java
+++ resident/resident-service/src/main/java/io/mosip/resident/exception/ApiExceptionHandler.java
@@ -114,17 +114,17 @@ public class ApiExceptionHandler {
 
 	private ResponseEntity<ResponseWrapper<ServiceError>> createResponseEntity(
 			ResponseWrapper<ServiceError> errorResponse, Exception e, HttpStatus httpStatus) {
-		if (e instanceof ObjectWithMetadata && ((ObjectWithMetadata) e).getMetadata() != null
-				&& ((ObjectWithMetadata) e).getMetadata().containsKey(ResidentConstants.EVENT_ID)) {
-			MultiValueMap<String, String> headers = new HttpHeaders();
-			headers.add(ResidentConstants.EVENT_ID,
-					(String) ((ObjectWithMetadata) e).getMetadata().get(ResidentConstants.EVENT_ID));
-			return new ResponseEntity<>(errorResponse, headers, httpStatus);
-		} else if (e instanceof ObjectWithMetadata && ((ObjectWithMetadata) e).getMetadata() != null
-				&& ((ObjectWithMetadata) e).getMetadata().containsKey(ResidentConstants.HTTP_STATUS_CODE)
-				&& ((ObjectWithMetadata) e).getMetadata().containsKey(ResidentConstants.REQ_RES_ID)) {
-			httpStatus = (HttpStatus) ((ObjectWithMetadata) e).getMetadata().get(ResidentConstants.HTTP_STATUS_CODE);
-			errorResponse.setId((String) ((ObjectWithMetadata) e).getMetadata().get(ResidentConstants.REQ_RES_ID));
+		if (e instanceof ObjectWithMetadata && ((ObjectWithMetadata) e).getMetadata() != null) {
+			if (((ObjectWithMetadata) e).getMetadata().containsKey(ResidentConstants.EVENT_ID)) {
+				MultiValueMap<String, String> headers = new HttpHeaders();
+				headers.add(ResidentConstants.EVENT_ID,
+						(String) ((ObjectWithMetadata) e).getMetadata().get(ResidentConstants.EVENT_ID));
+				return new ResponseEntity<>(errorResponse, headers, httpStatus);
+			} else if (((ObjectWithMetadata) e).getMetadata().containsKey(ResidentConstants.HTTP_STATUS_CODE)) {
+				httpStatus = (HttpStatus) ((ObjectWithMetadata) e).getMetadata().get(ResidentConstants.HTTP_STATUS_CODE);
+			} else if (((ObjectWithMetadata) e).getMetadata().containsKey(ResidentConstants.REQ_RES_ID)) {
+				errorResponse.setId((String) ((ObjectWithMetadata) e).getMetadata().get(ResidentConstants.REQ_RES_ID));
+			}
 			errorResponse.setVersion(env.getProperty(ResidentConstants.REQ_RES_VERSION));
 			return new ResponseEntity<>(errorResponse, httpStatus);
 		}
