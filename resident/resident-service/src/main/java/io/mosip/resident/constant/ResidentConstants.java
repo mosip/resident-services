3be53e981810cf4c7081c3b6093df563e8756844
Loganathan Sekar <m1047627@mindtree.com>
2023-01-10 16:05:33 +0530

    Fixed mask attrib prefix

--- resident/resident-service/src/main/java/io/mosip/resident/constant/ResidentConstants.java
+++ resident/resident-service/src/main/java/io/mosip/resident/constant/ResidentConstants.java
@@ -158,6 +158,6 @@ public class ResidentConstants {
     public static final String CHANNEL = "channel";
     
     public static final String MASK_PREFIX = "masked_";
-    public static final String MASK_PERPETUAL_VID = "mask_perpetualVID";
+    public static final String MASK_PERPETUAL_VID = "masked_perpetualVID";
 
 }
