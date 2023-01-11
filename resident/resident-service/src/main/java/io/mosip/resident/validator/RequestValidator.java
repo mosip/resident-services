91aae9641e4c0cdb99fdb84a7229e9012a9866df
Loganathan Sekar <m1047627@mindtree.com>
2023-01-11 18:31:43 +0530

    Minor correction to the id prop

--- resident/resident-service/src/main/java/io/mosip/resident/validator/RequestValidator.java
+++ resident/resident-service/src/main/java/io/mosip/resident/validator/RequestValidator.java
@@ -1072,7 +1072,7 @@ public class RequestValidator {
 
     public void validateDownloadPersonalizedCard(MainRequestDTO<DownloadPersonalizedCardDto> downloadPersonalizedCardMainRequestDTO) {
 		validateRequestType(downloadPersonalizedCardMainRequestDTO.getId(),
-				this.environment.getProperty(ResidentConstants.MOSIP_RESIDENT_DOWNLOAD_PERSONALIZED_CARD), ID);
+				this.environment.getProperty(ResidentConstants.MOSIP_RESIDENT_DOWNLOAD_PERSONALIZED_CARD_ID), ID);
 		validateDate(downloadPersonalizedCardMainRequestDTO.getRequesttime());
 		validateString(downloadPersonalizedCardMainRequestDTO.getRequest().getHtml(), "html");
 		validateEncodedString(downloadPersonalizedCardMainRequestDTO.getRequest().getHtml());
