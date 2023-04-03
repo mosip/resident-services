fad43e090980efef41e2a40d2383c3b4b9b04c34
Loganathan Sekar <m1047627@mindtree.com>
2023-04-03 23:56:47 +0530

    Added jdoc

--- resident/resident-service/src/main/java/io/mosip/resident/constant/PreUpdateInBatchJob.java
+++ resident/resident-service/src/main/java/io/mosip/resident/constant/PreUpdateInBatchJob.java
@@ -9,8 +9,25 @@ import io.mosip.resident.exception.ApisResourceAccessException;
 import io.mosip.resident.exception.ResidentServiceCheckedException;
 import io.mosip.resident.util.Utility;
 
+/**
+ * The interface for implementing logic to be invoked before updating the status in batch job
+ * 
+ * @author Loganathan S
+ *
+ */
 public interface PreUpdateInBatchJob {
 
+	/**
+	 * Invoked before updating the status in the database and before sending notification
+	 * 
+	 * @param env
+	 * @param utility
+	 * @param txn
+	 * @param credentialStatus
+	 * @param newStatusCode
+	 * @throws ResidentServiceCheckedException
+	 * @throws ApisResourceAccessException
+	 */
 	void preUpdateInBatchJob(Environment env, Utility utility, ResidentTransactionEntity txn,
 			Map<String, String> credentialStatus, String newStatusCode)
 			throws ResidentServiceCheckedException, ApisResourceAccessException;
