a85522b18a752941e16d2e2f9014bc27cc5f4188
Ritik Jain <M1074108@mindtree.com>
2023-05-10 17:20:21 +0530

    mapped schema type with request type

--- resident/resident-service/src/main/java/io/mosip/resident/service/impl/UISchemaTypes.java
+++ resident/resident-service/src/main/java/io/mosip/resident/service/impl/UISchemaTypes.java
@@ -5,25 +5,40 @@ import java.util.List;
 import java.util.Optional;
 
 public enum UISchemaTypes {
-	UPDATE_DEMOGRAPHICS("update-demographics"),
-	PERSONALIZED_CARD("personalized-card"),
-	SHARE_CREDENTIAL("share-credential");
+	UPDATE_DEMOGRAPHICS("update-demographics", "UPDATE_MY_UIN"),
+	PERSONALIZED_CARD("personalized-card", "DOWNLOAD_PERSONALIZED_CARD"),
+	SHARE_CREDENTIAL("share-credential", "SHARE_CRED_WITH_PARTNER");
 	;
 	
 	private String fileIdentifier;
+	private String requestTypeCode;
 
-	private UISchemaTypes(String fileIdentifier) {
+	private UISchemaTypes(String fileIdentifier, String requestTypeCode) {
 		this.fileIdentifier = fileIdentifier;
+		this.requestTypeCode = requestTypeCode;
 	}
 	
 	public String getFileIdentifier() {
 		return fileIdentifier;
 	}
 
+	public String getRequestTypeCode() {
+		return requestTypeCode;
+	}
+
 	public static Optional<UISchemaTypes> getUISchemaTypeFromString(String schemaTypeString) {
         for (UISchemaTypes uiSchemaType : values()) {
             if (uiSchemaType.getFileIdentifier().equals(schemaTypeString)) {
                 return Optional.of(uiSchemaType);
+            }
+        }
+        return Optional.empty();
+    }
+
+	public static Optional<String> getUISchemaTypeFromRequestTypeCode(String requestTypeCodeString) {
+        for (UISchemaTypes uiSchemaType : values()) {
+            if (uiSchemaType.getRequestTypeCode().equals(requestTypeCodeString)) {
+                return Optional.of(uiSchemaType.getFileIdentifier());
             }
         }
         return Optional.empty();
