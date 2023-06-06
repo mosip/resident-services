16644e80c2b5380c3ec8b744fbea6cd0217c512b
Aiham Hasan <89243555+aihamh@users.noreply.github.com>
2023-06-06 16:43:31 +0530

    [MOSIP-27696,MOSIP-27405]Updated Audits (#948)
    * [MOSIP-24670] Updated API correction id and version
    
    * [MOSIP-24670] Updated API body correction
    
    * [MOSIP-22640] Fixed Sonar Bugs
    
    * updated PR
    
    * updated PR
    
    * updated review code
    
    * [MOSIP-22640] Fixed sonar bugs
    
    * updated PR
    
    * updated PR
    
    * [MOSIP-22639] Resolved security hotspots in code
    
    * [MOSIP-24630] Fixed Security Vulnerability in code
    
    * added error for isValidUrl method
    
    * renamed vaildateURL method
    
    * updated PR
    
    * updated PR
    
    * [MOSIP-24670] Made corrections in  id and version
    
    * resolved conflicts
    
    * updated PR
    
    * updated PR
    
    * [MOSIP-24670] Made corrections n API body, response code
    
    * updated PR
    
    * updated PR
    
    * updated dateTime
    
    * updated PR
    
    * [MOSIP-25539] Made corrections in API body, response code
    
    * [MOSIP-25539] made corrections in API body
    
    * updated error message
    
    * updated error code
    
    * updated trasactionid dto change
    
    * added equals
    
    * [MOSIP-25539] made corrections in api body
    
    * [MOSIP-25539] Corrected API response body Task 5
    
    * resolved build failure
    
    * [MOSIP-25539] Task 2 Added error code for vaildateOtp API
    
    * updated PR
    
    * updated PR
    
    * updated PR
    
    * updated PR
    
    * [MOSIP-25539] Updated validateotp method
    
    * updated PR
    
    * [MOSIP-25798] Added validations
    
    * updated PR
    
    * updated PR
    
    * updated PR
    
    * updated PR
    
    * updated PR
    
    * [MOSIP-25791] Added purpose as mandatory
    
    * Added error
    
    * Added Audit logs
    
    * updated test cases
    
    * [MOSIP-22913],[MOSIP-22914],[MOSIP-24653],[MOSIP-24796] Added Audit logs
    
    * updated logs
    
    * updated auditlog code
    
    * updated enum name for audit logs
    
    * updated enum name for audit logs
    
    * updated enum name for audit logs
    
    * updated enum name for audit logs
    
    * updated enum name for audit logs
    
    * [MOSIP-25268] Fixed Sonar vulnerability
    
    * [MOSIP-25268] Fixed Sonar vulnerability
    
    * [MOSIP-26272] Added validation for /update-uin API
    
    * updated PR
    
    * updated update-uin PR
    
    * Fixed build issue
    
    * Updated transactionId error message
    
    * Updated Test cases ResidentServiceResUpdateTest class
    
    * [MOSIP-26343] Created module specific audits
    
    * updated PR
    
    * updated PR
    
    * [MOSIP-26087] Added featureName in pdf name
    
    * updated PR
    
    * Updated PR as per review
    
    * updated PR
    
    * updated PR
    
    * Updated build
    
    * [MOSIP-24494] Updated trasnliterate API error msg
    
    * updated PR
    
    * updated PR
    
    * [MOSIP-25363],[MOSIP-24796],[MOSIP-25711] Updated audit logs
    
    * [MOSIP-26089] Added authentication mode
    
    * Fixed Sonar Bugs
    
    * updated PR
    
    * updated as per review
    
    * Updated Sonar Fix
    
    * Updated Sonar bug fix
    
    * Updated Sonar bug fix
    
    * Updated Sonar bug fix
    
    * Updated Sonar bug fix
    
    * Updated Sonar bug fix
    
    * Updated Sonar bug fix
    
    * [MOSIP-25793] Added audit logs
    
    * Fixed sonar bug
    
    * Fixed sonar bug
    
    * Fixed Sonar Bug
    
    * Updated Sonar Fix
    
    * Fixed build failure
    
    * [MOSIP-27213] Updated update-uin method
    
    * [MOSIP-27213] Updated ObjectValidator method
    
    * [MOSIP-27213] Updated ObjectValidator method
    
    * [MOSIP-27213] Updated ObjectValidator method
    
    * [MOSIP-27253,MOSIP-27254,MOSIP27255] Updated Download card  method
    
    * updated PR
    
    * [MOSIP-25363,MOSIP-27337] Updated Audit logs
    
    * Updated Audit event name
    
    * Updated sonar coverage
    
    * [MOSIP-27548,MOSIP-27547]Updated audits
    
    * Updated Audits Event name
    
    * Updated PR
    
    * Updated PR
    
    * Updated PR
    
    * Updated PR
    
    * [MOSIP-27405] Updated Audits
    
    * [MOSIP-27405] Updated Audits
    
    * [MOSIP-27552] Added ref_id as hash value of id
    
    * [MOSIP-27552] Added ref_id as hash value of id
    
    * [MOSIP-27729,MOSIP-27730,27696] Updated Audits
    
    * [MOSIP-27729,MOSIP-27730,27696] Updated Audits
    
    * Updated PR
    
    * Updated PR
    
    * [MOSIP-27696,MOSIP-27405]Updated Audits

--- resident/resident-service/src/test/java/io/mosip/resident/test/service/PinUnpinStatusServiceTest.java
+++ resident/resident-service/src/test/java/io/mosip/resident/test/service/PinUnpinStatusServiceTest.java
@@ -19,6 +19,7 @@ import io.mosip.kernel.core.http.ResponseWrapper;
 import io.mosip.resident.constant.ResidentErrorCode;
 import io.mosip.resident.dto.ResponseDTO;
 import io.mosip.resident.entity.ResidentTransactionEntity;
+import io.mosip.resident.exception.ResidentServiceCheckedException;
 import io.mosip.resident.repository.ResidentTransactionRepository;
 import io.mosip.resident.service.PinUnpinStatusService;
 import io.mosip.resident.service.impl.PinUnpinStatusServiceImpl;
@@ -53,13 +54,13 @@ public class PinUnpinStatusServiceTest {
     }
 
     @Test
-    public void pinStatusSuccessTest(){
+    public void pinStatusSuccessTest() throws ResidentServiceCheckedException{
         ResponseWrapper<ResponseDTO> responseDTO = pinUnpinStatusService.pinStatus("eventId", true);
         assertEquals(responseDTO.getResponse().getStatus(), HttpStatus.OK.toString());
     }
 
     @Test
-    public void pinStatusFailureTest(){
+    public void pinStatusFailureTest() throws ResidentServiceCheckedException{
         residentTransactionEntity = Optional.empty();
         Mockito.when(residentTransactionRepository.findById(Mockito.anyString())).thenReturn(residentTransactionEntity);
         ResponseWrapper<ResponseDTO> responseDTO = pinUnpinStatusService.pinStatus("eventId", true);
@@ -67,13 +68,13 @@ public class PinUnpinStatusServiceTest {
     }
 
     @Test
-    public void unPinStatusSuccessTest(){
+    public void unPinStatusSuccessTest() throws ResidentServiceCheckedException{
         ResponseWrapper<ResponseDTO> responseDTO = pinUnpinStatusService.pinStatus("eventId", false);
         assertEquals(responseDTO.getResponse().getStatus(), HttpStatus.OK.toString());
     }
 
     @Test
-    public void unPinStatusFailureTest(){
+    public void unPinStatusFailureTest() throws ResidentServiceCheckedException{
         residentTransactionEntity = Optional.empty();
         Mockito.when(residentTransactionRepository.findById(Mockito.anyString())).thenReturn(residentTransactionEntity);
         ResponseWrapper<ResponseDTO> responseDTO = pinUnpinStatusService.pinStatus("eventId", false);
