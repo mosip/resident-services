4522224462a6b5bb75eb7659358528778c562b10
Loganathan Sekar <42532387+LoganathanSekar7627@users.noreply.github.com>
2023-06-21 16:14:27 +0530

    Code cleanup (#977)
    * fix build issue
    
    * Fix local startup issue
    
    * Code cleanup, added domain-url config, organised imports
    
    ---------
    
    Co-authored-by: Loganathan Sekar <m1047627@mindtree.com>
    Signed-off-by: Ritik Jain <M1074108@mindtree.com>

--- resident/resident-service/src/main/java/io/mosip/resident/service/impl/WebSubUpdateAuthTypeServiceImpl.java
+++ resident/resident-service/src/main/java/io/mosip/resident/service/impl/WebSubUpdateAuthTypeServiceImpl.java
@@ -1,7 +1,5 @@
 package io.mosip.resident.service.impl;
 
-import static io.mosip.resident.constant.ResidentConstants.RESIDENT;
-
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
