d058d41cf8250d027f26df73112f1cf63357d1c1
kameshsr <kameshsr1338@gmail.com>
2023-06-28 12:58:25 +0530

    MOSIP-24939 Improved code coverage of WebSubUpdateAuthTypeService.

--- resident/resident-service/src/test/java/io/mosip/resident/service/impl/WebSubUpdateAuthTypeServiceTest.java
+++ resident/resident-service/src/test/java/io/mosip/resident/service/impl/WebSubUpdateAuthTypeServiceTest.java
@@ -1,13 +1,24 @@
 package io.mosip.resident.service.impl;
 
-import static org.powermock.api.mockito.PowerMockito.mock;
-
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.mosip.kernel.core.websub.model.Event;
+import io.mosip.kernel.core.websub.model.EventModel;
+import io.mosip.kernel.core.websub.spi.PublisherClient;
+import io.mosip.kernel.core.websub.spi.SubscriptionClient;
+import io.mosip.kernel.websub.api.model.SubscriptionChangeRequest;
+import io.mosip.kernel.websub.api.model.SubscriptionChangeResponse;
+import io.mosip.kernel.websub.api.model.UnsubscriptionRequest;
+import io.mosip.resident.dto.NotificationRequestDtoV2;
+import io.mosip.resident.dto.NotificationResponseDTO;
+import io.mosip.resident.entity.ResidentTransactionEntity;
+import io.mosip.resident.exception.ApisResourceAccessException;
+import io.mosip.resident.exception.ResidentServiceCheckedException;
+import io.mosip.resident.repository.ResidentTransactionRepository;
+import io.mosip.resident.service.NotificationService;
+import io.mosip.resident.util.AuditUtil;
+import io.mosip.resident.util.EventEnum;
+import io.mosip.resident.util.ResidentServiceRestClient;
+import io.mosip.resident.util.Utility;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -21,25 +32,21 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpHeaders;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-import io.mosip.kernel.core.websub.model.Event;
-import io.mosip.kernel.core.websub.model.EventModel;
-import io.mosip.kernel.core.websub.spi.PublisherClient;
-import io.mosip.kernel.core.websub.spi.SubscriptionClient;
-import io.mosip.kernel.websub.api.model.SubscriptionChangeRequest;
-import io.mosip.kernel.websub.api.model.SubscriptionChangeResponse;
-import io.mosip.kernel.websub.api.model.UnsubscriptionRequest;
-import io.mosip.resident.dto.NotificationResponseDTO;
-import io.mosip.resident.exception.ApisResourceAccessException;
-import io.mosip.resident.exception.ResidentServiceCheckedException;
-import io.mosip.resident.repository.ResidentTransactionRepository;
-import io.mosip.resident.service.NotificationService;
-import io.mosip.resident.util.AuditUtil;
-import io.mosip.resident.util.ResidentServiceRestClient;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.powermock.api.mockito.PowerMockito.mock;
 
 @RunWith(MockitoJUnitRunner.class)
 @RefreshScope
@@ -69,9 +76,15 @@ public class WebSubUpdateAuthTypeServiceTest {
 
     @Mock
     SubscriptionClient<SubscriptionChangeRequest, UnsubscriptionRequest, SubscriptionChangeResponse> subscribe;
-    
+
     @Mock
-	private NotificationService notificationService;
+    private AuditUtil auditUtil;
+
+    @Mock
+    private NotificationService notificationService;
+
+    @Mock
+    private Utility utility;
     
     private ObjectMapper objectMapper = new ObjectMapper();
 
@@ -79,6 +92,7 @@ public class WebSubUpdateAuthTypeServiceTest {
 
     private EventModel eventModel;
     private Event event;
+    private String partnerId;
 
     @Before
     public void setup() throws ApisResourceAccessException, ResidentServiceCheckedException {
@@ -88,7 +102,8 @@ public class WebSubUpdateAuthTypeServiceTest {
         this.mockMvc = MockMvcBuilders.standaloneSetup(webSubUpdateAuthTypeService).build();
         notificationResponseDTO = new NotificationResponseDTO();
 		notificationResponseDTO.setStatus("Notification success");
-		//Mockito.lenient().when(notificationService.sendNotification(Mockito.any())).thenReturn(notificationResponseDTO);
+        partnerId = "mpartner-default-auth";
+        ReflectionTestUtils.setField(webSubUpdateAuthTypeService, "onlineVerificationPartnerId", partnerId);
     }
 
     @Test
@@ -103,7 +118,7 @@ public class WebSubUpdateAuthTypeServiceTest {
 
         webSubUpdateAuthTypeService.updateAuthTypeStatus(objectMapper.convertValue(eventModel, Map.class));
         webSubUpdateAuthTypeService = mock(WebSubUpdateAuthTypeServiceImpl.class);
-        Mockito.lenient().doNothing().when(webSubUpdateAuthTypeService).updateAuthTypeStatus(Mockito.any());
+        Mockito.lenient().doNothing().when(webSubUpdateAuthTypeService).updateAuthTypeStatus(any());
     }
 
     @Test
@@ -118,5 +133,43 @@ public class WebSubUpdateAuthTypeServiceTest {
         event.setData(data);
         eventModel.setEvent(event);
         webSubUpdateAuthTypeService.updateAuthTypeStatus(objectMapper.convertValue(eventModel, Map.class));
+    }
+
+
+    @Test
+    public void testUpdateAuthTypeStatus_Success() throws Exception {
+        // Mock data
+        Map<String, Object> eventModel = new HashMap<>();
+        eventModel.put("event", getMockEventMap());
+
+        // Mock repository response
+        ResidentTransactionEntity residentTransactionEntity = new ResidentTransactionEntity();
+        residentTransactionEntity.setOlvPartnerId(partnerId);
+        residentTransactionEntity.setEventId("12454578458478547");
+        residentTransactionEntity.setIndividualId("4515452565");
+        when(residentTransactionRepository.findByRequestTrnId("12345")).thenReturn(List.of(residentTransactionEntity));
+
+        // Mock utility response
+        when(utility.getSessionUserName()).thenReturn("testUser");
+
+        // Invoke the method
+        webSubUpdateAuthTypeService.updateAuthTypeStatus(eventModel);
+
+        // Verify that the expected methods were called
+        verify(auditUtil, times(1)).setAuditRequestDto(EventEnum.UPDATE_AUTH_TYPE_STATUS);
+        verify(residentTransactionRepository, times(1)).findByRequestTrnId("12345");
+        verify(notificationService, times(1)).sendNotification(any(NotificationRequestDtoV2.class));
+    }
+
+    private Map<String, Object> getMockEventMap() {
+        Map<String, Object> eventMap = new HashMap<>();
+        Map<String, Object> dataMap = new HashMap<>();
+        List<Map<String, Object>> authTypesList = new ArrayList<>();
+        Map<String, Object> authTypeStatus = new HashMap<>();
+        authTypeStatus.put("requestId", "12345");
+        authTypesList.add(authTypeStatus);
+        dataMap.put("authTypes", authTypesList);
+        eventMap.put("data", dataMap);
+        return eventMap;
     }
 }
