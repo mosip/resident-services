ff87d3471fe26685934b4a4821574997f6e3f253
kameshsr <kameshsr1338@gmail.com>
2023-06-28 17:42:22 +0530

    Added upgrade and rollback sql files.

---
+++ db_release_scripts/mosip_resident/sql/1.1.5.5_to_1.2.0.1-B1_upgrade.sql
@@ -0,0 +1,21 @@
+-- -------------------------------------------------------------------------------------------------
+-- Database Name: mosip_resident
+-- Release Version 	: 1.2.1
+-- Purpose    		: Database scripts for Resident Service DB.
+-- Create By   		: Manoj SP
+-- Created Date		: April-2022
+--
+-- Modified Date        Modified By             Comments / Remarks
+-- --------------------------------------------------------------------------------------------------
+-- April-2022			Manoj SP	            Added otp_transaction table creation scripts with comments.
+-- April-2022           Kamesh Shekhar Prasad   Added resident_transaction table creation scripts with comments.
+-- Dec-2022           Kamesh Shekhar Prasad     Added resident_grievance_ticket, resident_user_actions table creation scripts with comments.
+-----------------------------------------------------------------------------------------------------
+\c mosip_resident sysadmin
+
+\ir ddl/otp_transaction.sql
+\ir ddl/resident_transaction.sql
+\ir ddl/resident_grievance_ticket.sql
+\ir ddl/resident_user_actions.sql
+\ir ddl/resident_sessions.sql
+-----------------------------------------------------------------------------------------------------
