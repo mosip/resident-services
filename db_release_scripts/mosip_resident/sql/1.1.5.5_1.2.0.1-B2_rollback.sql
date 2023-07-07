d90ad7925d3169c5d0f549353100fe4b63dcdc2d
kameshsr <kameshsr1338@gmail.com>
2023-07-07 15:32:52 +0530

    Moved DDL files to upgrade sql

---
+++ db_release_scripts/mosip_resident/sql/1.1.5.5_1.2.0.1-B2_rollback.sql
@@ -0,0 +1,20 @@
+-- -------------------------------------------------------------------------------------------------
+-- Database Name: mosip_resident
+-- Release Version 	: 1.2.1
+-- Purpose    		: Database scripts for Resident Service DB.
+-- Create By   		: Manoj SP
+-- Created Date		: April-2022
+--
+-- Modified Date        Modified By             Comments / Remarks
+-- --------------------------------------------------------------------------------------------------
+-- April-2022			Manoj SP	            Added otp_transaction table creation scripts with comments.
+-- April-2022           Kamesh Shekhar Prasad   Added resident_transaction table creation scripts with comments.
+-----------------------------------------------------------------------------------------------------
+\c mosip_resident
+
+DROP TABLE IF EXISTS resident.otp_transaction;
+DROP TABLE IF EXISTS resident.resident_transaction;
+DROP TABLE IF EXISTS resident.resident_session;
+DROP TABLE IF EXISTS resident.resident_user_actions;
+
+-----------------------------------------------------------------------------------------------------
