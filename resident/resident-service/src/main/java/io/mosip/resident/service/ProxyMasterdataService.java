4e4c6b8baf9d9f19ab3881deba7d953364c32559
kameshsr <kameshsr1338@gmail.com>
2023-07-12 12:05:31 +0530

    Mosip-22193 Added caching of template and user info.

--- resident/resident-service/src/main/java/io/mosip/resident/service/ProxyMasterdataService.java
+++ resident/resident-service/src/main/java/io/mosip/resident/service/ProxyMasterdataService.java
@@ -166,5 +166,7 @@ public interface ProxyMasterdataService {
 
 	public ResponseWrapper<GenderCodeResponseDTO> getGenderCodeByGenderTypeAndLangCode(String gendertype, String langCode) throws ResidentServiceCheckedException, IOException;
 
+	String getTemplateValueFromTemplateTypeCodeAndLangCode(String languageCode, String templateTypeCode);
+
 	
 }
