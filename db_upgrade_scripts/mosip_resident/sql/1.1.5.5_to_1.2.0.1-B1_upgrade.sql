ba5d3160017c118f9fdab4cc0de242a2123feeb6
kameshsr <47484458+kameshsr@users.noreply.github.com>
2023-07-17 14:05:34 +0530

    Update 1.1.5.5_to_1.2.0.1-B1_upgrade.sql
    Signed-off-by: kameshsr <47484458+kameshsr@users.noreply.github.com>

--- db_upgrade_scripts/mosip_resident/sql/1.1.5.5_to_1.2.0.1-B1_upgrade.sql
+++ db_upgrade_scripts/mosip_resident/sql/1.1.5.5_to_1.2.0.1-B1_upgrade.sql
@@ -125,13 +125,13 @@ CREATE TABLE resident.resident_session(
     CONSTRAINT pk_session_id PRIMARY KEY (session_id)
 );
 
-COMMENT ON TABLE resident_session IS 'This Table is used to save the  user sessions.';
-COMMENT ON COLUMN resident_session.session_id IS 'The unique session identifier for each login';
-COMMENT ON COLUMN resident_session.ida_token IS 'The unique identifier for each user';
-COMMENT ON COLUMN resident_session.login_dtimes IS 'The time when the user last logged in';
-COMMENT ON COLUMN resident_session.ip_address IS 'The ip_address of device from which the user logged in';
-COMMENT ON COLUMN resident_session.host IS 'The host of the site';
-COMMENT ON COLUMN resident_session.machine_type IS 'The OS of device used for accessing the portal/app';
+COMMENT ON TABLE resident.resident_session IS 'This Table is used to save the  user sessions.';
+COMMENT ON COLUMN resident.resident_session.session_id IS 'The unique session identifier for each login';
+COMMENT ON COLUMN resident.resident_session.ida_token IS 'The unique identifier for each user';
+COMMENT ON COLUMN resident.resident_session.login_dtimes IS 'The time when the user last logged in';
+COMMENT ON COLUMN resident.resident_session.ip_address IS 'The ip_address of device from which the user logged in';
+COMMENT ON COLUMN resident.resident_session.host IS 'The host of the site';
+COMMENT ON COLUMN resident.resident_session.machine_type IS 'The OS of device used for accessing the portal/app';
 
 GRANT SELECT, INSERT, TRUNCATE, REFERENCES, UPDATE, DELETE
    ON resident.resident_session
@@ -225,9 +225,9 @@ CREATE TABLE resident.resident_user_actions(
     CONSTRAINT pk_ida_token PRIMARY KEY (ida_token)
 );
 
-COMMENT ON TABLE resident_user_actions IS 'This Table is used to save the  user actions';
-COMMENT ON COLUMN resident_user_actions.ida_token IS 'The unique identifier for each user';
-COMMENT ON COLUMN resident_user_actions.last_bell_notif_click_dtimes IS 'The time when the user last clicked on the bell notification';
+COMMENT ON TABLE resident.resident_user_actions IS 'This Table is used to save the  user actions';
+COMMENT ON COLUMN resident.resident_user_actions.ida_token IS 'The unique identifier for each user';
+COMMENT ON COLUMN resident.resident_user_actions.last_bell_notif_click_dtimes IS 'The time when the user last clicked on the bell notification';
 
 GRANT SELECT, INSERT, TRUNCATE, REFERENCES, UPDATE, DELETE
    ON resident.resident_user_actions
