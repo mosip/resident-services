8a8c1ec40ab428de3f691d3f4f2693f1438cd15d
kameshsr <47484458+kameshsr@users.noreply.github.com>
2023-07-18 17:22:18 +0530

    MOSIP-28517 Added index to resident tables. (#1017)
    * Fixed service history issue.
    
    * MOSIP-27716 Moved identity-data-formatter.mvel to credentialdata.mvel.
    
    * Fixed Null pointer exception in notification method.
    
    * MOSIP-28517 Added index to resident tables.
    
    * MOSIP-28517 Removed read_status index in resident_transaction.sql.
    
    * MOSIP-28517 Added read_status index in resident_transaction.sql.
    
    * MOSIP-28517 Added grant statement for resident tables.
    
    * MOSIP-28517 Removed truncate from grant.
    
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

--- db_release_scripts/mosip_resident/sql/1.2.0_resident-scripts_revoke.sql
+++ db_release_scripts/mosip_resident/sql/1.2.0_resident-scripts_revoke.sql
@@ -12,6 +12,16 @@
 -----------------------------------------------------------------------------------------------------
 \c mosip_resident sysadmin
 
+DROP INDEX IF EXISTS idx_resident_transaction_event_id;
+DROP INDEX IF EXISTS idx_resident_transaction_token_id;
+DROP INDEX IF EXISTS idx_resident_transaction_credential_request_id;
+DROP INDEX IF EXISTS idx_resident_transaction_request_dtimes;
+DROP INDEX IF EXISTS idx_resident_transaction_request_trn_id;
+DROP INDEX IF EXISTS idx_resident_transaction_ref_id;
+DROP INDEX IF EXISTS idx_resident_user_actions_ida_token;
+DROP INDEX IF EXISTS idx_resident_session_ida_token;
+DROP INDEX IF EXISTS idx_resident_transaction_read_status;
+
 DROP TABLE IF EXISTS resident.otp_transaction;
 DROP TABLE IF EXISTS resident.resident_transaction;
 DROP TABLE IF EXISTS resident.resident_session;
