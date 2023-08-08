9092909d36720bc5bf3fe5a23fa4715a4ad2b67c
Ritik Jain <M1074108@mindtree.com>
2023-08-08 14:17:59 +0530

    fixed pin unpin db calls

--- resident/resident-service/src/main/java/io/mosip/resident/service/impl/PinUnpinStatusServiceImpl.java
+++ resident/resident-service/src/main/java/io/mosip/resident/service/impl/PinUnpinStatusServiceImpl.java
@@ -1,7 +1,6 @@
 package io.mosip.resident.service.impl;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,7 +15,6 @@ import io.mosip.resident.config.LoggerConfiguration;
 import io.mosip.resident.constant.LoggerFileConstant;
 import io.mosip.resident.constant.ResidentErrorCode;
 import io.mosip.resident.dto.ResponseDTO;
-import io.mosip.resident.entity.ResidentTransactionEntity;
 import io.mosip.resident.exception.ResidentServiceCheckedException;
 import io.mosip.resident.repository.ResidentTransactionRepository;
 import io.mosip.resident.service.PinUnpinStatusService;
@@ -31,7 +29,6 @@ public class PinUnpinStatusServiceImpl implements PinUnpinStatusService {
     private static final Logger logger = LoggerConfiguration.logConfig(PinUnpinStatusServiceImpl.class);
 
     private static final String SUCCESS = "SUCCESS";
-    private static final String FAILURE = "FAILURE";
 
     @Autowired
     private ResidentTransactionRepository residentTransactionRepository;
@@ -51,45 +48,34 @@ public class PinUnpinStatusServiceImpl implements PinUnpinStatusService {
     @Override
 	public ResponseWrapper<ResponseDTO> pinStatus(String eventId, boolean status)
 			throws ResidentServiceCheckedException {
+    	ResponseWrapper<ResponseDTO> responseWrapper = new ResponseWrapper<>();
         try {
         	logger.debug("PinUnpinStatusServiceImpl::pinStatus()::entry");
-            Optional<ResidentTransactionEntity> optionalResidentTransactionEntity = residentTransactionRepository.findById(eventId);
-            if (optionalResidentTransactionEntity.isPresent()) {
-                ResidentTransactionEntity residentTransactionEntity = optionalResidentTransactionEntity.get();
-                residentTransactionEntity.setPinnedStatus(status);
-                residentTransactionRepository.save(residentTransactionEntity);
+            if (residentTransactionRepository.existsById(eventId)) {
+            	residentTransactionRepository.updatePinnedStatus(eventId);
             } else {
             	logger.error("PinUnpinStatusServiceImpl - %s", ResidentErrorCode.EVENT_STATUS_NOT_FOUND.getErrorMessage());
                 throw new ResidentServiceCheckedException(ResidentErrorCode.EVENT_STATUS_NOT_FOUND);
             }
-            ResponseWrapper<ResponseDTO> responseWrapper = new ResponseWrapper<>();
             ResponseDTO responseDTO = new ResponseDTO();
             responseDTO.setStatus(HttpStatus.OK.toString());
             responseDTO.setMessage(SUCCESS);
             responseWrapper.setResponse(responseDTO);
-            if(status){
-                responseWrapper.setId(pinnedStatusId);
-                responseWrapper.setVersion(pinnedStatusVersion);
-            } else{
-                responseWrapper.setId(unPinnedStatusId);
-                responseWrapper.setVersion(unPinnedStatusVersion);
-            }
             logger.debug("PinUnpinStatusServiceImpl::pinStatus()::exit");
-            return responseWrapper;
         }
         catch (Exception e){
             logger.error(LoggerFileConstant.SESSIONID.toString(), LoggerFileConstant.APPLICATIONID.toString(),
                     LoggerFileConstant.APPLICATIONID.toString(), ExceptionUtils.getStackTrace(e));
-            ResponseWrapper<ResponseDTO> responseWrapper = new ResponseWrapper<>();
-            if(status){
+            responseWrapper.setErrors(List.of(new ServiceError(ResidentErrorCode.EVENT_STATUS_NOT_FOUND.getErrorCode(), ResidentErrorCode.EVENT_STATUS_NOT_FOUND.getErrorMessage())));
+        } finally {
+        	if(status){
                 responseWrapper.setId(pinnedStatusId);
                 responseWrapper.setVersion(pinnedStatusVersion);
             } else{
                 responseWrapper.setId(unPinnedStatusId);
                 responseWrapper.setVersion(unPinnedStatusVersion);
             }
-            responseWrapper.setErrors(List.of(new ServiceError(ResidentErrorCode.EVENT_STATUS_NOT_FOUND.getErrorCode(), ResidentErrorCode.EVENT_STATUS_NOT_FOUND.getErrorMessage())));
-            return responseWrapper;
         }
+        return responseWrapper;
     }
 }
