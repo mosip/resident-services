4be773290ed49d5d9cabd292546f7e9017f8dbc7
RitikJain4108 <99730411+RitikJain4108@users.noreply.github.com>
2023-08-08 17:49:35 +0530

    MOSIP-28864 fixed pin unpin db call issue (#1042)
    * updated package
    
    * updated package of validation test calsses
    
    * updated
    
    * formatting
    
    * updated packages of controller test classes
    
    * updated package of service impl test classes
    
    * updated package of util test classes
    
    * updated package of aspect test classes
    
    * updated package of config test classes
    
    * updated package of handler test classes
    
    * added some exception test cases
    
    * added coverage for vid controller
    
    * added junit for configservice impl
    
    * added junits for config service impl
    
    * formatting
    
    * updated test cases
    
    * fixed get my uin issue
    
    * added junit
    
    * return string instead of uri
    
    * renamed method
    
    * fixed data share url check
    
    * added logger
    
    * updated logger from error to info
    
    * added coverage for download masterdata service impl
    
    * added coverage for credential service impl
    
    * formatting
    
    * removed unused code
    
    * removed unused code
    
    * added author and formatting
    
    * fixed resident otp service test
    
    * added coverage for verification service impl
    
    * covered one exception
    
    * used constant and add test case
    
    * fixed one condition
    
    * added test cases and formatting
    
    * formatting
    
    * added junits for notification service
    
    * sonar bug
    
    * sonar coverage
    
    * removed unused property
    
    * removed unused method
    
    * fixed response issue
    
    * multilingual for event status
    
    * removed redundant DB call
    
    * enhance the code
    
    * removed unused code
    
    * added aspect for db logging
    
    * fixed read status issue
    
    * fixed pin unpin db calls
    
    * fixed pin unpin db calls
    
    ---------
    
    Co-authored-by: Ritik Jain <M1074108@mindtree.com>
    Signed-off-by: Ritik Jain <M1074108@mindtree.com>

--- resident/resident-service/src/test/java/io/mosip/resident/service/impl/PinUnpinStatusServiceTest.java
+++ resident/resident-service/src/test/java/io/mosip/resident/service/impl/PinUnpinStatusServiceTest.java
@@ -2,8 +2,6 @@ package io.mosip.resident.service.impl;
 
 import static org.junit.Assert.assertEquals;
 
-import java.util.Optional;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -18,7 +16,6 @@ import org.springframework.test.context.ContextConfiguration;
 import io.mosip.kernel.core.http.ResponseWrapper;
 import io.mosip.resident.constant.ResidentErrorCode;
 import io.mosip.resident.dto.ResponseDTO;
-import io.mosip.resident.entity.ResidentTransactionEntity;
 import io.mosip.resident.exception.ResidentServiceCheckedException;
 import io.mosip.resident.repository.ResidentTransactionRepository;
 import io.mosip.resident.service.PinUnpinStatusService;
@@ -39,13 +36,9 @@ public class PinUnpinStatusServiceTest {
     @Mock
     ResidentTransactionRepository residentTransactionRepository;
 
-    Optional<ResidentTransactionEntity> residentTransactionEntity;
     @Before
     public void setup(){
-        residentTransactionEntity = Optional.of(new ResidentTransactionEntity());
-        residentTransactionEntity.get().setEventId("eventId");
-        residentTransactionEntity.get().setPinnedStatus(true);
-        Mockito.when(residentTransactionRepository.findById(Mockito.anyString())).thenReturn(residentTransactionEntity);
+        Mockito.when(residentTransactionRepository.existsById(Mockito.anyString())).thenReturn(true);
     }
 
     @Test
@@ -56,8 +49,7 @@ public class PinUnpinStatusServiceTest {
 
     @Test
     public void pinStatusFailureTest() throws ResidentServiceCheckedException{
-        residentTransactionEntity = Optional.empty();
-        Mockito.when(residentTransactionRepository.findById(Mockito.anyString())).thenReturn(residentTransactionEntity);
+        Mockito.when(residentTransactionRepository.existsById(Mockito.anyString())).thenReturn(false);
         ResponseWrapper<ResponseDTO> responseDTO = pinUnpinStatusService.pinStatus("eventId", true);
         assertEquals(responseDTO.getErrors().get(0).getErrorCode(), ResidentErrorCode.EVENT_STATUS_NOT_FOUND.getErrorCode());
     }
@@ -70,8 +62,7 @@ public class PinUnpinStatusServiceTest {
 
     @Test
     public void unPinStatusFailureTest() throws ResidentServiceCheckedException{
-        residentTransactionEntity = Optional.empty();
-        Mockito.when(residentTransactionRepository.findById(Mockito.anyString())).thenReturn(residentTransactionEntity);
+        Mockito.when(residentTransactionRepository.existsById(Mockito.anyString())).thenReturn(false);
         ResponseWrapper<ResponseDTO> responseDTO = pinUnpinStatusService.pinStatus("eventId", false);
         assertEquals(responseDTO.getErrors().get(0).getErrorCode(), ResidentErrorCode.EVENT_STATUS_NOT_FOUND.getErrorCode());
     }
