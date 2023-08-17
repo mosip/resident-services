af67b8adc667482990bf9853b2fcc20a83277d9c
RitikJain4108 <99730411+RitikJain4108@users.noreply.github.com>
2023-08-17 17:07:12 +0530

    added print statement for debugging (#1057)
    * pageable testing
    
    * added count query
    
    * added print statement for debugging in dev
    
    * added logger
    
    * added logger
    
    ---------
    
    Co-authored-by: Ritik Jain <M1074108@mindtree.com>
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

--- resident/resident-service/src/main/java/io/mosip/resident/service/impl/ResidentServiceImpl.java
+++ resident/resident-service/src/main/java/io/mosip/resident/service/impl/ResidentServiceImpl.java
@@ -1637,7 +1637,9 @@ public class ResidentServiceImpl implements ResidentService {
 		if(fromDateTime!=null && toDateTime!=null){
 			dateTimeTuple2= getDateQuery(fromDateTime, toDateTime, timeZoneOffset);
 		}
+		logger.debug("==========testing pageStart====>> "+pageStart+"==========testing pageFetch====>> "+pageFetch);
 		Pageable pageable = PageRequest.of(pageStart, pageFetch, Sort.by(Sort.Direction.DESC, "pinnedStatus", "crDtimes"));
+		logger.debug("==========testing pageable=========="+pageable.toString()+"==========testing pageable==========");
 		if (statusFilter != null && searchText != null){
 			return Tuples.of(residentTransactionRepository.findByTokenIdInStatusSearchEventId(idaToken, pageFetch,
 							(pageStart) * pageFetch,
