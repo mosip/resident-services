30d915ab7865a8e22e714eb48b989881510b22b9
kameshsr <47484458+kameshsr@users.noreply.github.com>
2023-08-29 13:17:19 +0530

    MOSIP-29105 Fixed automation issue. (#1069)
    * Fixed service history issue.
    
    * MOSIP-27716 Moved identity-data-formatter.mvel to credentialdata.mvel.
    
    * Fixed Null pointer exception in notification method.
    
    * MOSIP-28516 Removed redundant code for download personalized card.
    
    * MOSIP-28516 Removed redundant code for contact details send otp.
    
    * MOSIP-28516 Removed extra variable in sendNotificationV2.
    
    * Fix Profile api issue in resident.
    
    * MOSIP-29099 Fix Update UIN issue.
    
    * MOSIP-29099 Fix Update UIN issue.
    
    * MOSIP-29105 Fixed automation issue.
    
    * MOSIP-29105 Fixed automation issue.
    
    * MOSIP-29105 Fixed automation issue.

--- resident/resident-service/src/main/java/io/mosip/resident/constant/ResidentErrorCode.java
+++ resident/resident-service/src/main/java/io/mosip/resident/constant/ResidentErrorCode.java
@@ -69,7 +69,7 @@ public enum ResidentErrorCode {
 	IDVID_NOT_MATCH_TO_SESSION("RES-SER-452", "Provided individualId does not belong to the logged in session."),
 	UNAUTHORIZED("RES-ATH-401", "Authentication Failed"),
 	VID_NOT_BELONG_TO_INDIVITUAL("RES-SER-453", "Provided VID does not belong to the individualId."),
-	VID_NOT_BELONG_TO_SESSION("RES-SER-454", "Provided VID does not belong to the logged in session."),
+	VID_NOT_BELONG_TO_USER("RES-SER-454", "Provided VID does not belong to the logged in user."),
 	RID_NOT_FOUND("RES-SER-456", "RID not found"), AID_NOT_FOUND("RES-SER-457", "AID not found"),
 	CREDENTIAL_REQUEST_ID_NOT_FOUND("RES-SER-458", "Credential request id not found"),
 	CREDENTIAL_REQUEST_NOT_FOUND("RES-SER-459", "Credential request not found"),
