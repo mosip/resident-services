9b3acc3ba6a055713bbfc9795ddd88bd827e5213
RitikJain4108 <99730411+RitikJain4108@users.noreply.github.com>
2023-08-30 18:12:28 +0530

    MOSIP-28772-api-total-pages-is-not-correct-when-total-items-value-is-multiple-of-10 and did formatting (#1063)
    * updated package
    
    * updated package of validation test calsses
    
    * updated
    
    * formatting
    
    * updated packages of controller test classes
    
    * updated package of service impl test classes
    
    * updated package of util test classes
    
    * updated package of aspect test classes
    
    * updated package of config test classes
    
    * updated package of handler test classes
    
    * added some exception test cases
    
    * added coverage for vid controller
    
    * added junit for configservice impl
    
    * added junits for config service impl
    
    * formatting
    
    * updated test cases
    
    * fixed get my uin issue
    
    * added junit
    
    * return string instead of uri
    
    * renamed method
    
    * fixed data share url check
    
    * added logger
    
    * updated logger from error to info
    
    * added coverage for download masterdata service impl
    
    * added coverage for credential service impl
    
    * formatting
    
    * removed unused code
    
    * removed unused code
    
    * added author and formatting
    
    * fixed resident otp service test
    
    * added coverage for verification service impl
    
    * covered one exception
    
    * used constant and add test case
    
    * fixed one condition
    
    * added test cases and formatting
    
    * formatting
    
    * added junits for notification service
    
    * sonar bug
    
    * sonar coverage
    
    * removed unused property
    
    * removed unused method
    
    * fixed response issue
    
    * multilingual for event status
    
    * removed redundant DB call
    
    * enhance the code
    
    * removed unused code
    
    * added aspect for db logging
    
    * fixed read status issue
    
    * fixed pin unpin db calls
    
    * fixed pin unpin db calls
    
    * removed unnecessary API call and DB call
    
    * added validation for partner id
    
    * fixed db call in verify phone email API
    
    * fixed db call in verify phone email API
    
    * removed get my id type
    
    * fetch specific column
    
    * fetch specific column
    
    * fetch specific column
    
    * fix card type issue
    
    * fixed query for download card from eventId
    
    * fixed query for download card from eventId
    
    * fixed query for service history with dates
    
    * updated queries to fetch specific data
    
    * fixed null case
    
    * fixed int
    
    * uncomment junit
    
    * update all native query
    
    * removed pageable
    
    * removed pageable
    
    * removed pageable
    
    * fixed total pages issue
    
    ---------
    
    Co-authored-by: Ritik Jain <M1074108@mindtree.com>
    Signed-off-by: Ritik Jain <M1074108@mindtree.com>

--- resident/resident-service/src/main/java/io/mosip/resident/repository/ResidentTransactionRepository.java
+++ resident/resident-service/src/main/java/io/mosip/resident/repository/ResidentTransactionRepository.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 
 import javax.transaction.Transactional;
 
-import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
