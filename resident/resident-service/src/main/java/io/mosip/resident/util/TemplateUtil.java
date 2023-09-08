63f5cdfb40b8a35f0b04e15d303c630255efce19
Ritik Jain <M1074108@mindtree.com>
2023-09-08 13:54:08 +0530

    removed one request type

--- resident/resident-service/src/main/java/io/mosip/resident/util/TemplateUtil.java
+++ resident/resident-service/src/main/java/io/mosip/resident/util/TemplateUtil.java
@@ -2,7 +2,6 @@ package io.mosip.resident.util;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
