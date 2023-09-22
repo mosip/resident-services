e2a0cb508e87c989196075c618683966d5be8a30
kameshsr <47484458+kameshsr@users.noreply.github.com>
2023-09-22 11:26:15 +0530

    MOSIP-29475 Created API /proxy/masterdata/locationHierarchyLevels (#1096)
    * Fixed service history issue.
    
    * MOSIP-27716 Moved identity-data-formatter.mvel to credentialdata.mvel.
    
    * Fixed Null pointer exception in notification method.
    
    * MOSIP-28516 Removed redundant code for download personalized card.
    
    * MOSIP-28516 Removed redundant code for contact details send otp.
    
    * MOSIP-28516 Removed extra variable in sendNotificationV2.
    
    * Fix Profile api issue in resident.
    
    * MOSIP-29099 Fix Update UIN issue.
    
    * MOSIP-29099 Fix Update UIN issue.
    
    * MOSIP-29105 Fixed automation issue.
    
    * MOSIP-29105 Fixed automation issue.
    
    * MOSIP-29105 Fixed automation issue.
    
    * MOSIP-29105 Fixed automation issue.
    
    * MOSIP-29105 Fixed automation issue.
    
    * MOSIP-29161 Improved sonar coverage for utility class.
    
    * MOSIP-29161 Improved sonar coverage for utility class.
    
    * MOSIP-29161 Improved sonar coverage for utility class.
    
    * MOSIP-29161 Improved sonar coverage for utility class.
    
    * MOSIP-29161 Improved sonar coverage for utility class.
    
    * MOSIP-29161 Improved sonar coverage for utility class.
    
    * MOSIP-29161 Improved sonar coverage for utility class.
    
    * MOSIP-29161 Improved sonar coverage for utility class.
    
    * MOSIP-29161 Improved sonar coverage for utility class.
    
    * MOSIP-29161 Improved sonar coverage for utility class.
    
    * MOSIP-29161 Improved sonar coverage for utility class.
    
    * MOSIP-29161 Improved sonar coverage for utility class.
    
    * MOSIP-29161 Improved sonar coverage for utility class.
    
    * MOSIP-29161 Improved sonar coverage for utility class.
    
    * MOSIP-29161 Improved sonar coverage for Utilities class.
    
    * MOSIP-29161 Improved sonar coverage for Utilities class.
    
    * MOSIP-29161 Improved sonar coverage for Utilities class.
    
    * MOSIP-29161 Improved sonar coverage for Utilities class.
    
    * MOSIP-29161 Improved sonar coverage for Utilities class.
    
    * MOSIP-29161 Improved sonar coverage for ProxyPartnerManagementService class.
    
    * MOSIP-29161 Improved sonar coverage for ProxyPartnerManagementService class.
    
    * MOSIP-29161 Improved sonar coverage for ApiExceptionHandler class.
    
    * MOSIP-29161 Improved sonar coverage for ApiExceptionHandler class.
    
    * MOSIP-29161 Improved sonar coverage for ApiExceptionHandler class.
    
    * Revert "MOSIP-29161 Removed sonar exclusion for exception package. (#1078)"
    
    This reverts commit 9a7e1b9ccbdb7bc9ec6cf4b54ec4c4215c578566.
    
    * Increased machine_type column length
    
    * MOSIP-29475 Created Proxy API /proxy/masterdata/locationHierarchyLevels
    
    * MOSIP-29475 Created test case for API /proxy/masterdata/locationHierarchyLevels
    
    * MOSIP-29475 Created test case for API /proxy/masterdata/locationHierarchyLevels
    
    * MOSIP-29475 Created test case for API /proxy/masterdata/locationHierarchyLevels
    
    * MOSIP-29475 Removed unused import of ProxyMasterdataController.
    
    * MOSIP-29473 Added API /proxy/masterdata/dynamicfields/all/{fieldName}.
    
    * MOSIP-29473 Added API /proxy/masterdata/dynamicfields/all/{fieldName}.
    
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

--- resident/resident-service/src/main/java/io/mosip/resident/util/EventEnum.java
+++ resident/resident-service/src/main/java/io/mosip/resident/util/EventEnum.java
@@ -229,6 +229,13 @@ public enum EventEnum {
 	GET_LOCATION_HIERARCHY_LEVEL_EXCEPTION("RES-SER-148", RegistrationConstants.SYSTEM, "get location hierarchy levels: Failed",
 			"get location hierarchy level by lang code is failed", "RES-SER", "Residence service", "RS-LOC",
 			"Location", RegistrationConstants.RESIDENT_APPLICATION_ID, RegistrationConstants.RESIDENT_APPLICATION_NAME),
+
+	GET_LOCATION_HIERARCHY_LEVEL_ALL_LANG_SUCCESS("RES-SER-147", RegistrationConstants.SYSTEM, "get location hierarchy levels for all language: Success",
+			"get location hierarchy level is succeed", "RES-SER", "Residence service", "RS-LOC",
+			"Location", RegistrationConstants.RESIDENT_APPLICATION_ID, RegistrationConstants.RESIDENT_APPLICATION_NAME),
+	GET_LOCATION_HIERARCHY_LEVEL_ALL_LANG_EXCEPTION("RES-SER-148", RegistrationConstants.SYSTEM, "get location hierarchy levels for all language: Failed",
+			"get location hierarchy level is failed", "RES-SER", "Residence service", "RS-LOC",
+			"Location", RegistrationConstants.RESIDENT_APPLICATION_ID, RegistrationConstants.RESIDENT_APPLICATION_NAME),
 	
 	GET_IMMEDIATE_CHILDREN_SUCCESS("RES-SER-150", RegistrationConstants.SYSTEM, "get immediate children: Success",
 			"get immediate children by location code and lang code is succeed", "RES-SER", "Residence service", "RS-CHILD",
@@ -700,7 +707,13 @@ public enum EventEnum {
 	VERIFICATION_STATUS_FAILURE("RES-SER-318", RegistrationConstants.SYSTEM, "Verification status: Failed",
 			"Verification status is failed", "RES-SER", "Residence service", "RS-VER", "Verification section",
 			RegistrationConstants.RESIDENT_APPLICATION_ID, RegistrationConstants.RESIDENT_APPLICATION_NAME),
-	;
+	GET_ALL_DYNAMIC_FIELD_VALUE_EXCEPTION("RES-SER-148", RegistrationConstants.SYSTEM, "get all dynamic field value: Failed",
+			"get all dynamic field value is failed", "RES-SER", "Residence service", "RS-DYNAMIC",
+			"Dynamic field", RegistrationConstants.RESIDENT_APPLICATION_ID, RegistrationConstants.RESIDENT_APPLICATION_NAME),
+
+	GET_ALL_DYNAMIC_FIELD_VALUE_SUCCESS("RES-SER-150", RegistrationConstants.SYSTEM, "get all dynamic field value: Success",
+			"get all dynamic field value is succeed", "RES-SER", "Residence service", "RS-DYNAMIC",
+			"Dynamic field", RegistrationConstants.RESIDENT_APPLICATION_ID, RegistrationConstants.RESIDENT_APPLICATION_NAME);
 	
 
 
