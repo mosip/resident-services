836346f6f444840aae265c6d8f14d0de1f6f75e7
Loganathan Sekar <42532387+LoganathanSekar7627@users.noreply.github.com>
2023-11-20 19:46:36 +0530

    MOSIP-28816 add timed metrics for repos (#1158)
    * Adding timed annotation for repo-wip
    
    Signed-off-by: Loganathan Sekar <m1047627@mindtree.com>
    
    * Correction to audit description
    
    Signed-off-by: Loganathan Sekar <m1047627@mindtree.com>
    
    * Reverted config change for timed aspect
    
    Signed-off-by: Loganathan Sekar <m1047627@mindtree.com>
    
    * Reverted repo code
    
    Signed-off-by: Loganathan Sekar <m1047627@mindtree.com>
    
    * Added aspect for db query time logging
    
    Signed-off-by: Loganathan Sekar <m1047627@mindtree.com>
    
    * Updated comment
    
    Signed-off-by: Loganathan Sekar <m1047627@mindtree.com>
    
    * Comment
    
    Signed-off-by: Loganathan Sekar <m1047627@mindtree.com>
    
    * Corrected to have short signature
    
    Signed-off-by: Loganathan Sekar <m1047627@mindtree.com>
    
    ---------
    
    Signed-off-by: Loganathan Sekar <m1047627@mindtree.com>
    Co-authored-by: Loganathan Sekar <m1047627@mindtree.com>
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

--- resident/resident-service/src/main/java/io/mosip/resident/aspect/DatabaseMetricsAspect.java
+++ resident/resident-service/src/main/java/io/mosip/resident/aspect/DatabaseMetricsAspect.java
@@ -19,7 +19,7 @@ import io.micrometer.core.instrument.Timer;
 /**
  * Aspect class for database metrics to record the query time metrics
  * 
- * @author Ritik Jain
+ * @author Loganathan S
  */
 @Component
 @Aspect
@@ -38,8 +38,7 @@ public class DatabaseMetricsAspect {
 	public Object aroundAdvice(ProceedingJoinPoint jp) throws Throwable {
         Timer timer = Timer.builder(DB_QUERY_RESPONSE_TIME_ID)
         		.tag("label", DB_QUERY_RESPONSE_TIME_DESCRIPTION)
-        		.tag("method", jp.getSignature().toLongString())
-        		.tag("target", jp.getTarget().getClass().getCanonicalName())
+        		.tag("queryMethod", jp.getSignature().toShortString())
         		.register(registry);
         long start = System.nanoTime();
         Object result = jp.proceed();
