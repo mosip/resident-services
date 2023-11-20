3cb3735d821b1bf1e0a7931dbacdac18daa91113
Loganathan Sekar <m1047627@mindtree.com>
2023-11-20 19:07:46 +0530

    Reverted repo code
    Signed-off-by: Loganathan Sekar <m1047627@mindtree.com>

--- resident/resident-service/src/main/java/io/mosip/resident/repository/ResidentSessionRepository.java
+++ resident/resident-service/src/main/java/io/mosip/resident/repository/ResidentSessionRepository.java
@@ -1,8 +1,5 @@
 package io.mosip.resident.repository;
 
-import static io.mosip.resident.constant.ResidentConstants.*;
-import static io.mosip.resident.constant.ResidentConstants.API_RESPONSE_TIME_ID;
-
 import java.util.List;
 import java.util.Optional;
 
@@ -10,7 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import io.micrometer.core.annotation.Timed;
 import io.mosip.resident.entity.ResidentSessionEntity;
 
 /**
@@ -24,7 +20,6 @@ import io.mosip.resident.entity.ResidentSessionEntity;
 public interface ResidentSessionRepository extends JpaRepository<ResidentSessionEntity, String> {
 	Optional<ResidentSessionEntity> findById(String id);
 	
-	@Timed(value=DB_QUERY_RESPONSE_TIME_ID,description=DB_QUERY_RESPONSE_TIME_DESCRIPTION, extraTags = {"repo" , "ResidentSessionRepository" , "queryName", "io.mosip.resident.repository.ResidentSessionRepository.findFirst2ByIdaTokenOrderByLoginDtimesDesc(String)"}, percentiles = {0.5, 0.9, 0.95, 0.99} )
 	List<ResidentSessionEntity> findFirst2ByIdaTokenOrderByLoginDtimesDesc(String idaToken);
 
 	Optional<ResidentSessionEntity> findFirstByIdaTokenOrderByLoginDtimesDesc(String idaToken);
