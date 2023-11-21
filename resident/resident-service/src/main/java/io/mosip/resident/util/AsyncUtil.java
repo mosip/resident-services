f44e62b12c67fdae661231afacb9d57b99bc899c
Loganathan Sekar <m1047627@mindtree.com>
2023-11-21 19:48:04 +0530

    Async audit and notification logging issues fiix
    Signed-off-by: Loganathan Sekar <m1047627@mindtree.com>

--- resident/resident-service/src/main/java/io/mosip/resident/util/AsyncUtil.java
+++ resident/resident-service/src/main/java/io/mosip/resident/util/AsyncUtil.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class AsyncUtil {
 	
-	@Async
+	@Async("AsyncExecutor")
 	public void asyncRun(Runnable runnable) {
 		runnable.run();
 	}
