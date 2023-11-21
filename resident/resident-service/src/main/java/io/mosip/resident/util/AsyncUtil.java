816a12445e655cea45016c8a06440b43ac1999ed
Loganathan Sekar <42532387+LoganathanSekar7627@users.noreply.github.com>
2023-11-21 17:16:49 +0530

    Mosip 28816 add timed metrics for repos (#1162)
    * Adding timed annotation for repo-wip
    
    Signed-off-by: Loganathan Sekar <m1047627@mindtree.com>
    
    * Correction to audit description
    
    Signed-off-by: Loganathan Sekar <m1047627@mindtree.com>
    
    * Reverted config change for timed aspect
    
    Signed-off-by: Loganathan Sekar <m1047627@mindtree.com>
    
    * Reverted repo code
    
    Signed-off-by: Loganathan Sekar <m1047627@mindtree.com>
    
    * Added aspect for db query time logging
    
    Signed-off-by: Loganathan Sekar <m1047627@mindtree.com>
    
    * Updated comment
    
    Signed-off-by: Loganathan Sekar <m1047627@mindtree.com>
    
    * Comment
    
    Signed-off-by: Loganathan Sekar <m1047627@mindtree.com>
    
    * Corrected to have short signature
    
    Signed-off-by: Loganathan Sekar <m1047627@mindtree.com>
    
    * Added aspect for resident rest client-WIP
    
    Signed-off-by: Loganathan Sekar <m1047627@mindtree.com>
    
    * Added rest template metrics interceptor
    
    Signed-off-by: Loganathan Sekar <m1047627@mindtree.com>
    
    * Code fix
    
    Signed-off-by: Loganathan Sekar <m1047627@mindtree.com>
    
    * Added interceptor for both resttemplate beans
    
    Signed-off-by: Loganathan Sekar <m1047627@mindtree.com>
    
    * Added percentile histogram
    
    Signed-off-by: Loganathan Sekar <m1047627@mindtree.com>
    
    * Added service tag
    
    Signed-off-by: Loganathan Sekar <m1047627@mindtree.com>
    
    * Added fix for audit util issues
    
    Signed-off-by: Loganathan Sekar <m1047627@mindtree.com>
    
    * Junit fix
    
    Signed-off-by: Loganathan Sekar <m1047627@mindtree.com>
    
    * Junit fix
    
    Signed-off-by: Loganathan Sekar <m1047627@mindtree.com>
    
    * Added missing file
    
    Signed-off-by: Loganathan Sekar <m1047627@mindtree.com>
    
    ---------
    
    Signed-off-by: Loganathan Sekar <m1047627@mindtree.com>
    Co-authored-by: Loganathan Sekar <m1047627@mindtree.com>

---
+++ resident/resident-service/src/main/java/io/mosip/resident/util/AsyncUtil.java
@@ -0,0 +1,21 @@
+package io.mosip.resident.util;
+
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+/**
+ * Since @Async on the AuditUtil class is causing bean loading issue, this async
+ * util is created to delegate the Async bean creation to this class.
+ * 
+ * @author Loganathan S
+ *
+ */
+@Component
+public class AsyncUtil {
+	
+	@Async
+	public void asyncRun(Runnable runnable) {
+		runnable.run();
+	}
+
+}
