0b51777decc11be3025a829a39b95fb6faeaea9e
kameshsr <kameshsr1338@gmail.com>
2023-12-06 14:14:08 +0530

    MOSIP-30609 Removed duplicate code from ProxyIdRepoServiceImpl class.
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

--- resident/resident-service/src/main/java/io/mosip/resident/service/impl/ProxyIdRepoServiceImpl.java
+++ resident/resident-service/src/main/java/io/mosip/resident/service/impl/ProxyIdRepoServiceImpl.java
@@ -1,13 +1,9 @@
 package io.mosip.resident.service.impl;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.mosip.kernel.core.http.ResponseWrapper;
 import io.mosip.kernel.core.logger.spi.Logger;
 import io.mosip.resident.config.LoggerConfiguration;
 import io.mosip.resident.constant.ApiName;
-import io.mosip.resident.constant.MappingJsonConstants;
-import io.mosip.resident.dto.AttributeListDto;
-import io.mosip.resident.dto.UpdateCountDto;
 import io.mosip.resident.exception.ApisResourceAccessException;
 import io.mosip.resident.exception.ResidentServiceCheckedException;
 import io.mosip.resident.service.ProxyIdRepoService;
@@ -16,8 +12,6 @@ import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -42,12 +36,6 @@ public class ProxyIdRepoServiceImpl implements ProxyIdRepoService {
 	@Autowired
 	private IdentityServiceImpl identityServiceImpl;
 
-	@Autowired
-	private ResidentConfigServiceImpl residentConfigService;
-
-	@Autowired
-	private ObjectMapper objectMapper;
-
 	@Override
 	public ResponseWrapper<?> getRemainingUpdateCountByIndividualId(List<String> attributeList)
 			throws ResidentServiceCheckedException {
@@ -66,12 +54,6 @@ public class ProxyIdRepoServiceImpl implements ProxyIdRepoService {
 			ResponseWrapper<?> responseWrapper = residentServiceRestClient.getApi(ApiName.IDREPO_IDENTITY_UPDATE_COUNT,
 					pathsegements, queryParamName, queryParamValue, ResponseWrapper.class);
 
-			if (responseWrapper.getErrors() != null && !responseWrapper.getErrors().isEmpty()) {
-				ResponseWrapper<AttributeListDto> listDtoResponseWrapper = new ResponseWrapper<>();
-				listDtoResponseWrapper.setResponse(getRemainingUpdateCountFromConfig(attributeList));
-				logger.debug("ProxyIdRepoServiceImpl::getRemainingUpdateCountByIndividualId()::exit");
-				return listDtoResponseWrapper;
-			}
 			logger.debug("ProxyIdRepoServiceImpl::getRemainingUpdateCountByIndividualId()::exit");
 			return responseWrapper;
 			
@@ -79,34 +61,7 @@ public class ProxyIdRepoServiceImpl implements ProxyIdRepoService {
 			logger.error(ExceptionUtils.getStackTrace(e));
 			throw new ResidentServiceCheckedException(API_RESOURCE_ACCESS_EXCEPTION.getErrorCode(),
 					API_RESOURCE_ACCESS_EXCEPTION.getErrorMessage(), e);
-		} catch (IOException e) {
-			throw new RuntimeException(e);
 		}
 	}
 
-	private AttributeListDto getRemainingUpdateCountFromConfig(List<String> attributeList) throws ResidentServiceCheckedException, IOException {
-		String identityMapping = residentConfigService.getIdentityMapping();
-		Map<String, Object> identityMappingMap = objectMapper
-				.readValue(identityMapping.getBytes(StandardCharsets.UTF_8), Map.class);
-		Object identityObj = identityMappingMap.get(MappingJsonConstants.ATTRIBUTE_UPDATE_COUNT_LIMIT);
-		Map<String, Object> attributeUpdateCountLimit = (Map<String, Object>) identityObj;
-		AttributeListDto attributeListDto= new AttributeListDto();
-		List<UpdateCountDto> updateCountDtoList = new ArrayList<>();
-		if(attributeList == null || attributeList.isEmpty()){
-			attributeUpdateCountLimit.keySet().forEach(key ->
-					addAttributeInUpdateCountDtoList(key, attributeUpdateCountLimit, updateCountDtoList));
-		} else {
-			attributeList.stream()
-					.filter(attributeUpdateCountLimit::containsKey)
-					.forEach(attribute -> addAttributeInUpdateCountDtoList(attribute, attributeUpdateCountLimit, updateCountDtoList));
-		}
-		attributeListDto.setAttributes(updateCountDtoList);
-		return attributeListDto;
-	}
-	private void addAttributeInUpdateCountDtoList(String key, Map<String, Object> attributeUpdateCountLimit, List<UpdateCountDto> updateCountDtoList){
-		UpdateCountDto updateCountDto= new UpdateCountDto();
-		updateCountDto.setAttributeName(key);
-		updateCountDto.setNoOfUpdatesLeft((Integer) attributeUpdateCountLimit.get(key));
-		updateCountDtoList.add(updateCountDto);
-	}
 }
