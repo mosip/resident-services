facdcd860e4387cf3106551e79627e1747df726b
kameshsr <kameshsr1338@gmail.com>
2024-01-18 15:46:53 +0530

    MOSIP-31215 Added old error code for old req credential API
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

--- resident/resident-service/src/main/java/io/mosip/resident/service/impl/ResidentCredentialServiceImpl.java
+++ resident/resident-service/src/main/java/io/mosip/resident/service/impl/ResidentCredentialServiceImpl.java
@@ -139,7 +139,7 @@ public class ResidentCredentialServiceImpl implements ResidentCredentialService 
 	
 	@Override
 	public ResidentCredentialResponseDto reqCredential(ResidentCredentialRequestDto dto)
-			throws ResidentServiceCheckedException {
+			throws ResidentServiceCheckedException, OtpValidationFailedException {
 		Map<String, Object> additionalAttributes = new HashMap<>();
 		try {
 			if (idAuthService.validateOtp(dto.getTransactionID(), dto.getIndividualId(), dto.getOtp())) {
@@ -156,8 +156,7 @@ public class ResidentCredentialServiceImpl implements ResidentCredentialService 
 		} catch (OtpValidationFailedException e) {
 			trySendNotification(dto.getIndividualId(), NotificationTemplateCode.RS_CRE_REQ_FAILURE,
 					additionalAttributes);
-			throw new ResidentServiceException(ResidentErrorCode.OTP_VALIDATION_FAILED.getErrorCode(), e.getErrorText(),
-					e);
+			throw e;
 		}
 	}
 
