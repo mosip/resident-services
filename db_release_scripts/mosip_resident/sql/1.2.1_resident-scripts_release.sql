51dd6c90dbeef8745a227d6fc1f83e5cba52a07f
kameshsr <kameshsr1338@gmail.com>
2024-02-09 12:01:55 +0530

    MOSIP-31513 Added upgrade script
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

---
+++ db_release_scripts/mosip_resident/sql/1.2.1_resident-scripts_release.sql
@@ -0,0 +1,34 @@
+-- -------------------------------------------------------------------------------------------------
+-- Database Name: mosip_resident
+-- Release Version 	: 1.2.1
+-- Purpose    		: Database scripts for Resident Service DB.       
+-- Create By   		: Kamesh Shekhar Prasad
+-- Created Date		: February-2024
+-- 
+-- Modified Date        Modified By             Comments / Remarks
+-- --------------------------------------------------------------------------------------------------
+
+-----------------------------------------------------------------------------------------------------
+\c mosip_resident sysadmin
+
+\ir ddl/otp_transaction.sql
+\ir ddl/resident_transaction.sql
+\ir ddl/resident_grievance_ticket.sql
+\ir ddl/resident_user_actions.sql
+\ir ddl/resident_sessions.sql
+
+GRANT SELECT, INSERT, REFERENCES, UPDATE, DELETE
+ON resident.otp_transaction
+TO residentuser;
+
+GRANT SELECT, INSERT, REFERENCES, UPDATE, DELETE
+ON resident.resident_grievance_ticket
+TO residentuser;
+
+ALTER TABLE resident.resident_session alter column machine_type type varchar(100)
+-- Adding index to ida_token column
+CREATE INDEX idx_resident_user_actions_ida_token ON resident.resident_user_actions (ida_token);
+GRANT SELECT, INSERT, REFERENCES, UPDATE, DELETE
+   ON resident.resident_user_actions
+   TO residentuser;
+-----------------------------------------------------------------------------------------------------
