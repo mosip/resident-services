393a3810af72c20c64876dd7faacd809da3b01d6
kameshsr <kameshsr1338@gmail.com>
2024-02-09 14:12:09 +0530

    MOSIP-31513 Updated revoke.sql
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

--- db_release_scripts/mosip_resident/sql/1.2.1_resident-scripts_revoke.sql
+++ db_release_scripts/mosip_resident/sql/1.2.1_resident-scripts_revoke.sql
@@ -10,9 +10,14 @@
 -----------------------------------------------------------------------------------------------------
 \c mosip_resident sysadmin
 
-DROP TABLE IF EXISTS resident.otp_transaction;
-DROP TABLE IF EXISTS resident.resident_transaction;
-DROP TABLE IF EXISTS resident.resident_session;
-DROP TABLE IF EXISTS resident.resident_user_actions;
+DROP INDEX IF EXISTS idx_resident_transaction_event_id;
+DROP INDEX IF EXISTS idx_resident_transaction_token_id;
+DROP INDEX IF EXISTS idx_resident_transaction_credential_request_id;
+DROP INDEX IF EXISTS idx_resident_transaction_request_dtimes;
+DROP INDEX IF EXISTS idx_resident_transaction_request_trn_id;
+DROP INDEX IF EXISTS idx_resident_transaction_ref_id;
+DROP INDEX IF EXISTS idx_resident_user_actions_ida_token;
+DROP INDEX IF EXISTS idx_resident_session_ida_token;
+DROP INDEX IF EXISTS idx_resident_transaction_read_status;
 
 -----------------------------------------------------------------------------------------------------
