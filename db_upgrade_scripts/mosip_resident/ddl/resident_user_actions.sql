9f7a52dc05038d802a50b79b0092a3be81d9b33f
kameshsr <47484458+kameshsr@users.noreply.github.com>
2024-03-19 16:18:12 +0530

    MOSIP-31981 Added db upgrade scrict (#1213)
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

---
+++ db_upgrade_scripts/mosip_resident/ddl/resident_user_actions.sql
@@ -0,0 +1,23 @@
+-- -------------------------------------------------------------------------------------------------
+-- Database Name:    mosip_resident
+-- Release Version 	: 1.2.1
+-- Purpose    		: Database scripts for Resident Service DB.
+-- Create By   		: Kamesh Shekhar Prasad
+-- Created Date		: Aug-2022
+--
+-- Modified Date        Modified By         Comments / Remarks
+-- --------------------------------------------------------------------------------------------------
+--
+-----------------------------------------------------------------------------------------------------
+
+-- This Table is used to save the  user actions for the user actions table.
+
+CREATE TABLE resident.resident_user_actions(
+    ida_token character varying(128) NOT NULL,
+    last_bell_notif_click_dtimes timestamp,
+    CONSTRAINT pk_ida_token PRIMARY KEY (ida_token)
+);
+
+COMMENT ON TABLE resident_user_actions IS 'This Table is used to save the  user actions';
+COMMENT ON COLUMN resident_user_actions.ida_token IS 'The unique identifier for each user';
+COMMENT ON COLUMN resident_user_actions.last_bell_notif_click_dtimes IS 'The time when the user last clicked on the bell notification';
