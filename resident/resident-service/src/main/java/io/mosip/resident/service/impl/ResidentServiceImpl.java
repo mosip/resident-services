135925329cafa955c4e3c61663075a86d9f2881e
kameshsr <47484458+kameshsr@users.noreply.github.com>
2024-03-20 15:09:38 +0530

    MOSIP-31848 Fixed update uin issue (#1218)
    * MOSIP-32000 added null check for language not cached
    
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>
    
    * MOSIP-31848 Fixed update uin issue
    
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>
    
    * MOSIP-31848 Fixed update uin issue
    
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>
    
    * MOSIP-31848 Fixed update uin issue
    
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>
    
    * MOSIP-31848 Fixed update uin issue
    
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>
    
    * MOSIP-32000 Fixed update uin issue
    
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>
    
    ---------
    
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

--- resident/resident-service/src/main/java/io/mosip/resident/service/impl/ResidentServiceImpl.java
+++ resident/resident-service/src/main/java/io/mosip/resident/service/impl/ResidentServiceImpl.java
@@ -1756,6 +1756,8 @@ public class ResidentServiceImpl implements ResidentService {
 			templateType = TemplateType.SUCCESS;
 		} else if (statusCode.equalsIgnoreCase(EventStatusCanceled.CANCELED.name())) {
 			templateType = TemplateType.CANCELED;
+		}else if (residentTransactionEntity.getStatusCode().equalsIgnoreCase(EventStatusInProgress.IDENTITY_UPDATED.name())) {
+			templateType = TemplateType.REGPROC_SUCCESS;
 		} else {
 			templateType = TemplateType.FAILURE;
 		}
@@ -1775,6 +1777,8 @@ public class ResidentServiceImpl implements ResidentService {
 			templateType = TemplateType.SUCCESS;
 		} else if (statusCode.equalsIgnoreCase(EventStatusCanceled.CANCELED.name())) {
 			templateType = TemplateType.CANCELED;
+		} else if (residentTransactionEntity.getStatusCode().equalsIgnoreCase(EventStatusInProgress.IDENTITY_UPDATED.name())) {
+			templateType = TemplateType.REGPROC_SUCCESS;
 		} else {
 			return getDescriptionForLangCode(residentTransactionEntity, langCode, statusCode, requestType);
 		}
