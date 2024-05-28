ef3ce7cdeff5fd6c56ea4a654f9d1ac26bd1066b
kameshsr <kameshsr1338@gmail.com>
2024-05-28 18:18:07 +0530

    Fixed test case
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

--- resident/resident-service/src/test/java/io/mosip/resident/controller/AuthTransactionCallbackControllerTest.java
+++ resident/resident-service/src/test/java/io/mosip/resident/controller/AuthTransactionCallbackControllerTest.java
@@ -7,25 +7,21 @@ import static org.mockito.Mockito.verify;
 import java.time.LocalDateTime;
 import java.util.Map;
 
-import io.mosip.resident.util.Utility;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.client.RestTemplate;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -33,14 +29,7 @@ import io.mosip.kernel.core.websub.model.Event;
 import io.mosip.kernel.core.websub.model.EventModel;
 import io.mosip.resident.exception.ResidentServiceCheckedException;
 import io.mosip.resident.exception.ResidentServiceException;
-import io.mosip.resident.helper.ObjectStoreHelper;
 import io.mosip.resident.service.AuthTransactionCallBackService;
-import io.mosip.resident.service.DocumentService;
-import io.mosip.resident.service.IdAuthService;
-import io.mosip.resident.service.ProxyIdRepoService;
-import io.mosip.resident.service.ResidentVidService;
-import io.mosip.resident.service.VerificationService;
-import io.mosip.resident.service.impl.ResidentServiceImpl;
 import io.mosip.resident.test.ResidentTestBootApplication;
 import io.mosip.resident.util.AuditUtil;
 
@@ -51,15 +40,11 @@ import io.mosip.resident.util.AuditUtil;
  * @author Kamesh Shekhar Prasad
  */
 
-@RunWith(SpringRunner.class)
+@RunWith(MockitoJUnitRunner.class)
 @SpringBootTest(classes = ResidentTestBootApplication.class)
 @AutoConfigureMockMvc
 @TestPropertySource(locations = "classpath:application.properties")
 public class AuthTransactionCallbackControllerTest {
-
-	@MockBean
-	@Qualifier("selfTokenRestTemplate")
-	private RestTemplate residentRestTemplate;
 
 	@Mock
 	private AuditUtil audit;
@@ -67,40 +52,13 @@ public class AuthTransactionCallbackControllerTest {
 	@InjectMocks
 	AuthTransactionCallbackController authTransactionCallbackController;
 
-	@MockBean
+	@Mock
 	private AuthTransactionCallBackService authTransactionCallBackService;
 
 	@Autowired
 	private MockMvc mockMvc;
 
-	@MockBean
-	private VerificationService verificationService;
-
-	@MockBean
-	private IdAuthService idAuthService;
-
-	@MockBean
-	private ResidentVidService vidService;
-
-	@MockBean
-	private DocumentService docService;
-
-	@MockBean
-	private ObjectStoreHelper objectStore;
-
-	@MockBean
-	private ResidentServiceImpl residentService;
-
-	@MockBean
-	private ProxyIdRepoService proxyIdRepoService;
-
-	@MockBean
-	private Utility utility;
-
-	@InjectMocks
-	VerificationController verificationController;
-
-	@Autowired
+	@Mock
 	private ObjectMapper objectMapper;
 
 	private EventModel eventModel;
