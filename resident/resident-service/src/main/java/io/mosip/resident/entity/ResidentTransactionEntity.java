93d23ef4ad0e8ad410bf122daeb1fd98bb933540
kameshsr <kameshsr1338@gmail.com>
2024-07-26 18:58:46 +0530

    MOSIP-34712 Fixed Interceptor in resident service
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

--- resident/resident-service/src/main/java/io/mosip/resident/entity/ResidentTransactionEntity.java
+++ resident/resident-service/src/main/java/io/mosip/resident/entity/ResidentTransactionEntity.java
@@ -2,10 +2,7 @@ package io.mosip.resident.entity;
 
 import java.time.LocalDateTime;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 
 import lombok.AllArgsConstructor;
