a828dd91f2d05af7a03bed7dabe9cee5d1c76bdb
kameshsr <kameshsr1338@gmail.com>
2024-07-31 18:57:58 +0530

    MOSIP-34803 Added unit test case
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

--- resident/resident-service/src/test/java/io/mosip/resident/util/AttributeDisplayTextTest.java
+++ resident/resident-service/src/test/java/io/mosip/resident/util/AttributeDisplayTextTest.java
@@ -1,7 +1,9 @@
 package io.mosip.resident.util;
 
 import io.mosip.resident.constant.RequestType;
+import io.mosip.resident.constant.ResidentConstants;
 import io.mosip.resident.handler.service.ResidentConfigService;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -13,6 +15,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
 
 /**
  * @author Kamesh
@@ -27,6 +30,72 @@ public class AttributeDisplayTextTest {
     @Mock
     ResidentConfigService residentConfigService;
 
+    private Map<String, Map<String, Object>> mockUISchemaDataMap;
+
+    @Before
+    public void setUp() {
+        // Mock UI Schema Data
+        mockUISchemaDataMap = new HashMap<>();
+
+        Map<String, Object> attributeMap = new HashMap<>();
+        attributeMap.put(ResidentConstants.LABEL, "Mocked Label");
+
+        Map<String, String> formatOptionMap = new HashMap<>();
+        formatOptionMap.put("format1", "Formatted 1");
+        formatOptionMap.put("format2", "Formatted 2");
+
+        attributeMap.put(ResidentConstants.FORMAT_OPTION, formatOptionMap);
+
+        mockUISchemaDataMap.put("attr1", attributeMap);
+
+        // Mock ResidentConfigService response
+        when(residentConfigService.getUISchemaCacheableData(Mockito.anyString()))
+                .thenReturn(Map.of("en", mockUISchemaDataMap));
+    }
+
+    @Test
+    public void testGetAttributesDisplayText_withSchemaType() {
+        String attributesFromDB = "en:format1,format2";
+        String languageCode = "en";
+        RequestType requestType = RequestType.UPDATE_MY_UIN;
+
+        String result = attributesDisplayText.getAttributesDisplayText(attributesFromDB, languageCode, requestType);
+
+        assertEquals("en", result);
+    }
+
+    @Test
+    public void testGetAttributesDisplayText_withoutSchemaType() {
+        String attributesFromDB = "attr1";
+        String languageCode = "en";
+        RequestType requestType = RequestType.UPDATE_MY_UIN;
+        String result = attributesDisplayText.getAttributesDisplayText(attributesFromDB, languageCode, requestType);
+
+        assertEquals("Mocked Label", result);
+    }
+
+    @Test
+    public void testGetAttributesDisplayText_withNullAttributes() {
+        String attributesFromDB = null;
+        String languageCode = "en";
+        RequestType requestType = RequestType.UPDATE_MY_UIN;
+
+        String result = attributesDisplayText.getAttributesDisplayText(attributesFromDB, languageCode, requestType);
+
+        assertEquals("", result);
+    }
+
+    @Test
+    public void testGetAttributesDisplayText_withEmptyAttributes() {
+        String attributesFromDB = "";
+        String languageCode = "en";
+        RequestType requestType = RequestType.UPDATE_MY_UIN;
+
+        String result = attributesDisplayText.getAttributesDisplayText(attributesFromDB, languageCode, requestType);
+
+        assertEquals("", result);
+    }
+
     @Test
     public void testGetAttributesDisplayText(){
         Map<String, Map<String, Map<String, Object>>> keyMap = new HashMap<>();
@@ -35,7 +104,7 @@ public class AttributeDisplayTextTest {
         stringObjectMap.put("testKey", "testobj");
         attributeMap.put("key", stringObjectMap);
         keyMap.put("eng", attributeMap);
-        Mockito.when(residentConfigService.getUISchemaCacheableData(Mockito.anyString())).thenReturn(keyMap);
+        when(residentConfigService.getUISchemaCacheableData(Mockito.anyString())).thenReturn(keyMap);
         assertEquals("test",
                 attributesDisplayText.getAttributesDisplayText("test", "eng", RequestType.UPDATE_MY_UIN));
     }
