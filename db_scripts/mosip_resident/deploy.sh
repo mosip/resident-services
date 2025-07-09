3386189925166c773d1d03acb7bd89a592664d2c
kameshsr <kameshsr1338@gmail.com>
2025-07-09 17:59:27 +0530

    MOSIP-37812 corrected single quotes
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

--- db_scripts/mosip_resident/deploy.sh
+++ db_scripts/mosip_resident/deploy.sh
@@ -1,70 +1,72 @@
-
 ## Properties file
 set -e
 properties_file="$1"
 echo `date "+%m/%d/%Y %H:%M:%S"` ": $properties_file"
-if [ -f "$properties_file" ]
-then
-     echo `date "+%m/%d/%Y %H:%M:%S"` ": Property file \"$properties_file\" found."
-    while IFS='=' read -r key value
-    do
+
+if [ -f "$properties_file" ]; then
+    echo `date "+%m/%d/%Y %H:%M:%S"` ": Property file \"$properties_file\" found."
+    while IFS='=' read -r key value; do
         key=$(echo $key | tr '.' '_')
-         eval ${key}=\${value}
+        eval ${key}=\${value}
     done < "$properties_file"
 else
-     echo `date "+%m/%d/%Y %H:%M:%S"` ": Property file not found, Pass property file name as argument."
+    echo `date "+%m/%d/%Y %H:%M:%S"` ": Property file not found, Pass property file name as argument."
+    exit 1
 fi
 
 ## Terminate existing connections
 echo "Terminating active connections" 
-CONN=$(PGPASSWORD=$SU_USER_PWD psql -v ON_ERROR_STOP=1 --username=$SU_USER --host=$DB_SERVERIP --port=$DB_PORT --dbname=$DEFAULT_DB_NAME -t -c "SELECT count(pg_terminate_backend(pg_stat_activity.pid)) FROM pg_stat_activity WHERE datname = '$MOSIP_DB_NAME' AND pid <> pg_backend_pid()";exit;)
+CONN=$(PGPASSWORD=$SU_USER_PWD psql --set=ON_ERROR_STOP=1 --username=$SU_USER --host=$DB_SERVERIP --port=$DB_PORT \
+    --dbname=$DEFAULT_DB_NAME -t -c "SELECT count(pg_terminate_backend(pg_stat_activity.pid)) FROM pg_stat_activity WHERE datname = '$MOSIP_DB_NAME' AND pid <> pg_backend_pid();"; exit)
 echo "Terminated connections"
 
-## Drop db and role
+## Drop DB
 echo "Dropping DB"
 PGPASSWORD=$SU_USER_PWD psql --set=ON_ERROR_STOP=1 \
-  -v mosip_db_name=$MOSIP_DB_NAME \
-  --username=$SU_USER --host=$DB_SERVERIP --port=$DB_PORT \
-  --dbname=$DEFAULT_DB_NAME -f drop_db.sql
+    -v mosip_db_name=$MOSIP_DB_NAME \
+    --username=$SU_USER --host=$DB_SERVERIP --port=$DB_PORT \
+    --dbname=$DEFAULT_DB_NAME -f drop_db.sql
 
- 
+## Drop user
 echo "Dropping user"
 PGPASSWORD=$SU_USER_PWD psql --set=ON_ERROR_STOP=1 \
-  -v resident_user_name=\'$RESIDENT_USER_NAME\' \
-  --username=$SU_USER --host=$DB_SERVERIP --port=$DB_PORT \
-  --dbname=$DEFAULT_DB_NAME -f drop_role.sql
+    -v resident_user_name=$RESIDENT_USER_NAME \
+    --username=$SU_USER --host=$DB_SERVERIP --port=$DB_PORT \
+    --dbname=$DEFAULT_DB_NAME -f drop_role.sql
 
+## Create users
 echo `date "+%m/%d/%Y %H:%M:%S"` ": Creating database users" 
 PGPASSWORD=$SU_USER_PWD psql --set=ON_ERROR_STOP=1 \
-  -v resident_user_name=\'$RESIDENT_USER_NAME\' \
-  -v dbuserpwd=\'$DBUSER_PWD\' \
-  --username=$SU_USER --host=$DB_SERVERIP --port=$DB_PORT \
-  --dbname=$DEFAULT_DB_NAME -f role_dbuser.sql
+    -v resident_user_name=$RESIDENT_USER_NAME \
+    -v dbuserpwd=\'$DBUSER_PWD\' \
+    --username=$SU_USER --host=$DB_SERVERIP --port=$DB_PORT \
+    --dbname=$DEFAULT_DB_NAME -f role_dbuser.sql
 
 ## Create DB
 echo "Creating DB"
-PGPASSWORD=$SU_USER_PWD psql -v ON_ERROR_STOP=1 \
-  --username=$SU_USER --host=$DB_SERVERIP --port=$DB_PORT --dbname=$DEFAULT_DB_NAME \
-  -v mosip_db_name=\'$MOSIP_DB_NAME\' \
-  -v db_user=\'$DB_USER\' \
-  -f db.sql
- 
-PGPASSWORD=$SU_USER_PWD psql -v ON_ERROR_STOP=1 \
-  -v mosip_db_name=\'$MOSIP_DB_NAME\' \
-  --username=$SU_USER --host=$DB_SERVERIP --port=$DB_PORT --dbname=$DEFAULT_DB_NAME -f ddl.sql
+PGPASSWORD=$SU_USER_PWD psql --set=ON_ERROR_STOP=1 \
+    -v mosip_db_name=$MOSIP_DB_NAME \
+    -v db_user=$DB_USER \
+    --username=$SU_USER --host=$DB_SERVERIP --port=$DB_PORT \
+    --dbname=$DEFAULT_DB_NAME -f db.sql
+
+PGPASSWORD=$SU_USER_PWD psql --set=ON_ERROR_STOP=1 \
+    -v mosip_db_name=$MOSIP_DB_NAME \
+    --username=$SU_USER --host=$DB_SERVERIP --port=$DB_PORT \
+    --dbname=$DEFAULT_DB_NAME -f ddl.sql
 
 ## Grants
 PGPASSWORD=$SU_USER_PWD psql --set=ON_ERROR_STOP=1 \
-  -v mosip_db_name=\'$MOSIP_DB_NAME\' \
-  -v resident_user_name=\'$RESIDENT_USER_NAME\' \
-  -v schema_name=\'$SCHEMA_NAME\' \
-  --username=$SU_USER --host=$DB_SERVERIP --port=$DB_PORT \
-  --dbname=$DEFAULT_DB_NAME -f grants.sql
-
+    -v mosip_db_name=$MOSIP_DB_NAME \
+    -v resident_user_name=$RESIDENT_USER_NAME \
+    -v schema_name=$SCHEMA_NAME \
+    --username=$SU_USER --host=$DB_SERVERIP --port=$DB_PORT \
+    --dbname=$DEFAULT_DB_NAME -f grants.sql
 
 ## Populate tables
-if [ ${DML_FLAG} == 1 ]
-then
+if [ "$DML_FLAG" == "1" ]; then
     echo `date "+%m/%d/%Y %H:%M:%S"` ": Deploying DML for ${MOSIP_DB_NAME} database" 
-    PGPASSWORD=$SU_USER_PWD psql -v ON_ERROR_STOP=1 --username=$SU_USER --host=$DB_SERVERIP --port=$DB_PORT --dbname=$DEFAULT_DB_NAME -a -b -f dml.sql 
+    PGPASSWORD=$SU_USER_PWD psql --set=ON_ERROR_STOP=1 \
+        --username=$SU_USER --host=$DB_SERVERIP --port=$DB_PORT \
+        --dbname=$DEFAULT_DB_NAME -a -b -f dml.sql 
 fi
