e0f4dea3d3b74b0908485615e8e5b2a2c94e8c3c
kameshsr <kameshsr1338@gmail.com>
2025-11-21 11:56:57 +0530

    MOSIP-43064 corrected script
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

--- db_upgrade_scripts/mosip_resident/sql/1.2.1.2_to_1.3.0_upgrade.sql
+++ db_upgrade_scripts/mosip_resident/sql/1.2.1.2_to_1.3.0_upgrade.sql
@@ -1,6 +1,6 @@
 -- Below script is required to upgrade from 1.3.0-beta.1 to 1.3.0
 
-\c mosip_master
+\c mosip_resident
 
 CREATE INDEX idx_resident_transaction_aid_crdtime_desc
     ON resident.resident_transaction (aid, cr_dtimes DESC);
