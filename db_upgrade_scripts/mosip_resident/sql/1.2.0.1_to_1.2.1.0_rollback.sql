67de6c5a2369ff049f8e9e2d43705f1f0565922e
Nandhukumar <nandhukumare@gmail.com>
2025-11-27 16:56:24 +0530

    MOSIP-43834 | updated db scripts
    Signed-off-by: Nandhukumar <nandhukumare@gmail.com>

--- db_upgrade_scripts/mosip_resident/sql/1.2.0.1_to_1.2.1.0_rollback.sql
+++ db_upgrade_scripts/mosip_resident/sql/1.2.0.1_to_1.2.1.0_rollback.sql
@@ -1,1 +1,17 @@
-\echo 'Upgrade Queries not required for transition from $CURRENT_VERSION to $UPGRADE_VERSION'
+\c mosip_resident sysadmin
+
+REVOKE SELECT, INSERT, REFERENCES, UPDATE, DELETE
+ON resident.otp_transaction
+TO residentuser;
+
+REVOKE SELECT, INSERT, REFERENCES, UPDATE, DELETE
+ON resident.resident_grievance_ticket
+TO residentuser;
+
+ALTER TABLE resident.resident_session alter column machine_type type varchar(30);
+
+DROP INDEX IF EXISTS idx_resident_user_actions_ida_token;
+
+REVOKE SELECT, INSERT, REFERENCES, UPDATE, DELETE
+   ON resident.resident_user_actions
+   TO residentuser;
--- db_upgrade_scripts/mosip_resident/sql/1.2.0.1_to_1.2.1.0_upgrade.sql
+++ db_upgrade_scripts/mosip_resident/sql/1.2.0.1_to_1.2.1.0_upgrade.sql
@@ -1,1 +1,16 @@
-\echo 'Upgrade Queries not required for transition from $CURRENT_VERSION to $UPGRADE_VERSION'
+\c mosip_resident
+
+GRANT SELECT, INSERT, REFERENCES, UPDATE, DELETE
+ON resident.otp_transaction
+TO residentuser;
+
+GRANT SELECT, INSERT, REFERENCES, UPDATE, DELETE
+ON resident.resident_grievance_ticket
+TO residentuser;
+
+ALTER TABLE resident.resident_session alter column machine_type type varchar(100);
+-- Adding index to ida_token column
+CREATE INDEX idx_resident_user_actions_ida_token ON resident.resident_user_actions (ida_token);
+GRANT SELECT, INSERT, REFERENCES, UPDATE, DELETE
+   ON resident.resident_user_actions
+   TO residentuser;
