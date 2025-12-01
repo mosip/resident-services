416cb4399388d224bf5e05ff43eea2647539b5fa
kameshsr <kameshsr1338@gmail.com>
2025-12-01 17:59:40 +0530

    MOSIP-43872 Added test class for SessionUserNameUtilityTest
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

---
+++ resident/resident-service/src/test/java/io/mosip/resident/util/SessionUserNameUtilityTest.java
@@ -0,0 +1,86 @@
+package io.mosip.resident.util;
+
+import io.mosip.resident.constant.ResidentConstants;
+import io.mosip.resident.exception.ApisResourceAccessException;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.core.env.Environment;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
+
+/**
+ * Unit tests for SessionUserNameUtility
+ * @author Kamesh Shekhar Prasad
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class SessionUserNameUtilityTest {
+
+    private SessionUserNameUtility sessionUserNameUtility;
+
+    @Mock
+    private Environment environment;
+
+    @Mock
+    private AvailableClaimValueUtility availableClaimValueUtility;
+
+    @Before
+    public void setUp() {
+        sessionUserNameUtility = new SessionUserNameUtility();
+        ReflectionTestUtils.setField(sessionUserNameUtility, "environment", environment);
+        ReflectionTestUtils.setField(sessionUserNameUtility, "availableClaimValueUtility", availableClaimValueUtility);
+    }
+
+    @Test
+    public void getSessionUserName_whenNamePresent_returnsName() throws Exception {
+        String profileKey = "profile.name";
+        when(environment.getProperty(eq(ResidentConstants.NAME_FROM_PROFILE))).thenReturn(profileKey);
+        when(availableClaimValueUtility.getAvailableClaimValue(eq(profileKey))).thenReturn("John Doe");
+
+        String result = sessionUserNameUtility.getSessionUserName();
+
+        assertEquals("John Doe", result);
+        verify(environment, times(1)).getProperty(eq(ResidentConstants.NAME_FROM_PROFILE));
+        verify(availableClaimValueUtility, times(1)).getAvailableClaimValue(eq(profileKey));
+    }
+
+    @Test
+    public void getSessionUserName_whenNameNull_returnsUnknownConstant() throws Exception {
+        String profileKey = "profile.name";
+        when(environment.getProperty(eq(ResidentConstants.NAME_FROM_PROFILE))).thenReturn(profileKey);
+        when(availableClaimValueUtility.getAvailableClaimValue(eq(profileKey))).thenReturn(null);
+
+        String result = sessionUserNameUtility.getSessionUserName();
+
+        assertEquals(ResidentConstants.UNKNOWN, result);
+        verify(availableClaimValueUtility, times(1)).getAvailableClaimValue(eq(profileKey));
+    }
+
+    @Test
+    public void getSessionUserName_whenNameBlank_returnsUnknownConstant() throws Exception {
+        String profileKey = "profile.name";
+        when(environment.getProperty(eq(ResidentConstants.NAME_FROM_PROFILE))).thenReturn(profileKey);
+        when(availableClaimValueUtility.getAvailableClaimValue(eq(profileKey))).thenReturn("   ");
+
+        String result = sessionUserNameUtility.getSessionUserName();
+
+        assertEquals(ResidentConstants.UNKNOWN, result);
+        verify(availableClaimValueUtility, times(1)).getAvailableClaimValue(eq(profileKey));
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void getSessionUserName_whenAvailableClaimThrows_shouldWrapAndRethrow() throws Exception {
+        String profileKey = "profile.name";
+        when(environment.getProperty(eq(ResidentConstants.NAME_FROM_PROFILE))).thenReturn(profileKey);
+        when(availableClaimValueUtility.getAvailableClaimValue(eq(profileKey)))
+                .thenThrow(new ApisResourceAccessException("userinfo down"));
+
+        // expect RuntimeException wrapping the ApisResourceAccessException
+        sessionUserNameUtility.getSessionUserName();
+    }
+}
