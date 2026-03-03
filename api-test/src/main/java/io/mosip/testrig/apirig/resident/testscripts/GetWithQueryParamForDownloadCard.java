79cea808854f637df4b43a8cb79470def4ef0335
Sradha Mohanty <mohantysradha10@gmail.com>
2026-03-03 15:51:25 +0530

    MOSIP-31026
    Signed-off-by: Sradha Mohanty <mohantysradha10@gmail.com>

--- api-test/src/main/java/io/mosip/testrig/apirig/resident/testscripts/GetWithQueryParamForDownloadCard.java
+++ api-test/src/main/java/io/mosip/testrig/apirig/resident/testscripts/GetWithQueryParamForDownloadCard.java
@@ -1,6 +1,5 @@
 package io.mosip.testrig.apirig.resident.testscripts;
 
-import java.io.ByteArrayInputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -8,7 +7,6 @@ import java.util.Map;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.json.JSONObject;
-import org.testng.Assert;
 import org.testng.ITest;
 import org.testng.ITestContext;
 import org.testng.ITestResult;
@@ -19,9 +17,6 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import com.itextpdf.text.pdf.PdfReader;
-import com.itextpdf.text.pdf.parser.PdfTextExtractor;
-
 import io.mosip.testrig.apirig.dto.OutputValidationDto;
 import io.mosip.testrig.apirig.dto.TestCaseDTO;
 import io.mosip.testrig.apirig.resident.utils.ResidentConfigManager;
@@ -31,7 +26,6 @@ import io.mosip.testrig.apirig.testrunner.HealthChecker;
 import io.mosip.testrig.apirig.utils.AdminTestException;
 import io.mosip.testrig.apirig.utils.AdminTestUtil;
 import io.mosip.testrig.apirig.utils.GlobalConstants;
-import io.mosip.testrig.apirig.utils.GlobalMethods;
 import io.mosip.testrig.apirig.utils.OutputValidationUtil;
 import io.mosip.testrig.apirig.utils.ReportUtil;
 import io.mosip.testrig.apirig.utils.SecurityXSSException;
@@ -41,8 +35,6 @@ public class GetWithQueryParamForDownloadCard extends ResidentUtil implements IT
 	private static final Logger logger = Logger.getLogger(GetWithQueryParamForDownloadCard.class);
 	protected String testCaseName = "";
 	public Response response = null;
-	public byte[] pdf=null;
-	public String pdfAsText =null;
 	public boolean sendEsignetToken = false;
 	
 	@BeforeClass
@@ -120,48 +112,9 @@ public class GetWithQueryParamForDownloadCard extends ResidentUtil implements IT
 	
 	private void validatePdfOrError(Response response, TestCaseDTO testCaseDTO) throws AdminTestException {
 
-		String contentType = response != null ? response.getHeader("Content-Type") : null;
-		if (contentType != null && contentType.contains("application/pdf")) {
-			pdf = response.asByteArray();
-			try {
-				PdfReader reader;
-				try {
-					// First opening pdf without password
-					reader = new PdfReader(new ByteArrayInputStream(pdf));
-					if (!reader.isEncrypted()) {
-						logger.info("Opened non-encrypted PDF");
-					} else {
-						reader.close();
-						throw new com.itextpdf.text.exceptions.BadPasswordException("Encrypted PDF");
-					}
-
-				} catch (com.itextpdf.text.exceptions.BadPasswordException e) {
-
-					// If encrypted, try with password
-					String password = properties.getProperty("pdfPassword");
-
-					reader = new PdfReader(new ByteArrayInputStream(pdf), password.getBytes());
-
-					logger.info("Opened password protected PDF");
-				}
-				try {
-					pdfAsText = PdfTextExtractor.getTextFromPage(reader, 1);
-				} finally {
-					reader.close();
-				}
-
-				GlobalMethods.reportResponse(null, ApplnURI + testCaseDTO.getEndPoint(), "PDF Content:\n" + pdfAsText);
-
-				Map<String, List<OutputValidationDto>> outputValid = OutputValidationUtil.doJsonOutputValidation(
-						"{\"Content-Type\":\"" + contentType + "\"}",
-						getJsonFromTemplate(testCaseDTO.getOutput(), testCaseDTO.getOutputTemplate()), testCaseDTO,
-						response.getStatusCode());
-
-				Reporter.log(ReportUtil.getOutputValidationReport(outputValid));
-
-			} catch (Exception e) {
-				Assert.fail("Invalid PDF received: " + e.getMessage());
-			}
+		if (handlePdfResponse(response, testCaseDTO)) {
+			return;
+			
 		} else {
 			Map<String, List<OutputValidationDto>> outputValid = OutputValidationUtil.doJsonOutputValidation(
 					response.asString(), getJsonFromTemplate(testCaseDTO.getOutput(), testCaseDTO.getOutputTemplate()),
